b3c24a8ba5f98abeea1c0e11867a91918568b277
lms0806 <42136056+lms0806@users.noreply.github.com>
2020-12-13 00:08:49 +0900

    Create 2438.java

---
+++ 2000-2999/2438.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		
+		for(int i = 1; i <= a; i++) {
+			for(int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+}
