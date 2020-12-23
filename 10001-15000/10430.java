d411e0b947e1f45cb1bcc4e8350496e4ef4baabf
lms0806 <42136056+lms0806@users.noreply.github.com>
2020-12-24 01:42:57 +0900

    Create 10430.java

---
+++ 10001-15000/10430.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		
+		System.out.println((a+b)%c);
+		System.out.println(((a%c) + (b%c))%c);
+		System.out.println((a*b%c));
+		System.out.println(((a%c)*(b%c))%c);
+	}
+}
