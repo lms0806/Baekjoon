955d4a995812e7d3b183ba4f7b9f4e30089bc3b4
lms0806 <42136056+lms0806@users.noreply.github.com>
2020-12-24 01:44:49 +0900

    Create 10869.java

---
+++ 10001-15000/10869.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+}
