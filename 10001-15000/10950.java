7be4a8117ba6c144724130ea0b6662702145297e
lms0806 <42136056+lms0806@users.noreply.github.com>
2020-12-25 02:33:47 +0900

    Create 10950.java

---
+++ 10001-15000/10950.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int[] num = new int[a];
+		
+		for(int i = 0; i < a; i++) {
+			int b = scan.nextInt();
+			int c = scan.nextInt();
+			num[i] = b+c;
+		}
+		
+		for(int i = 0; i < a; i++) {
+			System.out.println(num[i]);
+		}
+		
+	}
+}
