a4d4454be93badd44f08f5c615b72c992ce8cdd3
lms0806 <42136056+lms0806@users.noreply.github.com>
2020-12-27 01:07:06 +0900

    Update 1152.java

--- 1-5000/1152.java
+++ 1-5000/1152.java
@@ -4,14 +4,12 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		String str = scan.nextLine();
+		String str = scan.nextLine().trim();
 		String[] strs = str.split(" ");
 		
 		int answer = strs.length;
-		if(str.substring(0,1).equals(" ")) {
-			answer-=1;
-		}
-		if(answer == -1) {
+		
+		if(str.equals("")) {
 			answer = 0;
 		}
 		System.out.println(answer);
