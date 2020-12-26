a5c149db3de9d36679345c48e831b5ad839d294b
lms0806 <42136056+lms0806@users.noreply.github.com>
2020-12-26 13:56:07 +0900

    Create 11654.java

---
+++ 10001-15000/11654.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String str = scan.nextLine();
+		System.out.println((int)str.charAt(0));
+	}
+}
