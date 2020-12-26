054b7ffb4d577efb022167561537755e14c1bb21
lms0806 <42136056+lms0806@users.noreply.github.com>
2020-12-26 16:18:01 +0900

    Create 16394.java

---
+++ 15001-20000/16394.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		System.out.println(n-1946);
+	}
+}
