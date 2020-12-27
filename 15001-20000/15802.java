cc813b1d3691f06be6bf7bf0e19ad0def6695aac
lms0806 <42136056+lms0806@users.noreply.github.com>
2020-12-28 02:08:02 +0900

    Create 15802.java

---
+++ 15001-20000/15802.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String str = scan.next();
+		
+		System.out.println("1");
+	}
+}
