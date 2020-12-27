8095dc6c353395f0f6fbbf9773b9de7d03934a32
lms0806 <42136056+lms0806@users.noreply.github.com>
2020-12-27 14:52:17 +0900

    Create 14645.java

---
+++ 10001-15000/14645.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		for(int i = 0; i < a; i++) {
+			int c = scan.nextInt();
+			int d = scan.nextInt();
+		}
+		
+		System.out.println("비와이");
+	}
+}
