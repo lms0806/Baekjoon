618314b4151ba8664bf0bd0a463a99bb0d8ae70a
lms0806 <42136056+lms0806@users.noreply.github.com>
2020-12-27 15:16:28 +0900

    Create 5339.java

---
+++ 5001-10000/5339.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("     /~\\");
+		System.out.println("    ( oo|");
+		System.out.println("    _\\=/_");
+		System.out.println("   /  _  \\");
+		System.out.println("  //|/.\\|\\\\");
+		System.out.println(" ||  \\ /  ||");
+		System.out.println("============");
+		System.out.println("|          |");
+		System.out.println("|          |");
+		System.out.println("|          |");
+	}
+}
