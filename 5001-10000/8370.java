c7ce8b8e15bb4970cb6f0b1e184ee9a0ff752ddd
lms0806 <42136056+lms0806@users.noreply.github.com>
2020-12-29 01:32:54 +0900

    Create 8370.java

---
+++ 5001-10000/8370.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		int d = scan.nextInt();
+		
+		int result = a*b + c*d;
+		
+		System.out.println(result);
+	}
+}
