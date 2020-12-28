54efa432d0f0bde804b288e65f0e3a347f4c10f0
lms0806 <42136056+lms0806@users.noreply.github.com>
2020-12-29 01:13:54 +0900

    Create 18108.java

---
+++ 15001-20000/18108.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		
+		System.out.println(a-543);
+	}
+}
