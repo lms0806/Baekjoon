b55f4511758aef5f39d76eda3c2cb16e656e0fb8
lms0806 <42136056+lms0806@users.noreply.github.com>
2020-12-30 19:13:37 +0900

    Create 2010.java

---
+++ 1-5000/2010.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		int sum = 0;
+		for(int i = 0; i < n; i++) {
+			sum += scan.nextInt();
+		}
+		
+		System.out.println(sum-n+1);
+	}
+}
