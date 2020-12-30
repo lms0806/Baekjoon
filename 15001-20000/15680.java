70b436674c887d71704df7fb57cf69fb5beb022c
lms0806 <42136056+lms0806@users.noreply.github.com>
2020-12-30 17:32:06 +0900

    Create 15680.java

---
+++ 15001-20000/15680.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		
+		if(a == 0) {
+			System.out.println("YONSEI");
+		}
+		else if(a == 1) {
+			System.out.println("Leading the Way to the Future");
+		}
+	}
+}
