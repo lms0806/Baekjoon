ab56718ef1ceafe572a9ae1a6370d277ebb77bc2
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-02 05:23:14 +0900

    Create 10474.java

---
+++ 10001-15000/10474.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			if(a == 0 && b == 0) {
+				break;
+			}
+			int num = a/b;
+			int num1 = a%b;
+			
+			bw.write(num + " " + num1 + " / " + b + "\n");
+		}
+		
+		bw.flush();
+
+		br.close();
+		bw.close();
+	}
+}
