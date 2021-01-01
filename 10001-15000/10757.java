71c5eb6463c54f1b3a4cda184b14055a0144d4ba
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-02 03:29:02 +0900

    Create 10757.java

---
+++ 10001-15000/10757.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	    
+		String[] arr = br.readLine().split(" "); 
+	    
+		System.out.println(new BigInteger(arr[0]).add(new BigInteger(arr[1])));
+	}
+}
