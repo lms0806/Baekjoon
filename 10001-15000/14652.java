47f10c557facc7c5f32743aae78f481fba20a909
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-04 21:42:25 +0900

    Create 14652.java

---
+++ 10001-15000/14652.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		
+		int answer1 = c/b;
+		int answer2 = c%b;
+				
+		bw.write(answer1 + " " + answer2 + " ");
+		bw.flush();
+
+		br.close();
+		bw.close();
+	}
+}
