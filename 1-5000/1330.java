f3d3c46a11e66cce8c5a13ec8f58ff5f49914dd3
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-05 02:47:11 +0900

    Update 1330.java

--- 1-5000/1330.java
+++ 1-5000/1330.java
@@ -1,11 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException,IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int a = scan.nextInt();
-		int b = scan.nextInt();
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		br.close();
+		
+		String str = "";
 		
 		if(a > b) {
 			System.out.print(">");
