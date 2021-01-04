815d298127f46eb8055250a22e9a52e40a93da24
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-05 00:46:09 +0900

    Create 11023.java

---
+++ 10001-15000/11023.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		String[] str = br.readLine().split(" ");
+		
+		int sum = 0;
+		for(int i = 0; i < str.length; i++) {
+			sum += Integer.parseInt(str[i]);
+		}
+
+		System.out.println(sum);
+		br.close();
+	}
+}
