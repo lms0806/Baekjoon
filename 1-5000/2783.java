5e1b0eac9b269dbb66e5d4693a466c982b322def
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-05 01:05:16 +0900

    Create 2783.java

---
+++ 1-5000/2783.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		double x1,y1,sum1 = 0;
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		double x = Double.parseDouble(st.nextToken());
+		double y = Double.parseDouble(st.nextToken());
+		
+		double sum = 1000/y*x;
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < size; i++) {
+			st = new StringTokenizer(br.readLine());
+			x1 = Double.parseDouble(st.nextToken());
+			y1 = Double.parseDouble(st.nextToken());
+			sum1 = 1000/y1*x1;
+			if(sum > sum1) {
+				sum = sum1;
+			}
+		}
+
+		System.out.println(sum);
+		br.close();
+	}
+}
