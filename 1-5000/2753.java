0479cf29dcb73a20096739f9e65910ba0a2d7ad5
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-05 03:05:37 +0900

    Update 2753.java

--- 1-5000/2753.java
+++ 1-5000/2753.java
@@ -1,16 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException,IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int a = scan.nextInt();
+		int a = Integer.parseInt(br.readLine());
 		
-		if((a%4 == 0 && a%100 != 0) || a%400 == 0) {
+		br.close();
+		
+		if((a%4 == 0 && a%100 != 0) || a%400 == 0) 
 			System.out.print("1");
-		}
-		else {
+		else 
 			System.out.print("0");
-		}
 	}
 }
