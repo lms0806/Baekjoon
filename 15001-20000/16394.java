7d377e3445754e73c4509c708edb57fcece0a2ef
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-05 01:45:16 +0900

    Update 16394.java

--- 15001-20000/16394.java
+++ 15001-20000/16394.java
@@ -1,10 +1,12 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		int n = scan.nextInt();
+	public static void main(String[] args) throws NumberFormatException,IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		int n;
+		n = Integer.parseInt(br.readLine());
 		
 		System.out.println(n-1946);
 	}
