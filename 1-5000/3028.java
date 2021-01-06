2b2fce9847463fa4aae9a2e0c1315bdf8b0b90c6
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-07 06:15:14 +0900

    Create 3028.java

---
+++ 1-5000/3028.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		String[] str = br.readLine().split("");
+		
+		int answer = 1;
+		for(int i = 0; i < str.length; i++) {
+			if(str[i].equals("A")) {
+				if(answer == 1) {
+					answer = 2;
+				}
+				else if(answer == 2) {
+					answer = 1;
+				}
+			}
+			else if(str[i].equals("B")) {
+				if(answer == 2) {
+					answer = 3;
+				}
+				else if(answer == 3) {
+					answer = 2;
+				}
+			}
+			else if(str[i].equals("C")) {
+				if(answer == 1) {
+					answer = 3;
+				}
+				else if(answer == 3) {
+					answer = 1;
+				}
+			}
+		}
+		System.out.println(answer);
+	}
+}
