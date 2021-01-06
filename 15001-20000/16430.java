2eb0fd1e6ad29f0fdef116497a5e993d3b7930ad
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-06 19:22:07 +0900

    Update 16430.java

--- 15001-20000/16430.java
+++ 15001-20000/16430.java
@@ -1,12 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
 		System.out.println(b-a + " " + b);
 	}
 }
