862cbf36c99b5d7f90561d956427b63d20b3f950
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-06 19:27:30 +0900

    Update 15964.java

--- 15001-20000/15964.java
+++ 15001-20000/15964.java
@@ -6,16 +6,13 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		long a, b, sum, sub;
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		a = Long.parseLong(st.nextToken());
-		b = Long.parseLong(st.nextToken());
-		sum = a + b;
-		sub = a - b;
+		long a = Long.parseLong(st.nextToken());
+		long b = Long.parseLong(st.nextToken());
 		
-		System.out.println(sum*sub);
-
 		br.close();
+		
+		System.out.println((a+b)*(a-b));
 	}
 }
