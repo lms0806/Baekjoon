c54b68b53887585d7a987cdc3023267fe5d204f3
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-08 22:06:52 +0900

    Update 15439.java

--- 15001-20000/15439.java
+++ 15001-20000/15439.java
@@ -1,10 +1,12 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int a = scan.nextInt();
+		int a = Integer.parseInt(br.readLine());
 		
 		int result = a*a-a;
 		
