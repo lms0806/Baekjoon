d87da67e1fc47e44a2551fe057e3a4fa05b005eb
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-08 21:34:06 +0900

    Update 17256.java

--- 15001-20000/17256.java
+++ 15001-20000/17256.java
@@ -1,32 +1,37 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
 		int[] a = new int[3];
 		int[] b = new int[3];
 		int[] c = new int[3];
 		
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < 3; i++) {
-			a[i] = scan.nextInt();
+			a[i] = Integer.parseInt(st.nextToken());
 		}
 		
+		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < 3; i++) {
-			c[i] = scan.nextInt();
+			b[i] = Integer.parseInt(st.nextToken());
 		}
 		
 		for(int i = 0; i < 3; i++) {
 			if(i == 1) {
-				b[i] = c[i] / a[i];
+				c[i] = b[i] / a[i];
 			}
 			else {
-				b[i] = c[i] - a[2-i];
+				c[i] = b[i] - a[2-i];
 			}
 		}
 		
 		for(int i = 0; i < 3; i++) {
-			System.out.println(b[i]);
+			System.out.print(c[i] + " ");
 		}
 	}
 }
