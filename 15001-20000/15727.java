fe0db5c3fc3f7969e950204528ee4f93b1f92714
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-08 20:48:49 +0900

    Update 15727.java

--- 15001-20000/15727.java
+++ 15001-20000/15727.java
@@ -1,15 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int a = scan.nextInt();
+		int a = Integer.parseInt(br.readLine());
 		
-		if(a == 0) {
-			System.out.println("0");
-		}
-		else if(a%5 == 0){
+		
+		if(a%5 == 0){
 			System.out.println((a/5));
 		}
 		else {
