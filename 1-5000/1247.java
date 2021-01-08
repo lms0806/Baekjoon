b365a8813db23715de66c90aa7bbb5b5dee8f17e
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-08 20:01:15 +0900

    Create 1247.java

---
+++ 1-5000/1247.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int size;
+		
+		for(int i = 0; i < 3; i++) {
+			BigInteger sum = new BigInteger("0");
+			size = Integer.parseInt(br.readLine());
+			for(int j = 0; j < size; j++) {
+				BigInteger a = new BigInteger(br.readLine());
+				sum = sum.add(a);
+			}
+			if(sum.compareTo(BigInteger.ZERO) == 0) {
+				System.out.println("0");
+			}
+			else if(sum.compareTo(BigInteger.ZERO) == -1) {
+				System.out.println("-");
+			}
+			else {
+				System.out.println("+");
+			}
+		}
+	}
+}
