96d32e42d06c94ecef72d3bc91178576b87bc965
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-08 21:02:26 +0900

    Create 2935.java

---
+++ 1-5000/2935.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		BigInteger a = new BigInteger(br.readLine());
+		String str = br.readLine();
+		BigInteger b = new BigInteger(br.readLine());
+		
+		if(str.equals("+")) {
+			a = a.add(b);
+		}
+		else {
+			a = a.multiply(b);
+		}
+		System.out.println(a);
+	}
+}
