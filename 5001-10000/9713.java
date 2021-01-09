e1159639813d47f4b69b25f759e14831137a2b4c
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-09 11:36:01 +0900

    Create 9713.java

---
+++ 5001-10000/9713.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		int num = 0;
+		for(int i = 0; i < size; i++) {
+			num = Integer.parseInt(br.readLine());
+			
+			int result = 0;
+			for(int j = 1; j <= num; j+=2) {
+				result += j;
+			}
+			
+			System.out.println(result);
+		}
+	}
+}
