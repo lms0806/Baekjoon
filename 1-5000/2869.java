27251c343f0937b7975f68d4a02ccdc95df90a3f
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-13 18:20:52 +0900

    Create 2869.java

---
+++ 1-5000/2869.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int v = Integer.parseInt(st.nextToken());
+		
+		int day = (v - b) / (a - b);
+		if((v - b) % (a - b) != 0) {
+			day++;
+		}
+		
+		System.out.println(day);
+	}
+}
