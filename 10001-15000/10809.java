8ed6f725a4de9a13fadfbefb53ec242d0ddf6957
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-13 12:10:10 +0900

    Create 10809.java

---
+++ 10001-15000/10809.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		String[] alpha = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+		int[] num = new int[alpha.length];
+		
+		String[] str = br.readLine().split("");
+		for(int i = 0; i < str.length; i++) {
+			for(int j = 0; j < alpha.length; j++) {
+				if(str[i].equals(alpha[j]) && num[j] == 0) {
+					num[j] = i+1;
+					break;
+				}
+			}
+		}
+		
+		for(int i = 0; i < num.length; i++) {
+			System.out.print(num[i]-1 + " ");
+		}
+	}
+}
