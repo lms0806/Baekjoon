7ee2b8dfea8cd60e9d814039b7bc3a3357618741
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-15 03:03:30 +0900

    Create 10829.java

---
+++ 10001-15000/10829.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+
+		BigInteger a = new BigInteger(br.readLine());
+		
+		System.out.println(a.toString(2));
+	}
+}
