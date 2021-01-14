d9cdd3ce9c03cc09f42abc383e8da392bb92e52f
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-15 02:46:52 +0900

    Create 16428.java

---
+++ 15001-20000/16428.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		BigInteger a = new BigInteger(st.nextToken());
+		BigInteger b = new BigInteger(st.nextToken());
+		
+		System.out.println(a.divide(b));
+		System.out.println(a.remainder(b));
+	}
+}
