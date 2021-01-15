bfa2b50ee5d5c6fdd43da5efec25435717e7fefc
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-16 01:29:05 +0900

    Create 1110.java

---
+++ 1-5000/1110.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		int start = num, number = 0, count = 0;
+		while(true) {
+			count++;
+			number = num/10 + num%10;
+			num = number%10 + (num%10)*10;
+			if(start == num) {
+				break;
+			}
+		}
+		
+		System.out.println(count);
+	}
+}
