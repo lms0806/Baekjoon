2607caedf93af516c91e4636d0b59e44d4bedfad
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-16 00:27:21 +0900

    Update 2525.java

--- 1-5000/2525.java
+++ 1-5000/2525.java
@@ -1,12 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(br.readLine());
 		
 		b += c;
 		while(b >= 60) {
