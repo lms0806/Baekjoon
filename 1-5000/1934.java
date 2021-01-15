d0b8d741a9401660afac2bf9e927ceea4db9f254
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-16 01:00:49 +0900

    Update 1934.java

--- 1-5000/1934.java
+++ 1-5000/1934.java
@@ -1,30 +1,29 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int n = scan.nextInt();
-		int[] nums = new int[n];
+		int size = Integer.parseInt(br.readLine());
+		int a, b;
 		
-		int m = 2;
-		int min = 1;
-		for(int i = 0; i < n; i++) {
-			int a = scan.nextInt();
-			int b = scan.nextInt();
+		int m = 2, min = 1;
+		for(int i = 0; i < size; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
 			while(m<=a && m<=b) {
 				if(a%m==0 && b%m==0) {
 					min = m;
 				}
 				m++;
 			}
-			nums[i] = a*b/min;
+			System.out.println(a*b/min);
 			min = 1;
-			m=2;
-		}
-		
-		for(int i = 0; i < n; i++) {
-			System.out.println(nums[i]);
+			m = 2;
 		}
 	}
 }
