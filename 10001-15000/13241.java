996c0274809ec42cc01c5cc4d664e820b7e4a74e
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-16 21:04:52 +0900

    Update 13241.java

--- 10001-15000/13241.java
+++ 10001-15000/13241.java
@@ -1,34 +1,27 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		long a = Integer.parseInt(st.nextToken());
 		long b = Integer.parseInt(st.nextToken());
 		
-		long n = 2;
-		long max = 0, min = 1;
+		long max = 0, min = gcd(a,b);
 		
-		while(n<=a && n<=b) {
-			if(a%n == 0 && b%n == 0) {
-				min = n;
-			}
-			n++;
-		}
 		max = a*b/min;
 		
-		bw.write(max + "\n");
-		bw.flush();
-
-		br.close();
-		bw.close();
+		System.out.println(max);
+	}
+	
+	public static long gcd(long a, long b) {
+		if(b == 0) {
+			return a;
+		}
+		return gcd(b, a%b);
 	}
 }
