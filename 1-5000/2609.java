1cffb157a8b181b2acd7d12db6eee696c9556216
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-16 20:56:29 +0900

    Update 2609.java

--- 1-5000/2609.java
+++ 1-5000/2609.java
@@ -1,33 +1,26 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		int a = Integer.parseInt(st.nextToken());
 		int b = Integer.parseInt(st.nextToken());
-		int n = 2;
-		int max = 0, min = 1;
-		
-		while(n<=a && n<=b) {
-			if(a%n == 0 && b%n == 0) {
-				min = n;
-			}
-			n++;
-		}
-		max = a*b/min;
-		
-		bw.write(min + "\n" + max);
-		bw.flush();
 
-		br.close();
-		bw.close();
+		int min = gcd(a,b);
+		int max = a * b / min;
+		System.out.println(min);
+		System.out.println(max);
+	}
+	
+	public static int gcd(int a, int b) {
+		if(b == 0) {
+			return a;
+		}
+		return gcd(b, a%b);
 	}
 }
