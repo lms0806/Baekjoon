e137a97123f8539afbff4ef8cc59521594f11f7c
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-16 21:22:39 +0900

    Create 1735.java

---
+++ 1-5000/1735.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int[] a = new int[2];
+		int[] b = new int[2];
+		
+		for(int i = 0; i < 2; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			a[i] = Integer.parseInt(st.nextToken());
+			b[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int min = gcd(b[0],b[1]);
+		int max = b[0] * b[1]/ min;
+		int num = max / b[0] * a[0] + max / b[1] * a[1];
+		
+		int gcd = gcd(num, max);
+		
+		System.out.println(num/gcd + " " + max/gcd);
+	}
+	
+	public static int gcd(int a, int b) {
+		if(b == 0) {
+			return a;
+		}
+		return gcd(b, a%b);
+	}
+}
