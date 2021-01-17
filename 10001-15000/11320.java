77cc2039fb2be6ef16656bfcbda9e15abc6ed69f
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-18 04:37:54 +0900

    Create 11320.java

---
+++ 10001-15000/11320.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < size; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			int num = a/b;
+			
+			System.out.println((int)Math.pow(num, 2));
+		}
+	}
+}
