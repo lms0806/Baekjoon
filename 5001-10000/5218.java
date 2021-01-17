d112f7c426283f26eb2e514564d409ead2c66dbe
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-17 22:05:41 +0900

    Create 5218.java

---
+++ 5001-10000/5218.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < size; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String x = st.nextToken();
+			String y = st.nextToken();
+			
+			StringBuilder sb = new StringBuilder();
+			
+			sb.append("Distances: ");
+			for(int j = 0; j < x.length(); j++) {
+				int cx = x.charAt(j) - 'A';
+				int cy = y.charAt(j) - 'A';
+				
+				if(cx > cy) {
+					sb.append(cy+26-cx + " ");
+				}
+				else {
+					sb.append(cy-cx + " ");
+				}
+			}
+			System.out.println(sb);
+		}
+	}
+}
