400c8152c3610901f3bc60aad5584581e1591c34
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-17 18:38:05 +0900

    Create 1292.java

---
+++ 1-5000/1292.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		int num = 1,count = 0, sum = 0;
+		while(true) {
+			for(int i = 1; i <= num; i++) {
+				count++;
+				if(count >= a && count <= b) {
+					sum += num;
+				}
+				if(count > b) {
+					break;
+				}
+			}
+			if(count > b) {
+				break;
+			}
+			num++;
+		}
+		System.out.println(sum);
+	}
+}
