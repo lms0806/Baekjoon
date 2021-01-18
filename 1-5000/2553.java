26c24b8fd8540f6913113d93de807b1b72d27225
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-18 21:35:29 +0900

    Create 2553.java

---
+++ 1-5000/2553.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		long sum = 1;
+		for(int i = 1; i <= n; i++) {
+			sum *= i;
+			sum %= 10000000;
+			
+			while(sum % 10 == 0) {
+				sum/=10;
+			}
+		}
+		
+		System.out.println(sum%10);
+	}
+}
