fe7e6e8933ca2cf7dee3f8076cd578685c8abd3e
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-19 20:50:59 +0900

    Create 9012.java

---
+++ 5001-10000/9012.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < num; i++) {
+			String str = br.readLine();
+			
+			String answer = "YES";
+			int count = 0;
+			for(int j = 0; j < str.length(); j++) {
+				if(str.charAt(j) == '(') {
+					count++;
+				}
+				else if(str.charAt(j) == ')') {
+					if(count > 0) {
+						count--;
+					}
+					else {
+						answer = "NO";
+						break;
+					}
+				}
+			}
+			
+			if(count != 0) {
+				answer = "NO";
+			}
+			System.out.println(answer);
+		}
+	}
+}
