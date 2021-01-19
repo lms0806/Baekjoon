87e5aff4e0f55649a870a10d68e4e7dbb6bdbe65
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-19 21:12:09 +0900

    Create 2751.java

---
+++ 1-5000/2751.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		boolean[] num = new boolean[2000001];
+		
+		for(int i = 0; i < size; i++) {
+			num[Integer.parseInt(br.readLine()) + 1000000] = true;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < num.length; i++) {
+			if(num[i]) {
+				sb.append(i - 1000000).append("\n");
+			}
+		}
+		
+		System.out.println(sb);
+	}
+}
