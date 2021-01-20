f680c9c26d019dca07fadd89cfdf4da718f76206
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-21 02:24:27 +0900

    Create 11283.java

---
+++ 10001-15000/11283.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	
+		String str = br.readLine();
+		
+		System.out.println((int)str.charAt(0) - '가' + 1);
+	}
+}
