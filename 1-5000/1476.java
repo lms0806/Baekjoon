34f2703cc96970be76748c9ee64bacf94b3d0a60
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-22 00:39:54 +0900

    Create 1476.java

---
+++ 1-5000/1476.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int e = Integer.parseInt(st.nextToken());
+		int s = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int answer = 1;
+		while(true) {
+			if((answer-e)%15 == 0 && (answer-s)%28 == 0 && (answer-m) % 19 == 0) {
+				break;
+			}
+			answer++;
+		}
+		
+		System.out.println(answer);
+	}
+}
