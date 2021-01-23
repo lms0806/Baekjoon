1ed21f17cc2cfecc93ffca1672769fac0da33f5d
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-24 03:54:55 +0900

    Create 19945.java

---
+++ 15001-20000/19945.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		String str = Integer.toBinaryString(Integer.parseInt(br.readLine()));
+		System.out.println(str.length());
+	}
+}
