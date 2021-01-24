58503e447727beae4356618400da7759af6c9bd5
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-24 23:20:54 +0900

    Create 5026.java

---
+++ 5001-10000/5026.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < size; i++) {
+			String str = br.readLine();
+			if(str.equals("P=NP")) {
+				sb.append("skipped").append("\n");
+			}
+			else {
+				String[] strs = str.split("\\+");
+				int answer = Integer.parseInt(strs[0]) + Integer.parseInt(strs[1]);
+				sb.append(answer).append("\n");
+			}
+		}
+		System.out.println(sb);
+	}
+}
