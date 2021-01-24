687dfdd0274c47ba09e33e60f49c43b75c83aeee
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-24 18:12:26 +0900

    Create 10810.java

---
+++ 10001-15000/10810.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int[] num = new int[n];
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			Arrays.fill(num, a-1, b, c);
+		}
+		
+		for(int i = 0; i < n; i++) {
+			sb.append(num[i]).append(" ");
+		}
+		
+		System.out.println(sb);
+	}
+}
