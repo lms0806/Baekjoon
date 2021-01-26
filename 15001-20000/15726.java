553b9bbe1ebebf35d75fc75da97d38aa8b009288
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-27 02:46:29 +0900

    Create 15726.java

---
+++ 15001-20000/15726.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		double a, b, c;
+		
+		a = Integer.parseInt(st.nextToken());
+		b = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		
+		double num = a * b / c;
+		double num1 = a / b * c;
+		
+		long answer = (long)Math.max(num, num1);
+		System.out.println(answer);
+		
+	}
+}
