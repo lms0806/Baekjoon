c746fb914d6f8684f64316975007a2542563b43d
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-27 19:30:51 +0900

    Create 1978.java

---
+++ 1-5000/1978.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		boolean istrue = true;
+		
+		int answer = 0;
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= n; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			istrue = true;
+			
+			istrue = sosu(num);
+			
+			if(istrue == true) {
+				answer++;
+			}
+		}
+		
+		System.out.println(answer);
+	}
+	
+	public static boolean sosu(int n) {
+		if(n == 1) {
+			return false;
+		}
+		for (int i = 2; i*i <= n; i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
