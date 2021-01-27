18f221af9e6d951180eb8649a184febfd02513bc
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-27 18:44:21 +0900

    Create 1620.java

---
+++ 1-5000/1620.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int size = Integer.parseInt(st.nextToken());
+		int pro = Integer.parseInt(st.nextToken());
+		
+		Map<String, String> map = new HashMap<>();
+		for(int i = 0; i < size; i++) {
+			String read = br.readLine();
+			map.put(read, Integer.toString(i+1));
+			map.put(Integer.toString(i+1), read);
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < pro; i++) {
+			sb.append(map.get(br.readLine())).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
