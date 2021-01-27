b98df5036b81fd50279a185e88c2c695164e1ad6
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-27 19:15:39 +0900

    Create 1929.java

---
+++ 1-5000/1929.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		boolean istrue = true;
+		
+		StringBuilder sb = new StringBuilder();
+		while(n <= m) {
+			istrue = true;
+			
+			istrue = sosu(n);
+			
+			if(istrue == true) {
+				sb.append(n + "\n");
+			}
+			n++;
+		}
+		
+		System.out.println(sb);
+	}
+	
+	public static boolean sosu(int n) {
+		if(n == 1) {
+			return false;
+		}
+		for (int i = 2; i*i <= n; i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
