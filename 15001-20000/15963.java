23fabe281c28aee3c07b7b727d7ca7b1139e2235
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-27 23:58:29 +0900

    Create 15963.java

---
+++ 15001-20000/15963.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long a = Long.parseLong(st.nextToken());
+		long b = Long.parseLong(st.nextToken());
+		
+		System.out.println((a == b) ? 1 : 0);
+	}
+}
