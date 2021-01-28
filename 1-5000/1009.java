6c49243b461277f5e94ecc15080505e36b2dbbcf
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-29 04:28:22 +0900

    Create 1009.java

---
+++ 1-5000/1009.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < size; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			int c = 1;
+			for(int j = 0; j < b; j++) {
+				c = (c*a)%10;
+			}
+			
+			if(c == 0) {
+				c = 10;
+			}
+			
+			sb.append(c).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+}
