1c171b27f6de27b70ff52159acf623e91b01d5f1
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-28 18:27:50 +0900

    Create 15792.java

---
+++ 15001-20000/15792.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		double a = Double.parseDouble(st.nextToken());
+		double b = Double.parseDouble(st.nextToken());
+		
+		System.out.println(a/b);
+	}
+}
