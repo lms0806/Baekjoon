8f7ba9094909b4000cdcdaadc03aead1f8415fbb
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-31 22:36:48 +0900

    Create 1252.java

---
+++ 1-5000/1252.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		BigInteger a = new BigInteger(st.nextToken(),2);
+		BigInteger b = new BigInteger(st.nextToken(),2);
+		
+		System.out.println(a.add(b).toString(2));
+	}
+}
