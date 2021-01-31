583b080d75051bc548f8a2a9f90127b47112bbb9
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-01-31 22:45:01 +0900

    Create 4328.java

---
+++ 1-5000/4328.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int b = Integer.parseInt(st.nextToken());
+			
+			if(b == 0) {
+				break;
+			}
+			
+			BigInteger p = new BigInteger(st.nextToken(),b);
+			BigInteger m = new BigInteger(st.nextToken(),b);
+			
+			System.out.println(p.remainder(m).toString(b));
+		}
+	}
+}
