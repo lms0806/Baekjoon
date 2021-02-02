ff044e078429d9dd6f3f2cbf5124c9186608e35d
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-02 20:08:47 +0900

    Create 4158.java

---
+++ 1-5000/4158.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		StringBuilder sb = new StringBuilder();
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			
+			if(n == 0 && m == 0) {
+				System.out.println(sb);
+				break;
+			}
+			
+			HashMap<Integer,Integer> map = new HashMap<>();
+			int num = 0;
+			int answer = 0;
+			
+			for(int i = 0; i < n; i++) {
+				num = Integer.parseInt(br.readLine());
+				map.put(num, 1);
+			}
+			
+			for(int i = 0; i < m; i++) {
+				num = Integer.parseInt(br.readLine());
+				if(map.containsKey(num)) {
+					answer++;
+				}
+			}
+			sb.append(answer).append(" ");
+		}
+	}
+}
