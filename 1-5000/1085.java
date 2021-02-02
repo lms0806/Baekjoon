d66c0347fd448269e7fc82c9698cacfd602b7450
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-02 18:58:56 +0900

    Create 1085.java

---
+++ 1-5000/1085.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		int w = Integer.parseInt(st.nextToken());
+		int h = Integer.parseInt(st.nextToken());
+		
+		int answer = (int)Math.min(Math.min(w-x, x), Math.min(h-y, y));
+		
+		System.out.println(answer);
+	}
+}
