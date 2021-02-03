f11df341646483484ce2ea86fd8d835ebbae0dfc
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-03 22:19:08 +0900

    Create 10845.java

---
+++ 10001-15000/10845.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		Queue<Integer> queue = new LinkedList<>();
+		
+		int size = Integer.parseInt(br.readLine())+1;
+		int charat, num = 0;
+		String str;
+		
+		StringBuilder sb = new StringBuilder();
+		while(0<--size) {
+			str = br.readLine();
+			charat = str.charAt(0);
+			if(charat == 'f') {
+				sb.append(queue.isEmpty() ? -1 : queue.peek()).append("\n");
+			}
+			else if(charat == 'b') {
+				sb.append(queue.isEmpty() ? -1 : num).append("\n");
+			}
+			else if(charat == 'e') {
+				sb.append(queue.isEmpty() ? 1 : 0).append("\n");
+			}
+			else if(charat == 's') {
+				sb.append(queue.size()).append("\n");
+			}
+			else if(charat == 'p') {
+				if(str.charAt(1) == 'u') {
+					num = Integer.parseInt(str.substring(5));
+					queue.add(num);
+				}
+				else {
+					sb.append(queue.isEmpty() ? -1 : queue.poll()).append("\n");
+				}
+			}
+		}
+		System.out.print(sb);
+	}
+}
