d651757c39779c40b8714f81bc6ed6aeefcd4515
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-08 00:46:59 +0900

    Update 2440.java

--- 1-5000/2440.java
+++ 1-5000/2440.java
@@ -1,16 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException,IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+
+		int size = Integer.parseInt(br.readLine());
 		
-		int n = scan.nextInt();
-		
-		for(int i = 0; i < n; i++) {
-			for(int j = n-i; j > 0; j--) {
-				System.out.print("*");
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < size; i++) {
+			for(int j = size-i; j > 0; j--) {
+				sb.append("*");
 			}
-			System.out.println("");
+			sb.append("\n");
 		}
+		System.out.print(sb);
 	}
 }
