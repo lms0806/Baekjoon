0c0be831b822d924aef83ad98562a04819d78ebf
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-08 01:08:28 +0900

    Update 2506.java

--- 1-5000/2506.java
+++ 1-5000/2506.java
@@ -1,14 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException,IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int num = scan.nextInt();
+		int size = Integer.parseInt(br.readLine());
 		
 		int n = 0, sum = 0;
-		for(int i = 0; i < num; i++) {
-			int a = scan.nextInt();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < size; i++) {
+			int a = Integer.parseInt(st.nextToken());
 			if(a == 1) {
 				if(n != 0) {
 					n++;
