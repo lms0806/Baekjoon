ad1e07e8222684a0a31ca77ee75f7a4d518f7715
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-08 00:09:45 +0900

    Update 1550.java

--- 1-5000/1550.java
+++ 1-5000/1550.java
@@ -1,22 +1,15 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws NumberFormatException,IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		String str = br.readLine();
 		
 		int num = Integer.parseInt(str, 16);
 		
-		bw.write(num + "\n");
-		bw.flush();
-
-		br.close();
-		bw.close();
+		System.out.print(num);
 	}
 }
