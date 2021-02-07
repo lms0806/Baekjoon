09575528e4367d743474044b814c18d788d88bb1
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-08 01:17:02 +0900

    Update 2562.java

--- 1-5000/2562.java
+++ 1-5000/2562.java
@@ -1,15 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException,IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
 		int[] num = new int[9];
 		
 		int max = num[0];
 		int index = 0;
 		for(int i = 0; i < num.length; i++) {
-			num[i] = scan.nextInt();
+			num[i] = Integer.parseInt(br.readLine());
 			if(max < num[i]) {
 				max = num[i];
 				index = i+1;
