d9d62251d903cb3ff4410346296a8948826454cd
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-08 01:04:47 +0900

    Update 2493.java

--- 1-5000/2493.java
+++ 1-5000/2493.java
@@ -11,19 +11,21 @@ public class Main{
 		int size = Integer.parseInt(br.readLine());
 		
 		String[] str = br.readLine().split(" ");
+        	StringBuilder sb = new StringBuilder();
 		for(int i = 1; i <= size; i++) {
 			int num = Integer.parseInt(str[i-1]);
 			while(!sta.isEmpty()) {
 				if(sta.peek()[1] >= num) {
-					System.out.print(sta.peek()[0] + " ");
+					sb.append(sta.peek()[0]).append(" ");
 					break;
 				}
 				sta.pop();
 			}
 			if(sta.isEmpty()) {
-				System.out.print("0 ");
+				sb.append("0 ");
 			}
 			sta.push(new int[] {i, num});
 		}
+        	System.out.print(sb);
 	}
 }
