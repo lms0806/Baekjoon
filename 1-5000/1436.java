17676f5fcf52ea56cbea7593383e424cbea6081d
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-07 23:54:29 +0900

    Update 1436.java

--- 1-5000/1436.java
+++ 1-5000/1436.java
@@ -1,14 +1,10 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws NumberFormatException,IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
 		int n = Integer.parseInt(br.readLine());
 		
 		int num = 666;
@@ -21,10 +17,6 @@ public class Main {
 			}
 		}
 		
-		bw.write(num + "\n");
-		bw.flush();
-
-		br.close();
-		bw.close();
+		System.out.println(num);
 	}
 }
