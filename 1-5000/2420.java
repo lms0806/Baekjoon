98f34e0384b927c8e6966991860f126973722bb1
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-08 00:43:33 +0900

    Update 2420.java

--- 1-5000/2420.java
+++ 1-5000/2420.java
@@ -1,13 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException,IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		long a = scan.nextInt();
-		long b = scan.nextInt();
+		long a = Long.parseLong(st.nextToken());
+		long b = Long.parseLong(st.nextToken());
 		
-		long result = Math.abs(a-b);
-		System.out.println(result);
+		System.out.println(Math.abs(a-b));
 	}
 }
