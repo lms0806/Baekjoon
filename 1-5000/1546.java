2e428f458f0be87357a005c072929d1e2e8a212c
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-08 00:07:08 +0900

    Update 1546.java

--- 1-5000/1546.java
+++ 1-5000/1546.java
@@ -1,26 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException,IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int number = scan.nextInt();
+		int size = Integer.parseInt(br.readLine());
 		
-		double[] num = new double[number];
+		double[] num = new double[size];
 		
-		for(int i = 0; i < number; i++) {
-			num[i] = scan.nextInt();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < size; i++) {
+			num[i] = Double.parseDouble(st.nextToken());
 		}
 		
 		Arrays.sort(num);
 		
-		double max = num[number-1];
+		double max = num[size-1];
 		
 		double sum = 0;
-		for(int i = 0; i < number; i++) {
+		for(int i = 0; i < size; i++) {
 			sum += num[i] / max * 100;
 		}
-		System.out.println(sum/number);
+		System.out.println(sum/size);
 	}
 }
