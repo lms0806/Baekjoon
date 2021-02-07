bed34c46ea3023593a09e01d888f69ab00dbedfb
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-08 01:19:12 +0900

    Update 2566.java

--- 1-5000/2566.java
+++ 1-5000/2566.java
@@ -1,15 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException,IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
 		int max = 0;
 		int x = 0, y = 0;
 		
 		for(int i = 0; i < 9; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int j = 0; j < 9; j++) {
-				int a = scan.nextInt();
+				int a = Integer.parseInt(st.nextToken());
 				if(a > max) {
 					max = a;
 					x = i;
@@ -17,8 +21,8 @@ public class Main {
 				}
 			}
 		}
-		
-		System.out.println(max);
-		System.out.println((x+1) + " " + (y+1));
+		StringBuilder sb = new StringBuilder();
+		sb.append(max).append("\n").append(x+1).append(" ").append(y+1);
+		System.out.print(sb);
 	}
 }
