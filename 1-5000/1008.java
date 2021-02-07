c585fc05d84efff6d36f916bc13b282f17487014
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-07 23:43:58 +0900

    Update 1008.java

--- 1-5000/1008.java
+++ 1-5000/1008.java
@@ -1,12 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int a = scan.nextInt();
-		int b = scan.nextInt();
+		double answer;
+		answer = Integer.parseInt(st.nextToken());
+		answer /= Integer.parseInt(st.nextToken());
 		
-		System.out.print((double)a/b);
+		System.out.println(answer);
 	}
 }
