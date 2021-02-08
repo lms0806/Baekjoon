6fdec48655a2337765b979c82fb297659d750f9b
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-08 22:09:29 +0900

    Update 2702.java

--- 1-5000/2702.java
+++ 1-5000/2702.java
@@ -1,17 +1,15 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int size = Integer.parseInt(br.readLine());
 		
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < size; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
@@ -25,11 +23,8 @@ public class Main {
 			}
 			max = a * b / min;
 			
-			bw.write(max + " " + min + "\n");
+			sb.append(max).append(" ").append(min).append("\n");
 		}
-		bw.flush();
-
-		br.close();
-		bw.close();
+		System.out.print(sb);
 	}
 }
