a09a99bb326861af390d024329532f483768629f
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-08 22:25:15 +0900

    Update 2747.java

--- 1-5000/2747.java
+++ 1-5000/2747.java
@@ -1,13 +1,10 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int num = Integer.parseInt(br.readLine());
 		
@@ -22,10 +19,6 @@ public class Main {
 			c = 1;
 		}
 		
-		bw.write(c + "\n");
-		bw.flush();
-
-		br.close();
-		bw.close();
+		System.out.println(c);
 	}
 }
