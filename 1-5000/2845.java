7bd7c485cf6e7502b103e0401cadb55db36e89e9
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-09 00:09:15 +0900

    Update 2845.java

--- 1-5000/2845.java
+++ 1-5000/2845.java
@@ -1,16 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int a = scan.nextInt();
-		int b = scan.nextInt();
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
 		int sum = a*b;
 		
+		StringBuilder sb = new StringBuilder();
+		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < 5; i++) {
-			int c = scan.nextInt();
-			System.out.println(c-sum);
+			int c = Integer.parseInt(st.nextToken());
+			sb.append(c-sum).append("\n");
 		}
+		System.out.print(sb);
 	}
 }
