88a346dc1e4e7a9acb0fb9bd44a988518ce54792
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-09 00:06:34 +0900

    Update 2822.java

--- 1-5000/2822.java
+++ 1-5000/2822.java
@@ -1,14 +1,11 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int[] num = new int[8];
 		int[] answer = new int[5];
@@ -34,16 +31,14 @@ public class Main {
 			}
 		}
 		
-		bw.write(sum + "\n");
+		StringBuilder sb = new StringBuilder();
+		sb.append(sum).append("\n");
 		
 		Arrays.sort(answer);
 		
 		for(int i = 0; i < 5; i++) {
-			bw.write(answer[i]+1 + " ");
+			sb.append(answer[i]+1).append(" ");
 		}
-		bw.flush();
-
-		br.close();
-		bw.close();
+		System.out.print(sb);
 	}
 }
