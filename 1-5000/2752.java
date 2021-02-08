03ca2a6e38fd3d88c7a52b43091bdf6c74e7de0c
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-08 22:31:04 +0900

    Update 2752.java

--- 1-5000/2752.java
+++ 1-5000/2752.java
@@ -1,31 +1,26 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int[] num = new int[3];
 		
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < num.length; i++) {
 			num[i] = Integer.parseInt(st.nextToken());
 		}
 		
 		Arrays.sort(num);
 		
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < num.length; i++) {
-			bw.append(num[i] + " ");
+			sb.append(num[i]).append(" ");
 		}
-		bw.flush();
-
-		br.close();
-		bw.close();
+		System.out.println(sb);
 	}
 }
