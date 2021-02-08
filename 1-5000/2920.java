02c70735604380410fc1f3fe9fc0bae536a39adb
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-09 00:21:39 +0900

    Update 2920.java

--- 1-5000/2920.java
+++ 1-5000/2920.java
@@ -1,19 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		int[] num = new int[8];
 		
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < num.length; i++) {
-			num[i] = scan.nextInt();
+			num[i] = Integer.parseInt(st.nextToken());
 		}
 		
 		int count = 0;
 		for(int i = 0; i < num.length; i++) {
 			if(num[i] == i+1) {
 				count++;
+			}
+			else {
+				break;
 			}
 		}
 		
@@ -22,15 +28,21 @@ public class Main {
 			if(num[i] == num.length-i) {
 				count1++;
 			}
+			else {
+				break;
+			}
 		}
+		
+		StringBuilder sb = new StringBuilder();
 		if(count == 8) {
-			System.out.println("ascending");
+			sb.append("ascending");
 		}
 		else if(count1 == 8) {
-			System.out.println("descending");
+			sb.append("descending");
 		}
 		else {
-			System.out.println("mixed");
+			sb.append("mixed");
 		}
+		System.out.print(sb);
 	}
 }
