f20f57a5f96b6372690c4f76dec04e73701a7603
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-08 22:28:23 +0900

    Update 2750.java

--- 1-5000/2750.java
+++ 1-5000/2750.java
@@ -1,21 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int n = scan.nextInt();
+		int n = Integer.parseInt(br.readLine());
 		int[] num = new int[n];
 		
 		for(int i = 0; i < num.length; i++) {
-			num[i] = scan.nextInt();
+			num[i] = Integer.parseInt(br.readLine());
 		}
 		
 		Arrays.sort(num);
 		
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < num.length; i++) {
-			System.out.println(num[i]);
+			sb.append(num[i]).append("\n");
 		}
+		System.out.print(sb);
 	}
 }
