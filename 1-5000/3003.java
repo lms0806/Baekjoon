736a988ca35224a2ca48dc95247bd98aa4dafee0
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-09 00:30:02 +0900

    Update 3003.java

--- 1-5000/3003.java
+++ 1-5000/3003.java
@@ -1,31 +1,20 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int[] pull = {1,1,2,2,2,8};
-		int[] input = new int[pull.length];
-		int[] answer = new int[pull.length];
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < pull.length; i++) {
-			input[i] = Integer.parseInt(st.nextToken());
+			pull[i] -= Integer.parseInt(st.nextToken());
+			sb.append(pull[i]).append(" ");
 		}
-		
-		for(int i = 0; i < pull.length; i++) {
-			answer[i] = pull[i] - input[i];
-			bw.write(answer[i] + " ");
-		}
-		bw.flush();
-
-		br.close();
-		bw.close();
+		System.out.print(sb);
 	}
 }
