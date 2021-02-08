7c8e95a7fafc1258c1e03335c510d9d5806931b2
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-09 00:24:40 +0900

    Update 2953.java

--- 1-5000/2953.java
+++ 1-5000/2953.java
@@ -1,14 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
 		int num = 0, sum = 0;
 		int n = 0;
 		for(int i = 0; i < 5; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int j = 0; j < 4; j++) {
-				sum += scan.nextInt();
+				sum += Integer.parseInt(st.nextToken());
 			}
 			if(sum > num) {
 				n = i+1;
