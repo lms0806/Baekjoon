f9efa6843b7ec74301da5f5c1f1d4859338b21d1
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-09 00:18:43 +0900

    Update 2914.java

--- 1-5000/2914.java
+++ 1-5000/2914.java
@@ -1,14 +1,11 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int a = Integer.parseInt(st.nextToken());
@@ -16,10 +13,6 @@ public class Main {
 		
 		int sum = a * b + 1;
 		
-		bw.write(sum + "\n");
-		bw.flush();
-
-		br.close();
-		bw.close();
+		System.out.print(sum);
 	}
 }
