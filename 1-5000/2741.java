6f4cb4d13798d748d9fb2a9a0a3398542404772b
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-08 22:18:02 +0900

    Create 2741.java

---
+++ 1-5000/2741.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int a = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 1; i <= a; i++) {
+			sb.append(i).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
