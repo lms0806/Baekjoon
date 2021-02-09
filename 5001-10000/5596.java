c41e91e6b849fdf5aa9d18fdb4465c11102604e2
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-09 20:07:51 +0900

    Update 5596.java

--- 5001-10000/5596.java
+++ 5001-10000/5596.java
@@ -1,23 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		String str = scan.nextLine();
-		String str1 = scan.nextLine();
+		int num = 0, num1 = 0;
 		
-		String[] a = str.split(" ");
-		String[] b = str1.split(" ");
+		for(int i = 0; i < 4; i++) {
+			num += Integer.parseInt(st.nextToken());
+		}
 		
-		int num = Integer.parseInt(a[0]) + Integer.parseInt(a[1]) + Integer.parseInt(a[2]) + Integer.parseInt(a[3]);
-		int num1 = Integer.parseInt(b[0]) + Integer.parseInt(b[1]) + Integer.parseInt(b[2]) + Integer.parseInt(b[3]);
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < 4; i++) {
+			num1 += Integer.parseInt(st.nextToken());
+		}
 		
+		StringBuilder sb = new StringBuilder();
 		if(num >= num1) {
-			System.out.println(num);
+			sb.append(num);
 		}
 		else {
-			System.out.println(num1);
+			sb.append(num1);
 		}
+		System.out.print(sb);
 	}
 }
