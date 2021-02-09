2231533f18c1ec70f48636c001b615a455329e59
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-09 19:56:24 +0900

    Update 5575.java

--- 5001-10000/5575.java
+++ 5001-10000/5575.java
@@ -1,26 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+
 		int h = 0, m = 0, s = 0;
 		int num = 0;
 		
 		int result = 0;
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < 3; i++) {
-			h = scan.nextInt();
-			m = scan.nextInt();
-			s = scan.nextInt();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			h = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+			s = Integer.parseInt(st.nextToken());
 			num = h * 3600 + m * 60 + s;
 			
-			h = scan.nextInt();
-			m = scan.nextInt();
-			s = scan.nextInt();
-			result = h * 3600 + m * 60 + s;
-			result -= num;
+			h = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+			s = Integer.parseInt(st.nextToken());
+			result = h * 3600 + m * 60 + s - num;
 			
-			System.out.println(result/3600 + " " + (result%3600)/60 + " " + result%60);
-		}	
+			sb.append(result/3600).append(" ").append((result%3600)/60).append(" ").append(result%60).append("\n");
+		}
+		System.out.print(sb);
 	}
 }
