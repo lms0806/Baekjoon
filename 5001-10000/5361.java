85f603571758b11ec0efec9be1ccbc6115e80907
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-09 19:02:54 +0900

    Update 5361.java

--- 5001-10000/5361.java
+++ 5001-10000/5361.java
@@ -1,19 +1,26 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int size = scan.nextInt();
+		int size = Integer.parseInt(br.readLine());
+		
 		double[] num = {350.34, 230.90, 190.55, 125.30, 180.90};
 		
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < size; i++) {
 			double sum = 0;
+			StringTokenizer st = new StringTokenizer(br.readLine()," ");
 			for(int j = 0; j < 5; j++) {
-				int a = scan.nextInt();
+				int a = Integer.parseInt(st.nextToken());
 				sum += a * num[j];
 			}
-			System.out.println("$" + String.format("%.2f", sum));
+			sb.append("$").append(String.format("%.2f", sum)).append("\n");
 		}
+		System.out.print(sb);
 	}
 }
