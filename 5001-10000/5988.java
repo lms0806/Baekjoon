63e30a42bef6324c64bb2aaa1a05a26f541f082e
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-09 20:21:49 +0900

    Update 5988.java

--- 5001-10000/5988.java
+++ 5001-10000/5988.java
@@ -1,29 +1,24 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int size = Integer.parseInt(br.readLine());
 		
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < size; i++) {
-			String[] strs = br.readLine().split("");
+			String str = br.readLine();
 			
-			if(Integer.parseInt(strs[strs.length-1])%2 != 0) {
-				bw.write("odd\n");
+			if((str.charAt(str.length()-1)-'0')%2 != 0) {
+				sb.append("odd").append("\n");
 			}
 			else {
-				bw.write("even\n");
+				sb.append("even").append("\n");
 			}
 		}
-		bw.flush();
-
-		br.close();
-		bw.close();
+		System.out.print(sb);
 	}
 }
