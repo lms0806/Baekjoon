10107d4d2d396f217ee3669ebc4110b2bf4b5d26
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-09 19:44:59 +0900

    Update 5532.java

--- 5001-10000/5532.java
+++ 5001-10000/5532.java
@@ -1,16 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int l = scan.nextInt();
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		int d = scan.nextInt();
+		int l = Integer.parseInt(br.readLine());
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		int c = Integer.parseInt(br.readLine());
+		int d = Integer.parseInt(br.readLine());
 		
-		int num1 = 0, num2 = 0;
+		int num1 = 0;
+		int num2 = 0;
 		int result = 0;
 		
 		if(a%c != 0) {
