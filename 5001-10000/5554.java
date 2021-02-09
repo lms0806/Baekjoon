926e14a953649435bcfa4c50546691ad74a1d646
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-09 19:52:29 +0900

    Update 5554.java

--- 5001-10000/5554.java
+++ 5001-10000/5554.java
@@ -1,16 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+
 		int sum = 0;
 		for(int i = 0; i < 4; i++) {
-			int num = scan.nextInt();
-			sum += num;
+			sum += Integer.parseInt(br.readLine());
 		}
 		
-		System.out.println(sum/60);
-		System.out.println(sum%60);
+		StringBuilder sb = new StringBuilder();
+		sb.append(sum/60).append("\n").append(sum%60);
+		System.out.print(sb);
 	}
 }
