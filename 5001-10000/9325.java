18ad1b020460c4a1b2b00a67de36379fc31d26e3
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-10 00:43:38 +0900

    Update 9325.java

--- 5001-10000/9325.java
+++ 5001-10000/9325.java
@@ -1,21 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int num = scan.nextInt();
+		int num = Integer.parseInt(br.readLine());
 		
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < num; i++) {
-			int sum = scan.nextInt();
-			int number = scan.nextInt();
+			int sum = Integer.parseInt(br.readLine());
+			int number = Integer.parseInt(br.readLine());
 			for(int j = 0; j < number; j++) {
-				int q = scan.nextInt();
-				int p = scan.nextInt();
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int q = Integer.parseInt(st.nextToken());
+				int p = Integer.parseInt(st.nextToken());
 				
 				sum += q*p;
 			}
-			System.out.println(sum);
+			sb.append(sum).append("\n");
 		}
+		System.out.print(sb);
 	}
 }
