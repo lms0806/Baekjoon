57212f9e71e8e18e265a1b1f8c77532983e8905a
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-10 00:39:45 +0900

    Update 9295.java

--- 5001-10000/9295.java
+++ 5001-10000/9295.java
@@ -1,18 +1,22 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int size = scan.nextInt();
+		int size = Integer.parseInt(br.readLine());
 		
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < size; i++) {
-			int a = scan.nextInt();
-			int b = scan.nextInt();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 			
-			int sum = a + b;
-			
-			System.out.println("Case " + (i+1) + ": " + sum);
+			sb.append("Case ").append(i+1).append(": ").append(a+b).append("\n");
 		}
+		System.out.print(sb);
 	}
 }
