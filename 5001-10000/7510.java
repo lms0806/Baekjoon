d87ba9d49746eb31c237f1588eb1f02cabd271b2
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-10 00:17:52 +0900

    Update 7510.java

--- 5001-10000/7510.java
+++ 5001-10000/7510.java
@@ -1,39 +1,35 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int n = Integer.parseInt(br.readLine());
 		int[] num = new int[3];
 		
+        StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int j = 0; j < 3; j++) {
 				num[j] = Integer.parseInt(st.nextToken());
 			}
+			
 			Arrays.sort(num);
 			
-			bw.append("Scenario #" + (i+1) + ":\n");
+			sb.append("Scenario #").append(i+1).append(":\n");
 			
 			if(num[0]*num[0] + num[1]*num[1] == num[2]*num[2]) {
-				bw.append("yes\n\n");
+				sb.append("yes");
 			}
 			else {
-				bw.append("no\n\n");
+				sb.append("no");
 			}
+            sb.append("\n").append("\n");
 		}
-		
-		bw.flush();
-
-		br.close();
-		bw.close();
+		System.out.print(sb);
 	}
 }
