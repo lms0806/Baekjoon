fbd6374ff7c0dd55101b17cb8bf4274c47b710f2
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-10 00:28:14 +0900

    Update 8958.java

--- 5001-10000/8958.java
+++ 5001-10000/8958.java
@@ -1,20 +1,22 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int number = scan.nextInt();
+		int number = Integer.parseInt(br.readLine());
 		
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < number; i++) {
-			String str = scan.next();
-			String[] strs = str.split("");
-			int[] num = new int[strs.length];
+			String str = br.readLine();
+			int[] num = new int[str.length()];
 			int sum = 0;
 			int count = 1;
 			
-			for(int j = 0; j < strs.length; j++) {
-				if(strs[j].equals("O")) {
+			for(int j = 0; j < str.length(); j++) {
+				if(str.charAt(j) == 'O') {
 					num[j] += count;
 					count++;
 				}
@@ -26,7 +28,8 @@ public class Main {
 			for(int j = 0; j < num.length; j++) {
 				sum += num[j];
 			}
-			System.out.println(sum);
+			sb.append(sum).append("\n");
 		}
+		System.out.print(sb);
 	}
 }
