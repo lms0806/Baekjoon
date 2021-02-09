c12ee467b843728b017cff157732a4b2380444a7
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-09 20:12:46 +0900

    Update 5618.java

--- 5001-10000/5618.java
+++ 5001-10000/5618.java
@@ -1,15 +1,12 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int n = Integer.parseInt(br.readLine());
 		int[] num = new int[n];
@@ -21,6 +18,7 @@ public class Main {
 		
 		Arrays.sort(num);
 		
+		StringBuilder sb = new StringBuilder();
 		for(int i = 1; i <= num[0]; i++) {
 			int count = 0;
 			for(int j = 0; j < n; j++) {
@@ -32,13 +30,9 @@ public class Main {
 				}
 			}
 			if(count == n) {
-				bw.append(i + "\n");
+				sb.append(i).append("\n");
 			}
 		}
-		
-		bw.flush();
-
-		br.close();
-		bw.close();
+		System.out.print(sb);
 	}
 }
