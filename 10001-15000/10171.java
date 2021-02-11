96e73caaeca95c9e54e5646aaa3d5cf333736b01
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-11 20:43:30 +0900

    Update 10171.java

--- 10001-15000/10171.java
+++ 10001-15000/10171.java
@@ -1,9 +1,10 @@
 public class Main {
-
 	public static void main(String[] args) {
-		System.out.println("\\    /\\");
-		System.out.println(" )  ( \')");
-		System.out.println("(  /  )");
-		System.out.println(" \\(__)|");
+		StringBuilder sb = new StringBuilder();
+		sb.append("\\    /\\").append("\n");
+		sb.append(" )  ( \')").append("\n");
+		sb.append("(  /  )").append("\n");
+		sb.append(" \\(__)|");
+		System.out.print(sb);
 	}
 }
