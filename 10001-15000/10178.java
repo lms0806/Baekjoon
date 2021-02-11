375841d140c515d9497e7e5eff82d07660a158f0
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-11 20:47:12 +0900

    Update 10178.java

--- 10001-15000/10178.java
+++ 10001-15000/10178.java
@@ -1,19 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int size = scan.nextInt();
+		int size = Integer.parseInt(br.readLine());
 		
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < size; i++) {
-			int a = scan.nextInt();
-			int b = scan.nextInt();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 			
 			int num = a/b;
 			int num1 = a%b;
 			
-			System.out.println("You get " + num + " piece(s) and your dad gets " + num1 + " piece(s).");
+			sb.append("You get ").append(num).append(" piece(s) and your dad gets ").append(num1).append(" piece(s).").append("\n");
 		}
+		System.out.print(sb);
 	}
 }
