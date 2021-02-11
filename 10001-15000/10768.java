f68f96fd404a303db52c5b977a82137e7138ad4b
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-11 21:17:05 +0900

    Update 10768.java

--- 10001-15000/10768.java
+++ 10001-15000/10768.java
@@ -1,22 +1,26 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int a = scan.nextInt();
-		int b = scan.nextInt();
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
 		
 		int num = a * 30 + b;
 		
+		StringBuilder sb = new StringBuilder();
 		if(num == 78) {
-			System.out.println("Special");
+			sb.append("Special");
 		}
 		else if(num > 78) {
-			System.out.println("After");
+			sb.append("After");
 		}
 		else {
-			System.out.println("Before");
+			sb.append("Before");
 		}
+		System.out.print(sb);
 	}
 }
