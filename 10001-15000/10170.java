9e5db593e8f2d03de24c473a867033d9967f19ef
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-11 20:42:22 +0900

    Update 10170.java

--- 10001-15000/10170.java
+++ 10001-15000/10170.java
@@ -1,17 +1,19 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("NFC West       W   L  T");
-		System.out.println("-----------------------");
-		System.out.println("Seattle        13  3  0");
-		System.out.println("San Francisco  12  4  0");
-		System.out.println("Arizona        10  6  0");
-		System.out.println("St. Louis      7   9  0\n");
+		StringBuilder sb = new StringBuilder();
+		sb.append("NFC West       W   L  T").append("\n");
+		sb.append("-----------------------").append("\n");
+		sb.append("Seattle        13  3  0").append("\n");
+		sb.append("San Francisco  12  4  0").append("\n");
+		sb.append("Arizona        10  6  0").append("\n");
+		sb.append("St. Louis      7   9  0\n").append("\n");
 
-		System.out.println("NFC North      W   L  T");
-		System.out.println("-----------------------");
-		System.out.println("Green Bay      8   7  1");
-		System.out.println("Chicago        8   8  0");
-		System.out.println("Detroit        7   9  0");
-		System.out.println("Minnesota      5  10  1");
+		sb.append("NFC North      W   L  T").append("\n");
+		sb.append("-----------------------").append("\n");
+		sb.append("Green Bay      8   7  1").append("\n");
+		sb.append("Chicago        8   8  0").append("\n");
+		sb.append("Detroit        7   9  0").append("\n");
+		sb.append("Minnesota      5  10  1");
+		System.out.print(sb);
 	}
 }
