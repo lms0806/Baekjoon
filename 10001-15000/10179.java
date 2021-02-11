7848cdbdbad07c72763dad25d33a59bea2d5a152
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-11 20:48:50 +0900

    Update 10179.java

--- 10001-15000/10179.java
+++ 10001-15000/10179.java
@@ -1,16 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int number = scan.nextInt();
+		int number = Integer.parseInt(br.readLine());
 		
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < number; i++) {
-			double a = scan.nextDouble();
+			double a = Double.parseDouble(br.readLine());
 			
-			double sum = a*0.8;
-			System.out.println("$" + String.format("%.2f", sum));
+			sb.append("$").append(String.format("%.2f", a*0.8)).append("\n");
 		}
+		System.out.print(sb);
 	}
 }
