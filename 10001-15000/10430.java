ef48012885e2bad29dd3b61cb307499632426f3d
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-11 21:05:50 +0900

    Update 10430.java

--- 10001-15000/10430.java
+++ 10001-15000/10430.java
@@ -1,16 +1,22 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException,IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
 		
-		System.out.println((a+b)%c);
-		System.out.println(((a%c) + (b%c))%c);
-		System.out.println((a*b%c));
-		System.out.println(((a%c)*(b%c))%c);
+        	StringBuilder sb = new StringBuilder();
+		sb.append((a + b) % c).append("\n");
+		sb.append(((a % c) + (b % c)) % c).append("\n");
+		sb.append((a * b) % c).append("\n");
+		sb.append(((a % c) * (b % c)) %c);
+		System.out.print(sb);
 	}
 }
