9086aca180a5eca11d319d57534a6ce6acdce065
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-11 22:06:37 +0900

    Update 10953.java

--- 10001-15000/10953.java
+++ 10001-15000/10953.java
@@ -1,28 +1,22 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
 		int size = Integer.parseInt(br.readLine());
 		
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < size; i++) {
 			int sum = 0;
 			String[] str = br.readLine().split(",");
 			for(int j = 0; j < str.length; j++) {
 				sum += Integer.parseInt(str[j]);
 			}
-			bw.write(sum + "\n");
+			sb.append(sum).append("\n");
 		}
-		
-		bw.flush();
-
-		br.close();
-		bw.close();
+		System.out.print(sb);
 	}
 }
