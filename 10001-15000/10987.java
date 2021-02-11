50be515d7d571c3762e7e53773a92e60dbf8e92c
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-11 22:09:51 +0900

    Update 10987.java

--- 10001-15000/10987.java
+++ 10001-15000/10987.java
@@ -1,18 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		String str = scan.nextLine();
-		String[] s = str.split("");
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+
+		String str = br.readLine();
 		int n = 0;
 		
-		for(int i = 0; i < s.length; i++) {
-			if(s[i].equals("a") || s[i].equals("e") || s[i].equals("i") || s[i].equals("o") || s[i].equals("u")) {
+		for(int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
 				n++;
 			}
 		}
-		System.out.println(n);
+		System.out.print(n);
 	}
 }
