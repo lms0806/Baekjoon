c8e879af7a0ea2dc15f32c87d3802cade0e2634e
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-11 21:52:44 +0900

    Update 10871.java

--- 10001-15000/10871.java
+++ 10001-15000/10871.java
@@ -1,22 +1,29 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int n = scan.nextInt();
-		int a = scan.nextInt();
+		int n = Integer.parseInt(st.nextToken());
+		int a = Integer.parseInt(st.nextToken());
 		
 		int[] num = new int[n];
 		
+		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < num.length; i++) {
-			num[i] = scan.nextInt();
+			num[i] = Integer.parseInt(st.nextToken());
 		}
 		
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < num.length; i++) {
 			if(num[i] < a) {
-				System.out.print(num[i] + " ");
+				sb.append(num[i]).append(" ");
 			}
 		}
+		System.out.print(sb);
 	}
 }
