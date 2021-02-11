1de760daeb9127f86ec48e6d811cfe6fc2921362
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-11 21:54:16 +0900

    Update 10872.java

--- 10001-15000/10872.java
+++ 10001-15000/10872.java
@@ -1,16 +1,17 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int n = scan.nextInt();
+		int n = Integer.parseInt(br.readLine());
 		
 		int num = 1;
 		for(int i = 1; i <= n; i++) {
 			num*=i;
 		}
-		System.out.println(num);
+		System.out.print(num);
 	}
 }
