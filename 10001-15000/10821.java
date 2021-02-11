9f275f32bd781b8586d8808a9612e5a4d9e7d77a
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-11 21:31:51 +0900

    Update 10821.java

--- 10001-15000/10821.java
+++ 10001-15000/10821.java
@@ -1,21 +1,13 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		String[] str = br.readLine().split(",");
 		
-		bw.write(str.length + "\n");
-		
-		bw.flush();
-
-		br.close();
-		bw.close();
+		System.out.println(str.length);
 	}
 }
