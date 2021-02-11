cbae8ae40f7c252205f190cfe7835fe31e87d508
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-11 21:07:21 +0900

    Update 10474.java

--- 10001-15000/10474.java
+++ 10001-15000/10474.java
@@ -1,15 +1,13 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
+		StringBuilder sb = new StringBuilder();
 		while(true) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
@@ -21,12 +19,8 @@ public class Main {
 			int num = a/b;
 			int num1 = a%b;
 			
-			bw.write(num + " " + num1 + " / " + b + "\n");
+			sb.append(num).append(" ").append(num1).append(" / ").append(b).append("\n");
 		}
-		
-		bw.flush();
-
-		br.close();
-		bw.close();
+		System.out.print(sb);
 	}
 }
