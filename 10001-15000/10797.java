72bf917a99b7f398643f0db269c3ffcb3fab071b
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-11 21:18:48 +0900

    Update 10797.java

--- 10001-15000/10797.java
+++ 10001-15000/10797.java
@@ -1,20 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int a = scan.nextInt();
+		int a = Integer.parseInt(br.readLine());
 		int[] num = new int[5];
 		
 		int result = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < num.length; i++) {
-			num[i] = scan.nextInt();
+			num[i] = Integer.parseInt(st.nextToken());
 			if(num[i] == a) {
 				result++;
 			}
 		}
-		
-		System.out.println(result);
+		System.out.print(result);
 	}
 }
