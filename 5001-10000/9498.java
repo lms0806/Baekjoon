8c842bfcb093e90f7bb8961b4448161b4f47a944
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-11 18:52:31 +0900

    Update 9498.java

--- 5001-10000/9498.java
+++ 5001-10000/9498.java
@@ -1,25 +1,29 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException,IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int a = scan.nextInt();
-		
-		if(a >= 90) {
-			System.out.print("A");
+		int a = Integer.parseInt(br.readLine());
+        
+		StringBuilder sb = new StringBuilder();
+		if(a > 89) {
+            		sb.append("A");
 		}
-		else if(a >= 80) {
-			System.out.print("B");
+		else if(a > 79 && a < 90) {
+            		sb.append("B");
 		}
-		else if(a >= 70) {
-			System.out.print("C");
+		else if(a > 69 && a < 80) {
+            		sb.append("C");
 		}
-		else if(a >= 60) {
-			System.out.print("D");
+		else if(a > 59 && a < 70) {
+            		sb.append("D");
 		}
 		else {
-			System.out.print("F");
+            		sb.append("F");
 		}
+        	System.out.print(sb);
 	}
 }
