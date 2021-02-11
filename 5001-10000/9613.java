d51e5d125d0b8c6bf80d358f1594df4fb3270e78
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-11 19:02:29 +0900

    Update 9613.java

--- 5001-10000/9613.java
+++ 5001-10000/9613.java
@@ -9,10 +9,13 @@ public class Main {
 		
 		int size = Integer.parseInt(br.readLine());
 		
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < size; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
+			
 			int num = Integer.parseInt(st.nextToken());
 			int[] number = new int[num];
+			
 			for(int j = 0; j < num; j++) {
 				number[j] = Integer.parseInt(st.nextToken());
 			}
@@ -23,8 +26,9 @@ public class Main {
 					sum += gcd(number[j],number[k]);
 				}
 			}
-			System.out.println(sum);
+			sb.append(sum).append("\n");
 		}
+		System.out.print(sb);
 	}
 	
 	public static int gcd(int a, int b) {
