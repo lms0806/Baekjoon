0790950ab7ef9b1b6f5687a9edf8cdede17f58de
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-11 21:21:01 +0900

    Update 10807.java

--- 10001-15000/10807.java
+++ 10001-15000/10807.java
@@ -1,16 +1,12 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
 		int n = Integer.parseInt(br.readLine());
 		
 		int[] num = new int[n];
@@ -29,10 +25,6 @@ public class Main {
 			}
 		}
 		
-		bw.write(count + "\n");
-		bw.flush();
-
-		br.close();
-		bw.close();
+		System.out.println(count);
 	}
 }
