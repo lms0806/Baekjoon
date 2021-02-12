4421957d293d3ab8947fa6a7dfc525f513a2ba4e
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-12 21:37:33 +0900

    Update 13706.java

--- 10001-15000/13706.java
+++ 10001-15000/13706.java
@@ -7,8 +7,6 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		BigInteger a = new BigInteger(br.readLine());
-		
-		System.out.println(a.sqrt());
+		System.out.print(new BigInteger(br.readLine()).sqrt());
 	}
 }
