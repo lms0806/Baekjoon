b966bbe7cd48dd0f4d7b026392bea7d774785d82
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-13 00:52:00 +0900

    Update 15680.java

--- 15001-20000/15680.java
+++ 15001-20000/15680.java
@@ -1,16 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int a = scan.nextInt();
+		int a = Integer.parseInt(br.readLine());
 		
-		if(a == 0) {
-			System.out.println("YONSEI");
-		}
-		else if(a == 1) {
-			System.out.println("Leading the Way to the Future");
-		}
+		System.out.print(a == 0 ? "YONSEI" : "Leading the Way to the Future");
 	}
 }
