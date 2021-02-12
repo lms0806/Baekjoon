645c5ee1ea45c3ed5313aa93f23b73aa2b65daeb
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-13 02:06:39 +0900

    Update 18096.java

--- 15001-20000/18096.java
+++ 15001-20000/18096.java
@@ -1,13 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-	
-		int size = Integer.parseInt(br.readLine());
-		
-		System.out.println(1);
+	public static void main(String[] args) {		
+		System.out.print(1);
 	}
 }
