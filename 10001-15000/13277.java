e881a78720c3118d874497234e5f15d0630fcbc6
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-12 21:29:00 +0900

    Update 13277.java

--- 10001-15000/13277.java
+++ 10001-15000/13277.java
@@ -2,13 +2,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-	    
-		String[] arr = br.readLine().split(" "); 
-	    
-		System.out.println(new BigInteger(arr[0]).multiply(new BigInteger(arr[1])));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		System.out.print(new BigInteger(st.nextToken()).multiply(new BigInteger(st.nextToken())));
 	}
 }
