1f8b05552ee2c0cc5f15ab1dafcc06268964762c
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-13 01:36:41 +0900

    Update 16486.java

--- 15001-20000/16486.java
+++ 15001-20000/16486.java
@@ -1,14 +1,11 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		
-		double sum = a*2 + b*2*3.141592;
-		
-		System.out.println(sum);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+
+		System.out.println(Integer.parseInt(br.readLine()) * 2 + Integer.parseInt(br.readLine()) * 2 * 3.141592);
 	}
 }
