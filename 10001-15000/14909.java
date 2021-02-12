8e923cfae2661184218894a9ef3945b4086eb879
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-13 00:35:16 +0900

    Update 14909.java

--- 10001-15000/14909.java
+++ 10001-15000/14909.java
@@ -1,29 +1,22 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		String[] strs = br.readLine().split(" ");
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int count = 0;
-		for(int i = 0; i < strs.length; i++) {
-			int num = Integer.parseInt(strs[i]);
+		while(st.hasMoreTokens()) {
+			int num = Integer.parseInt(st.nextToken());
 			
 			if(num > 0) {
 				count++;
 			}
 		}
 		
-		bw.write(count + "\n");
-		bw.flush();
-
-		br.close();
-		bw.close();
+		System.out.println(count);
 	}
 }
