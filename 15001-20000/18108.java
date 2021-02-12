2947c061bbd76338f8ee84a0a098c5c2e8efbbdd
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-13 02:07:53 +0900

    Update 18108.java

--- 15001-20000/18108.java
+++ 15001-20000/18108.java
@@ -1,11 +1,11 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int a = scan.nextInt();
-		
-		System.out.println(a-543);
+		System.out.println(Integer.parseInt(br.readLine()) - 543);
 	}
 }
