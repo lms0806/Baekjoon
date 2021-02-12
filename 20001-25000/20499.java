bc63cbddef3e3a32d346d8e49eb229720b768e6d
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-13 02:33:25 +0900

    Update 20499.java

--- 20001-25000/20499.java
+++ 20001-25000/20499.java
@@ -1,22 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		String str = scan.next();
+		String str = br.readLine();
 		String[] strs = str.split("/");
+		
 		int[] num = new int[strs.length];
 		
 		for(int i = 0; i < strs.length; i++) {
 			num[i] = Integer.parseInt(strs[i]);
 		}
 		
-		if(num[0] + num[2] < num[1] || num[1] == 0) {
-			System.out.println("hasu");
-		}
-		else {
-			System.out.println("gosu");
-		}
+		System.out.print(num[0] + num[2] < num[1] || num[1] == 0 ? "hasu" : "gosu");
 	}
 }
