5cff9958978e6ed0e7c8552f0cc9766d629b4232
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-12 21:06:46 +0900

    Update 12756.java

--- 10001-15000/12756.java
+++ 10001-15000/12756.java
@@ -20,25 +20,27 @@ public class Main {
 			b[i] = Integer.parseInt(st.nextToken());
 		}
 		
+		StringBuilder sb = new StringBuilder(0);
 		while(true) {
 			a[1] -= b[0];
 			b[1] -= a[0];
 			
 			if(b[1] <= 0 && a[1] > 0) {
-				System.out.println("PLAYER A");
+				sb.append("PLAYER A");
 				break;
 			}
 			else if(a[1] <= 0 && b[1] > 0) {
-				System.out.println("PLAYER B");
+				sb.append("PLAYER B");
 				break;
 			}
 			else if(a[1] > 0 && a[1] > 0) {
 				continue;
 			}
 			else {
-				System.out.println("DRAW");
+				sb.append("DRAW");
 				break;
 			}
 		}
+		System.out.print(sb);
 	}
 }
