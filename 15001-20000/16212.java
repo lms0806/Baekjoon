508e60574473ea48a412b3a4dcb70d91b619c07f
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-13 01:24:08 +0900

    Update 16212.java

--- 15001-20000/16212.java
+++ 15001-20000/16212.java
@@ -1,28 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 	
-		int size = Integer.parseInt(br.readLine());
-		
-		int[] num = new int[size];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < size; i++) {
-			num[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(num);
-		
-		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < size; i++) {
-			sb.append(num[i]).append(" ");
-		}
-		System.out.println(sb);
+        	ArrayList<Integer> arr = new ArrayList<>();
+
+        	int num = Integer.parseInt(br.readLine());
+        	StringTokenizer st = new StringTokenizer(br.readLine());
+        	while(num-->0){
+            		arr.add(Integer.parseInt(st.nextToken()));
+        	}
+        	Collections.sort(arr);
+
+        	StringBuilder sb = new StringBuilder();
+        	for(Integer i : arr){
+            		sb.append(i).append(" ");
+        	}
+        	System.out.print(sb);
 	}
 }
