eacca4c97955bface4534a95f424c3ccae3708a4
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-12 21:10:33 +0900

    Update 12790.java

--- 10001-15000/12790.java
+++ 10001-15000/12790.java
@@ -1,36 +1,39 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int num = scan.nextInt();
+		int num = Integer.parseInt(br.readLine());
 		
 		int[] a = new int[4];
-		int[] b = new int[4];
 		
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < num; i++) {
-			int sum = 0;
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			
 			for(int j = 0; j < 4; j++) {
-				a[j] = scan.nextInt();
+				a[j] = Integer.parseInt(st.nextToken());
 			}
 			for(int j = 0; j < 4; j++) {
-				b[j] = scan.nextInt() + a[j];
+				a[j] += Integer.parseInt(st.nextToken());
 			}
 			
-			if(b[0] < 1) {
-				b[0] = 1;
+			if(a[0] < 1) {
+				a[0] = 1;
 			}
-			if(b[1] < 1) {
-				b[1] = 1;
+			if(a[1] < 1) {
+				a[1] = 1;
 			}
-			if(b[2] < 0) {
-				b[2] = 0;
+			if(a[2] < 0) {
+				a[2] = 0;
 			}
 			
-			sum = b[0] + 5*b[1] + 2*b[2] + 2*b[3];
-			System.out.println(sum);
+			sb.append(a[0] + 5*a[1] + 2*a[2] + 2*a[3]).append("\n");
 		}
+		System.out.print(sb);
 	}
 }
