b496257f416612bab425e3f7eeec0529322c80de
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-12 21:01:48 +0900

    Update 12096.java

--- 10001-15000/12096.java
+++ 10001-15000/12096.java
@@ -1,14 +1,5 @@
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		bw.write("02-521-0487");
-		bw.flush();
-        
-		bw.close();
+	public static void main(String[] args) {
+		System.out.print("02-521-0487");
 	}
 }
