340fc019df657d185aa72ff07e3192978fd8e9f4
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-13 02:13:20 +0900

    Update 18301.java

--- 15001-20000/18301.java
+++ 15001-20000/18301.java
@@ -1,15 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		
-		int result = ((a+1) * (b+1)) / (c+1) -1;
-		
-		System.out.println(result);
+		System.out.println(((Integer.parseInt(st.nextToken()) + 1) * (Integer.parseInt(st.nextToken()) + 1)) / (Integer.parseInt(st.nextToken()) + 1) - 1);
 	}
 }
