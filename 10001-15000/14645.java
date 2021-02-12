3d84913e3747a054dd8d2f7868622a6e0a388be1
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-13 00:22:56 +0900

    Update 14645.java

--- 10001-15000/14645.java
+++ 10001-15000/14645.java
@@ -1,17 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int a = scan.nextInt();
-		int b = scan.nextInt();
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
 		
+		int c, d;
 		for(int i = 0; i < a; i++) {
-			int c = scan.nextInt();
-			int d = scan.nextInt();
+			st = new StringTokenizer(br.readLine());
+			c = Integer.parseInt(st.nextToken());
+			d = Integer.parseInt(st.nextToken());
 		}
 		
-		System.out.println("비와이");
+		System.out.print("비와이");
 	}
 }
