46e214099dc29a1ef00e16dd860ed02a180a5a6b
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-13 01:13:50 +0900

    Update 15953.java

--- 15001-20000/15953.java
+++ 15001-20000/15953.java
@@ -1,42 +1,51 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
 		int[][] str17 = {{5000000,1},{3000000,2},{2000000,3},{500000,4},{300000,5},{100000,6}};
 		int[][] str18 = {{5120000,1},{2560000,2},{1280000,4},{640000,8},{320000,16}};
 		
-		int num = scan.nextInt();
+		int num = Integer.parseInt(br.readLine());
 		
-		for(int i = 0; i < num; i++) {
+		int a, b;
+		StringBuilder sb = new StringBuilder();
+		while(num-->0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			int count = 0, sum = 0;
-			int a = scan.nextInt();
-			int b = scan.nextInt();
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
 			while(a != 0) {
 				if(count == str17.length) {
 					break;
 				}
-				a-=str17[count][1];
+				a -= str17[count][1];
 				if(a <= 0) {
 					sum+=str17[count][0];
 					break;
 				}
 				count++;
 			}
+			
 			count = 0;
+			
 			while(b != 0) {
 				if(count == str18.length) {
 					break;
 				}
-				b-=str18[count][1];
+				b -= str18[count][1];
 				if(b <= 0) {
 					sum += str18[count][0];
 					break;
 				}
 				count++;
 			}
-			System.out.println(sum);
+			sb.append(sum).append("\n");
 		}
+		System.out.print(sb);
 	}
 }
