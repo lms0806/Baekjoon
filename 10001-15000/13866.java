166fa8affdbda434a379f1f0b2c75d348462c7b2
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-12 21:40:55 +0900

    Update 13866.java

--- 10001-15000/13866.java
+++ 10001-15000/13866.java
@@ -1,16 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		int d = scan.nextInt();
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
 		
-		int sum = Math.abs((a+d) - (b+c));
-		
-		System.out.println(sum);
+		System.out.print(Math.abs((a+d) - (b+c)));
 	}
 }
