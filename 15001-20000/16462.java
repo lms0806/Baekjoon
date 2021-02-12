c6763431504d3c9beb663c825b643bbcf750d989
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-13 01:35:03 +0900

    Update 16462.java

--- 15001-20000/16462.java
+++ 15001-20000/16462.java
@@ -1,28 +1,22 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int size = Integer.parseInt(br.readLine());
 		
 		double sum = 0;
 		for(int i = 0; i < size; i++) {
-			String[] strs = br.readLine().split("");
+			String str = br.readLine();
 			
-			String str = "";
-			for(int j = 0; j < strs.length; j++) {
-				if(strs[j].equals("0") || strs[j].equals("6")) {
-					strs[j] = "9";
-				}
-				str+=strs[j];
+			String answer = "";
+			for(int j = 0; j < str.length(); j++) {
+				answer += str.charAt(j) == '0' || str.charAt(j) == '6' ? "9" : str.charAt(j);
 			}
-			double num = Integer.parseInt(str);
+			double num = Double.parseDouble(answer);
 			if(num > 100) {
 				num = 100;
 			}
@@ -34,10 +28,6 @@ public class Main {
 			sum++;
 		}
 		
-		bw.write((int)sum+"\n");
-		bw.flush();
-
-		br.close();
-		bw.close();
+		System.out.print((int)sum);
 	}
 }
