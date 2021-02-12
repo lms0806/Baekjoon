a6b64aa6697d523a6c89fe35be92895d325724f9
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-13 00:41:08 +0900

    Update 14915.java

--- 10001-15000/14915.java
+++ 10001-15000/14915.java
@@ -1,57 +1,51 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int m = Integer.parseInt(st.nextToken());
 		int n = Integer.parseInt(st.nextToken());
 		
+		StringBuilder sb = new StringBuilder();
 		String str = "";
 		if(m == 0) {
-			bw.write("0\n");
+			sb.append(0);
 		}
-		while(m != 0) {
-			if(m%n == 10) {
-				str+="A";
+		else {
+			while(m != 0) {
+				if(m%n == 10) {
+					str += "A";
+				}
+				else if(m%n == 11) {
+					str += "B";
+				}
+				else if(m%n == 12) {
+					str += "C";
+				}
+				else if(m%n == 13) {
+					str += "D";
+				}
+				else if(m%n == 14) {
+					str += "E";
+				}
+				else if(m%n == 15) {
+					str += "F";
+				}
+				else {
+					str += Integer.toString(m%n);
+				}
+				m/=n;
 			}
-			else if(m%n == 11) {
-				str+="B";
-			}
-			else if(m%n == 12) {
-				str+="C";
-			}
-			else if(m%n == 13) {
-				str+="D";
-			}
-			else if(m%n == 14) {
-				str+="E";
-			}
-			else if(m%n == 15) {
-				str+="F";
-			}
-			else {
-				str+= Integer.toString(m%n);
-			}
-			m/=n;
 		}
-		String[] strs = str.split("");
-		str = "";
 		
-		for(int i = strs.length - 1; i >= 0; i--) {
-			str += strs[i];
+		for(int i = str.length() - 1; i >= 0; i--) {
+			sb.append(str.charAt(i));
 		}
-		bw.write(str+"\n");
-		bw.flush();
-
-		br.close();
-		bw.close();
+		System.out.print(sb);
 	}
 }
