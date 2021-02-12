d39713a611bf830157c44dea58d0b6ab7193b3b1
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-12 14:44:57 +0900

    Update 11023.java

--- 10001-15000/11023.java
+++ 10001-15000/11023.java
@@ -1,18 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		String[] str = br.readLine().split(" ");
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int sum = 0;
-		for(int i = 0; i < str.length; i++) {
-			sum += Integer.parseInt(str[i]);
+		while(st.hasMoreTokens()) {
+			sum += Integer.parseInt(st.nextToken());
 		}
-
-		System.out.println(sum);
-		br.close();
+		System.out.print(sum);
 	}
 }
