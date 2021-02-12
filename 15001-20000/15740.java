a4135f762a2df8b4b273a3c42c226dcb7315ce40
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-13 00:57:10 +0900

    Update 15740.java

--- 15001-20000/15740.java
+++ 15001-20000/15740.java
@@ -1,26 +1,14 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		BigInteger a = new BigInteger(st.nextToken());
-		BigInteger b = new BigInteger(st.nextToken());
-		
-		BigInteger answer = a.add(b);
 				
-		bw.write(answer +" ");
-		bw.flush();
-
-		br.close();
-		bw.close();
+		System.out.print(new BigInteger(st.nextToken()).add(new BigInteger(st.nextToken())));
 	}
 }
