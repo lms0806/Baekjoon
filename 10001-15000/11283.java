ea1eea842b8f8a7b3d32d4710df4b40f2dc801b9
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-12 20:25:30 +0900

    Update 11283.java

--- 10001-15000/11283.java
+++ 10001-15000/11283.java
@@ -5,9 +5,7 @@ import java.io.InputStreamReader;
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-	
-		String str = br.readLine();
 		
-		System.out.println((int)str.charAt(0) - '가' + 1);
+		System.out.println(br.readLine().charAt(0) - '가' + 1);
 	}
 }
