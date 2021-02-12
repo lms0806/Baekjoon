506f4729cc9c30472d56b0177bb144863bba784b
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-13 00:48:05 +0900

    Update 15552.java

--- 15001-20000/15552.java
+++ 15001-20000/15552.java
@@ -1,27 +1,20 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main{
 	public static void main(String[] args) throws NumberFormatException,IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int a, b;
-		
+
 		int size = Integer.parseInt(br.readLine());
 		
-		for(int i = 0; i < size; i++) {
+		StringBuilder sb = new StringBuilder();
+		while(size-->0) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
 			
-			bw.write(a+b + "\n");
+			sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append("\n");
 		}
-		
-		bw.flush();
+		System.out.print(sb);
 	}
 }
