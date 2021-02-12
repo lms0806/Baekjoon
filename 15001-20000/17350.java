189504486dd3f71859874138a8234eca2330dc51
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-13 01:58:45 +0900

    Update 17350.java

--- 15001-20000/17350.java
+++ 15001-20000/17350.java
@@ -1,29 +1,21 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int size = Integer.parseInt(br.readLine());
 		
 		String answer = "뭐야?";
-		for(int i = 0; i < size; i++) {
-			String str = br.readLine();
-			
-			if(str.equals("anj")) {
+		while(size-->0) {
+			if(br.readLine().equals("anj")) {
 				answer = "뭐야;";
+				break;
 			}
 		}
 		
-		bw.write(answer);
-		bw.flush();
-
-		br.close();
-		bw.close();
+		System.out.print(answer);
 	}
 }
