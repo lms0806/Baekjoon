71c4c10438316c5d8f115095f7e82bffd1b12ccc
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-13 00:44:52 +0900

    Update 14928.java

--- 10001-15000/14928.java
+++ 10001-15000/14928.java
@@ -1,13 +1,10 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		String arr = br.readLine();
 		long result = 0;
@@ -15,10 +12,6 @@ public class Main {
 		for(int i = 0; i < arr.length(); i++) {
 			result = (result * 10 + (arr.charAt(i) - '0')) % 20000303;
 		}
-		bw.write(result + "\n");
-		
-		bw.flush();
-		br.close();
-		bw.close();
+		System.out.print(result);
 	}
 }
