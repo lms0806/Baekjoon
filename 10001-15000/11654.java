d0e2c7aecc3adde037e1efa685530df7f38f4e40
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-12 20:37:26 +0900

    Update 11654.java

--- 10001-15000/11654.java
+++ 10001-15000/11654.java
@@ -1,10 +1,11 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		String str = scan.nextLine();
-		System.out.println((int)str.charAt(0));
+		System.out.println((int)br.readLine().charAt(0));
 	}
 }
