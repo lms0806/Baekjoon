e83b3445964ceec8b21a33caf03a543d2b7503e4
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-12 20:22:42 +0900

    Update 11050.java

--- 10001-15000/11050.java
+++ 10001-15000/11050.java
@@ -1,14 +1,11 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int n = Integer.parseInt(st.nextToken());
@@ -20,10 +17,6 @@ public class Main {
 			b *= k - i;
 		}
 		
-		bw.write(a/b + "\n");
-		bw.flush();
-
-		br.close();
-		bw.close();
+		System.out.print(a/b);
 	}
 }
