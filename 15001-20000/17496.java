10d0fe4db27346f9d6c9b1be03e5a7a80a748aca
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-13 02:04:33 +0900

    Update 17496.java

--- 15001-20000/17496.java
+++ 15001-20000/17496.java
@@ -6,17 +6,14 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int[] a = new int[4];
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < 4; i++) {
+			a[i] = Integer.parseInt(st.nextToken());
+		}
 		
-		int a, b, c, d;
-		
-		a = Integer.parseInt(st.nextToken());
-		b = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
-		d = Integer.parseInt(st.nextToken());
-		
-		int result = ((a-1)/b)*c*d;
-		
-		System.out.println(result);
+		System.out.println(((a[0]-1)/a[1])*a[2]*a[3]);
 	}
 }
