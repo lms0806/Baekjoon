a1e55df958cedb688f037e3b8cd20dafb75b863b
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-15 22:05:01 +0900

    Create 1927.java

---
+++ 1-5000/1927.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		PriorityQueue<Integer> queue = new PriorityQueue<>();
+		
+		StringBuilder sb = new StringBuilder();
+		while(size-->0) {
+			int num = Integer.parseInt(br.readLine());
+			if(num == 0) {
+				if(queue.isEmpty()) {
+					sb.append("0");
+				}
+				else {
+					sb.append(queue.poll());
+				}
+				sb.append("\n");
+			}
+			else {
+				queue.add(num);
+			}
+		}
+		System.out.print(sb);
+	}
+}
