5aa4c117d72b6a3e3c41a9a8b68d30f29792092d
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-02-16 19:49:18 +0900

    Create 11943.java

---
+++ 10001-15000/11943.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int[] apple = new int[2];
+		int[] orange = new int[2];
+		
+		for(int i = 0; i < 2; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			apple[i] = Integer.parseInt(st.nextToken());
+			orange[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		System.out.print(Math.min(apple[1] + orange[0], apple[0] + orange[1]));
+	}
+}
