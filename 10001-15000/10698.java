ea6d1fe644151c83e7dc3390657538e01c497afe
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-03-16 12:52:21 +0900

    Create 10698.java

---
+++ 10001-15000/10698.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < size; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			String[] strs = new String[5];
+			for(int j = 0; j < 5; j++) {
+				strs[j] = st.nextToken();
+			}
+			
+			int a = Integer.parseInt(strs[0]);
+			int b = Integer.parseInt(strs[2]);
+			int c = Integer.parseInt(strs[4]);
+			
+			sb.append("Case ").append(i+1).append(": ");
+			if(strs[1].equals("+")) {
+				if(a+b == c) {
+					sb.append("YES");
+				}
+				else {
+					sb.append("NO");
+				}
+			}
+			else {
+				if(a-b == c) {
+					sb.append("YES");
+				}
+				else {
+					sb.append("NO");
+				}
+			}
+			sb.append("\n");
+		}
+		System.out.print(sb);
+	}
+}
