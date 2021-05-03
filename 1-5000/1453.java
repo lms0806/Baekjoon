55432ad4cc0d49222de3f35144e21a9e29e0322d
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-05-03 20:28:37 +0900

    Create 1453.java

---
+++ 1-5000/1453.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		boolean[] num = new boolean[100];
+		
+		int answer = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < size; i++) {
+			int number = Integer.parseInt(st.nextToken())-1;
+			if(!num[number]) {
+				num[number] = true;
+			}
+			else {
+				answer++;
+			}
+		}
+		
+		System.out.print(answer);
+	}
+}
