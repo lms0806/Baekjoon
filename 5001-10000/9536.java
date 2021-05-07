d0c4af1e08f72afc33ed05bf1184a646dd00c1f5
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-05-07 20:51:17 +0900

    Create 9536.java

---
+++ 5001-10000/9536.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		String[] strs = new String[100];
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(size --> 0) {
+			int count = 0;
+			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+			while(st.hasMoreTokens()) {
+				strs[count] = st.nextToken();
+				count++;
+			}
+			
+			while(true) {
+				String str = br.readLine().split(" ")[2];
+				
+				if(str.equals("the")) {
+					break;
+				}
+				
+				for(int j = 0; j < count; j++) {
+					if(strs[j].equals(str)) {
+						strs[j] = "";
+					}
+				}
+			}
+			
+			for(int j = 0; j < count; j++) {
+				if(!strs[j].equals("")) {
+					sb.append(strs[j]).append(" ");
+				}
+			}
+			sb.append("\n");
+		}
+		System.out.print(sb);
+	}
+}
