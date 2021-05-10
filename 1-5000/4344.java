b659eb449655a3918776b0620bcb9661422ba53b
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-05-10 19:49:09 +0900

    Create 4344.java

---
+++ 1-5000/4344.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+
+		int size = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(size --> 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int num = Integer.parseInt(st.nextToken());
+			
+			double n = 0;
+			int[] number = new int[num];
+			for(int i = 0; i < num; i++) {
+				number[i] = Integer.parseInt(st.nextToken());
+				n += number[i];
+			}
+			n /= num;
+			
+			double count = 0;
+			for(int i = 0; i < num; i++) {
+				if(number[i] > n) {
+					count++;
+				}
+			}
+			
+			double answer = count*100/num;
+			
+			sb.append(String.format("%.3f", Math.round(answer*1000)/1000.0) + "%").append("\n");
+		}
+		System.out.println(sb);
+	}
+}
