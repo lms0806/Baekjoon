3cdf69cb0a1df8f267661aa1f13dc2972cfb0a31
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-05-11 14:12:17 +0900

    Create 5949.java

---
+++ 5001-10000/5949.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		String str = br.readLine();
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < str.length(); i++) {
+			if(i > 0 && (str.length()-i) % 3 == 0) {
+				sb.append(",");
+			}
+			sb.append(str.charAt(i));
+		}
+		System.out.print(sb);
+	}
+}
