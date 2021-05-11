75d8e0cd97252cfb79d898b0c0dc75252cf7c2a1
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-05-11 16:08:50 +0900

    Create 10820.java

---
+++ 10001-15000/10820.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		String str;
+		StringBuilder sb = new StringBuilder();
+		while((str = br.readLine()) != null) {
+			int a = 0, b = 0, c = 0, d = 0;
+
+			for(int i = 0; i < str.length(); i++) {
+				char ch = str.charAt(i);
+				
+				if(ch >= 97 && ch <= 122) {
+					a++;
+				}
+				else if(ch >= 65 && ch <= 90) {
+					b++;
+				}
+				else if(ch >= 48 && ch <= 57) {
+					c++;
+				}
+				else if(ch == ' ') {
+					d++;
+				}
+			}
+			sb.append(a).append(" ").append(b).append(" ").append(c).append(" ").append(d).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
