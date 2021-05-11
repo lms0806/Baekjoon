8dcd96c6ae3d0718f26636b02f69a0046a1f3d32
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-05-11 13:28:22 +0900

    Create 4458.java

---
+++ 1-5000/4458.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(size --> 0) {
+			String s = br.readLine();
+			sb.append(s.substring(0,1).toUpperCase()).append(s.substring(1)).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
