9f4318427feece40cde742c79a0dac20168f4349
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-05-12 21:15:02 +0900

    Create 12780.java

---
+++ 10001-15000/12780.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		String str = br.readLine();
+		String s = br.readLine();
+		
+		System.out.print((str.length()-str.replace(s,"").length())/s.length());
+	}
+}
