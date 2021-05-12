845a7a9e3fac60866e18856d2bc89f80735f5369
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-05-12 22:52:02 +0900

    Create 13163.java

---
+++ 10001-15000/13163.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+        int size = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        while(size --> 0) {
+        	StringTokenizer st = new StringTokenizer(br.readLine());
+        	sb.append("god");
+        	st.nextToken();
+        	while(st.hasMoreTokens()) {
+        		sb.append(st.nextToken());
+        	}
+        	sb.append("\n");
+        }
+        System.out.print(sb);
+	}
+}
