20a4ff83984c5d7441259c3bb89ea85ad09971af
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-05-14 17:18:12 +0900

    Create 17094.java

---
+++ 15001-20000/17094.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		br.readLine();
+		String str = br.readLine();
+		
+		int a = 0;
+		for(int i = 0; i < str.length(); i++) {
+			if(str.charAt(i) == 'e') {
+				a++;
+			}
+		}
+		int b = str.length()  -a;
+		System.out.print(a > b ? "e" : a == b ? "yee" : 2);
+	}
+}
