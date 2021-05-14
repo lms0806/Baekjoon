d6bbaa632011136764ae389be15b04c67f992404
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-05-14 21:23:23 +0900

    Create 11655.java

---
+++ 10001-15000/11655.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		String str = br.readLine();
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if(ch >= 'A' && ch <= 'Z') {
+				sb.append(ch - 13 < 'A' ? (char)(ch+13) : (char)(ch-13));
+			}
+			else if(ch >= 'a' && ch <= 'z') {
+				sb.append(ch - 13 < 'a' ? (char)(ch+13) : (char)(ch-13));
+			}
+			else {
+				sb.append(ch);
+			}
+		}
+		System.out.print(sb);
+	}
+}
