3bb7605c33b8f0cfed3a7619656fd21daedd304e
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-05-15 15:12:44 +0900

    Create 1260.java

---
+++ 1-5000/1260.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static int[][] arr;
+	public static boolean[] visited;
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int point = Integer.parseInt(st.nextToken());
+		int line = Integer.parseInt(st.nextToken());
+		int start = Integer.parseInt(st.nextToken())-1;
+		
+		arr = new int[point][point];
+		
+		while(line --> 0 ) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken())-1;
+			int b = Integer.parseInt(st.nextToken())-1;
+			
+			arr[a][b] = 1;
+			arr[b][a] = 1;
+		}
+		
+		visited = new boolean[point];
+		dfs(start);
+		
+		sb.append("\n");
+		
+		visited = new boolean[point];
+		bfs(start);
+		
+		System.out.print(sb);
+	}
+	
+	public static void dfs(int start) {
+		visited[start] = true;
+		sb.append(start+1).append(" ");
+		
+		if(start == arr.length) {
+			return;
+		}
+		
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[start][i] == 1 && !visited[i]) {
+				dfs(i);
+			}
+		}
+	}
+	
+	public static void bfs(int start) {
+		Queue<Integer> que = new LinkedList<Integer>();
+		
+		que.add(start);
+		visited[start] = true;
+		sb.append(start+1).append(" ");
+		
+		while(!que.isEmpty()) {
+			int count = 0;
+			int temp = que.peek();
+			que.poll();
+			for(int i = 0; i < arr.length; i++) {
+				if(arr[temp][i] == 1 && !visited[i]) {
+					que.add(i);
+					visited[i] = true;
+					sb.append(i+1).append(" ");
+				}
+				if(visited[i]) {
+					count++;
+				}
+			}
+			if(count == arr.length) {
+				break;
+			}
+		}
+	}
+}
