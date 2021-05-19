5ff7a9e389f5eaf13ca1803900b23a20c7d6adaa
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-05-19 16:23:40 +0900

    Update 16172.java

--- 15001-20000/16172.java
+++ 15001-20000/16172.java
@@ -6,8 +6,6 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		String str = br.readLine().replaceAll("[0-9]", "");
-		
-        	System.out.print(str.lastIndexOf(br.readLine()) != -1 ? 1 : 0);
+        	System.out.print(br.readLine().replaceAll("[0-9]", "").lastIndexOf(br.readLine()) != -1 ? 1 : 0);
 	}
 }
