6d7e5a9a015e1fd977ae6df7a96c1acbc9f7931c
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-05-21 20:10:11 +0900

    Create 15927.java

---
+++ 15001-20000/15927.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	
+		System.out.print(solve(br.readLine()));
+	}
+	
+	public static int solve(String str) {
+		int n = 0;
+		for(int i = 1; i < str.length(); i++) {
+			if(str.charAt(0) == str.charAt(i)) {
+				n++;
+			}
+		}
+		if(n == str.length()-1) {
+			return -1;
+		}
+		
+		for(int i = 0; i < str.length(); i++) {
+			if(str.charAt(i) != str.charAt(str.length()-i-1)) {
+				return str.length();
+			}
+		}
+		
+		return str.length()-1;
+	}
+}
