7b6953ab4a715cd06a6c8a7220b47bf03e862ea8
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-05-25 14:35:57 +0900

    Update 11723.java

--- 10001-15000/11723.java
+++ 10001-15000/11723.java
@@ -1,50 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		ArrayList<Integer> arr = new ArrayList<>(20);
+		boolean[] set = new boolean[20];
 		
 		int num = Integer.parseInt(br.readLine());
 		
-		StringBuilder sb = new StringBuilder();
+		StringBuilder sb  = new StringBuilder();
 		while(num-->0) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			String str = st.nextToken();
-			if(str.equals("all")) {
-				for(int j = 1; j < 21; j++) {
-					if(!arr.contains(j)) {
-						arr.add(j);
+			String str = br.readLine();
+			char ch = str.charAt(0);
+			
+			if(ch == 'a') {
+				if(str.charAt(1) == 'd') {
+					if(!set[Integer.parseInt(str.substring(4, str.length()))-1]) {
+						set[Integer.parseInt(str.substring(4, str.length()))-1] = true;
+					}
+				}
+				else {
+					for(int i = 0; i < 20; i++) {
+						if(!set[i]) {
+							set[i] = true;
+						}
 					}
 				}
 			}
-			else if(str.equals("empty")) {
-				arr.clear();
+			else if(ch == 'e'){
+				for(int i = 0; i < 20; i++) {
+					if(set[i]) {
+						set[i] = false;
+					}
+				}
 			}
-			else {
-				int number = Integer.parseInt(st.nextToken());
-				if(str.charAt(0) == 'a' && !arr.contains(number)) {
-					arr.add(number);
+			else if(ch == 'r') {
+				if(set[Integer.parseInt(str.substring(7, str.length()))-1]) {
+					set[Integer.parseInt(str.substring(7, str.length()))-1] = false;
 				}
-				else if(str.charAt(0) == 'r' && arr.contains(number)) {
-					arr.remove(arr.indexOf(number));
-				}
-				else if(str.charAt(0) == 'c') {
-					sb.append(arr.contains(number) ? 1 : 0).append("\n");
-				}
-				else if(str.charAt(0) == 't') {
-					if(arr.contains(number)) {
-						arr.remove(arr.indexOf(number));
-					}
-					else {
-						arr.add(number);
-					}
-				}
+			}
+			else if(ch == 'c') {
+				sb.append(set[Integer.parseInt(str.substring(6, str.length()))-1] ? 1 : 0).append("\n");
+			}
+			else if(ch == 't') {
+				set[Integer.parseInt(str.substring(7, str.length()))-1] = !set[Integer.parseInt(str.substring(7, str.length()))-1];
 			}
 		}
 		System.out.print(sb);
