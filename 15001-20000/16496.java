ff0aa629cf57ebae40ba383272f98c9556ed3b72
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-05-31 16:01:15 +0900

    Create 16496.java

---
+++ 15001-20000/16496.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int size = Integer.parseInt(br.readLine());
+		
+		String[] numbers = new String[size];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < size; i++) {
+			numbers[i] = st.nextToken();
+		}
+        
+        System.out.print(solve(numbers));
+	}
+	
+	public static String solve(String[] str) {
+		
+        Arrays.sort(str, new Comparator<String>() {
+            public int compare(String a, String b){
+                return (b+a).compareTo(a+b);
+            }
+        });
+        
+        if(str[0].equals("0")){
+            return "0";
+        }
+	    
+        String answer = "";
+        for(int i = 0; i < str.length; i++){
+            answer += str[i];
+        }
+        
+        return answer;
+	}
+}
