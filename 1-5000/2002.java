cd343727754478327e95f3e99c0b7f9ebecd3dc3
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-06-01 12:09:26 +0900

    Create 2002.java

---
+++ 1-5000/2002.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		ArrayList<String> arr = new ArrayList<>();
+		int[] num = new int[size];
+		
+		for(int i = 0; i < size; i++) {
+			arr.add(br.readLine());
+		}
+		for(int i = 0; i < size; i++) {
+			num[i] = arr.indexOf(br.readLine());
+		}
+		
+		int answer = 0;
+		for(int i = 0; i < num.length-1; i++) {
+			for(int j = i+1; j < num.length; j++) {
+				if(num[i] > num[j]) {
+					answer++;
+					break;
+				}
+			}
+		}
+		
+		System.out.print(answer);
+	}
+}
