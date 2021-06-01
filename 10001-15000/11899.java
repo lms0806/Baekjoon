dd9a9d3bbc0afeea4fa51cf06f5135ef63cc498d
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-06-01 23:47:31 +0900

    Create 11899.java

---
+++ 10001-15000/11899.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		
+		Stack<Character> stack = new Stack<>();
+		
+		int answer = 0;
+		for(char ch : str.toCharArray()) {
+			if(ch == '(') {
+				stack.add(ch);
+			}
+			else {
+				if(!stack.isEmpty() && stack.peek() == '(') {
+					stack.pop();
+				}
+				else {
+					answer++;
+				}
+			}
+		}
+		answer += stack.size();
+		System.out.print(answer);
+	}
+}
