7ed98fd23094df00b6e29f0fcc15535d985f0f20
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-06-03 23:27:32 +0900

    Create 1769.java

---
+++ 1-5000/1769.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		
+		int answer = 0;
+		int last = 0;
+		while(str.length() != 1) {
+			for(char ch : str.toCharArray()) {
+				last += ch - '0';
+			}
+			str = Integer.toString(last);
+			last = 0;
+			answer++;
+		}
+		
+		System.out.println(answer);
+		System.out.print(Integer.parseInt(str) % 3 == 0 ? "YES" : "NO");
+	}
+}
