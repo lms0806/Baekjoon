e70839964d903566d621f8b62bcac9a008d4284e
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-06-04 02:22:22 +0900

    Create 1312.java

---
+++ 1-5000/1312.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		
+		int answer = a % b;
+		
+		for(int i = 0; i < n - 1; i++) {
+			answer = answer * 10 % b;
+		}
+		answer *= 10;
+		
+		System.out.print(answer / b);
+	}
+}
