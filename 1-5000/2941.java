994d824e9f96586dcc9cbff70cfeb0b611456540
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-06-07 02:25:27 +0900

    Create 2941.java

---
+++ 1-5000/2941.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		String str = br.readLine();
+		
+		int answer = 0, i = 0;
+		for(i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			
+			if(ch == 'c' && i < str.length() - 1) {
+				if(str.charAt(i+1) == '=' || str.charAt(i+1) == '-') {
+					i++;
+				}
+			}
+			else if(ch == 'd' && i < str.length() - 1) {
+				if(str.charAt(i+1) == 'z') {
+					if(i < str.length()-2 && str.charAt(i+2) == '=') {
+						i+=2;
+					}
+				}
+				else if(str.charAt(i+1) == '-') {
+					i++;
+				}
+			}
+			else if((ch == 'l' || ch == 'n') && i < str.length() - 1) {
+				if(str.charAt(i+1) == 'j') {
+					i++;
+				}
+			}
+			else if((ch == 's' || ch == 'z') && i < str.length() - 1) {
+				if(str.charAt(i+1) == '=') {
+					i++;
+				}
+			}
+			answer++;
+		}
+		System.out.print(answer);
+	}
+}
