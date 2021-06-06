74b17ce72e45d8918f4198f25d9fd9459bc925d0
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-06-07 03:41:19 +0900

    Create 11659.java

---
+++ 10001-15000/11659.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int num = Integer.parseInt(st.nextToken())+1;
+		int size = Integer.parseInt(st.nextToken());
+		
+		int[] sum = new int[num];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 1; i < num; i++) {
+			sum[i] =  sum[i - 1] + Integer.parseInt(st.nextToken());
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		while(size --> 0) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			sb.append(sum[b] - sum[a - 1]).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
