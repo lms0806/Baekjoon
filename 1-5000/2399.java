8892f167fe9d52a757dde6f54dfa7f8e2bac3b96
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-06-07 23:06:59 +0900

    Create 2399.java

---
+++ 1-5000/2399.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		long[] num = new long[size];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < size; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(num);
+		
+		long answer = 0;
+		for(int i = 0; i < size; i++) {
+			for(int j = i+1; j < size; j++) {
+				answer += num[j] - num[i];
+			}
+		}
+		System.out.print(answer*2);
+	}
+}
