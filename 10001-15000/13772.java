030fe23363417a1d6c67939bb1f21aa75897e8b2
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-06-09 01:01:48 +0900

    Create 13772.java

---
+++ 10001-15000/13772.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(size --> 0) {
+			String str = br.readLine();
+			
+			int answer = 0;
+			for(char ch : str.toCharArray()) {
+				if(ch == 'A' || ch == 'D' || ch == 'O' || ch == 'P' || ch == 'Q' || ch == 'R') {
+					answer++;
+				}
+				else if(ch == 'B') {
+					answer += 2;
+				}
+			}
+			sb.append(answer).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
