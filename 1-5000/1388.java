76736bdc0e18323f37b8c0a08180c89b274ee4ac
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-06-08 23:25:32 +0900

    Create 1388.java

---
+++ 1-5000/1388.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		char[][] ch = new char[n][m];
+		
+		for(int i = 0; i < n; i++) {
+			ch[i] = br.readLine().toCharArray();
+		}
+		
+		System.out.print(solve(ch, n, m, false, '-') + solve(ch, m, n, true, '|'));
+	}
+	
+	public static int solve(char[][] ch, int n, int m, boolean check, char c) {
+		int answer = 0;
+		
+		for(int i = 0; i < n; i++) {
+			int num = 0;
+			for(int j = 0; j < m; j++) {
+				if(!check) {
+					if(ch[i][j] == c) {
+						num++;
+					}
+					else {
+						if(num != 0) {
+							answer++;
+						}
+						num = 0;
+					}
+				}
+				else {
+					if(ch[j][i] == c) {
+						num++;
+					}
+					else {
+						if(num != 0) {
+							answer++;
+						}
+						num = 0;
+					}	
+				}
+			}
+			if(num != 0) {
+				answer++;
+			}
+		}
+		
+		return answer;
+	}
+}
