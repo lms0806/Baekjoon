814234ebb91a72f38679370d84f101c0f873c16c
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-06-09 00:33:14 +0900

    Create 1402.java

---
+++ 1-5000/1402.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(size --> 0) {
+			br.readLine();
+			
+			sb.append("yes").append("\n");
+		}
+		System.out.print(sb);
+	}
+}
