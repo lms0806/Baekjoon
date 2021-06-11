86e0e48fd047ff5aed3ed645ad4bf3efef889c29
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-06-11 17:39:05 +0900

    Create 21631.java

---
+++ 20001-25000/21631.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long a = Long.parseLong(st.nextToken());
+		long b = Long.parseLong(st.nextToken());
+		
+		long answer = Math.min(a, b) + 1;
+		
+		if(answer > b) {
+			answer--;
+		}
+		System.out.print(answer);
+	}
+}
