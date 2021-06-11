77c4579ded37a86a6a4185a3f2bc18388c11fefd
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-06-12 03:04:20 +0900

    Create 14614.java

---
+++ 10001-15000/14614.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+		String c = st.nextToken();
+
+		System.out.print((c.charAt(c.length() - 1) - '0') % 2 != 0 ? a ^ b : a);
+	}
+}
