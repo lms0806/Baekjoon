803c039897104ce6a371c19bc93a118d52c86ed3
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-06-11 17:48:54 +0900

    Create 21612.java

---
+++ 20001-25000/21612.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int a = Integer.parseInt(br.readLine()) * 5 - 400;
+		
+		System.out.println(a);
+		System.out.print(a > 100 ? -1 : a == 100 ? 0 : 1);
+	}
+}
