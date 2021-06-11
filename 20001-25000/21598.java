ef4cea3244e1274c2a1cd945b1ca25297794ecdc
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-06-11 17:52:22 +0900

    Create 21598.java

---
+++ 20001-25000/21598.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(n --> 0) {
+			sb.append("SciComLove").append("\n");
+		}
+		System.out.print(sb);
+	}
+}
