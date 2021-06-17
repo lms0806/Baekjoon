0f6fa4328ef4c2d5354385658bb8149beb959293
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-06-17 18:02:54 +0900

    Create 1120.java

---
+++ 1-5000/1120.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		String a = st.nextToken(), b = st.nextToken();
+		
+		int num = a.length();
+		
+		for(int i = 0; i < b.length() - a.length() + 1; i++) {
+			int temp = 0;
+			
+			for(int j = 0; j < a.length(); j++) {
+				if(a.charAt(j) != b.charAt(j + i)) {
+					temp++;
+				}
+			}
+			num = Math.min(num, temp);
+		}
+		
+		System.out.print(num);
+	}
+}
