aa8429821ea5fd2764c61009bad5515bb159ae41
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-06-19 18:08:52 +0900

    Create 9094.java

---
+++ 5001-10000/9094.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(size --> 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			
+			int answer = 0;
+			for(int i = 1; i < n - 1; i++) {
+				int a = i * i;
+				for(int j = i + 1; j < n; j++) {
+					int b = j * j;
+					
+					if((a + b + m) % (i * j) == 0) {
+						answer++;
+					}
+				}
+			}
+			sb.append(answer).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
