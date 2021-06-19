ef19db0fb27b8a198f3b463ae290c3bb8eba47a5
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-06-19 17:26:16 +0900

    Create 2231.java

---
+++ 1-5000/2231.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		System.out.print(solve(n));
+	}
+	
+	public static int solve(int n) {
+		
+		for(int i = 1; i < n; i++) {
+			int number = i, num = 0;
+			
+			while(number != 0) {
+				num += number % 10;
+				number /= 10;
+			}
+			
+			if(num + i == n) {
+				return i;
+			}
+		}
+		return 0;
+	}
+}
