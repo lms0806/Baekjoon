72e5be4289fe4e99ede8879258e6df22196ec861
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-06-19 20:58:08 +0900

    Create 19532.java

---
+++ 15001-20000/19532.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		int e = Integer.parseInt(st.nextToken());
+		int f = Integer.parseInt(st.nextToken());
+		
+		System.out.print(solve(a,b,c,d,e,f));
+	}
+	
+	public static String solve(int a, int b, int c, int d, int e, int f) {
+		for(int x = -999; x <= 999; x++) {
+			for(int y = -999; y <= 999; y++) {
+				if(a * x + b * y == c && d * x + e * y == f) {
+					return x + " " + y;
+				}
+			}
+		}
+		return "";
+	}
+}
