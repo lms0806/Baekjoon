160355574aef1a82a0ac97c8c87c81ab643bb587
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-06-29 02:18:49 +0900

    Create 21955.java

---
+++ 20001-25000/21955.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < str.length(); i++) {
+			if(i == str.length() / 2) {
+				sb.append(" ");
+			}
+			sb.append(str.charAt(i));
+		}
+		
+		System.out.print(sb);
+	}
+}
