a6bb24b9bbc13447021d935f593f8b99ec5e8e42
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-02 01:42:50 +0900

    Create 8974.java

---
+++ 5001-10000/8974.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+		
+		int num = 1, count = 0, answer = 0;
+		for(int i = 1; i <= b; i++) {
+			if(i >= a) {
+				answer += num;
+			}
+			
+			count++;
+			
+			if(num == count) {
+				num++;
+				count = 0;
+			}
+		}
+		System.out.print(answer);
+	}
+}
