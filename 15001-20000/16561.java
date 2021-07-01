b7e7e49bd782ff3f5a512670279ec1164caa2b43
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-02 01:18:03 +0900

    Create 16561.java

---
+++ 15001-20000/16561.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		int answer = 1, count = 2;
+		
+		for(int i = 9; i < size; i += 3) {
+			answer += count;
+			count++;
+		}
+		
+		System.out.print(answer);
+	}
+}
