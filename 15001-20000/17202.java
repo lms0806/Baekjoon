2858c86b0c2ff7ede67ce016769bb601421bbd21
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-03 00:46:01 +0900

    Create 17202.java

---
+++ 15001-20000/17202.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		String a = br.readLine(), b = br.readLine();
+		
+		int[] num = new int[16];
+		for(int i = 0; i < 8; i++) {
+			num[i * 2] = a.charAt(i) - '0';
+			num[i * 2 + 1] = b.charAt(i) - '0';
+		}
+		
+		for(int i = 15; i > 1; i--) {
+			for(int j = 0; j < i; j++) {
+				num[j] += num[j + 1];
+				num[j] %= 10;
+			}
+		}
+		
+		System.out.print(num[0] + "" + num[1]);
+	}
+}
