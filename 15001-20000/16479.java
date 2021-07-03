4f07cfc35663c0cf53a7e2e24f2f18d7beb9db3a
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-03 19:31:47 +0900

    Create 16479.java

---
+++ 15001-20000/16479.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		double num = Double.parseDouble(st.nextToken()) / 2 - Double.parseDouble(st.nextToken()) / 2;
+		System.out.println(n * n - num * num);
+	}
+}
