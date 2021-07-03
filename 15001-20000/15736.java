84440021bba319b993c481e6fc4e380ace75790f
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-03 22:52:10 +0900

    Create 15736.java

---
+++ 15001-20000/15736.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int answer = 0;
+		for(int i = 1; i <= Math.sqrt(n); i++) {
+			answer++;
+		}
+		System.out.print(answer);
+	}
+}
