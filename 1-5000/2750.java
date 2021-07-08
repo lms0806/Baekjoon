85dac81bacd9ad445eb4dd81c1769fa216340554
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-08 16:44:29 +0900

    Update 2750.java

--- 1-5000/2750.java
+++ 1-5000/2750.java
@@ -4,20 +4,20 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int n = Integer.parseInt(br.readLine());
-		int[] num = new int[n];
+		var size = Integer.parseInt(br.readLine());
+		int[] num = new int[size];
 		
-		for(int i = 0; i < num.length; i++) {
+		for(int i = 0; i < size; i++) {
 			num[i] = Integer.parseInt(br.readLine());
 		}
 		
 		Arrays.sort(num);
 		
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < num.length; i++) {
+		for(int i = 0; i < size; i++) {
 			sb.append(num[i]).append("\n");
 		}
 		System.out.print(sb);
