25bdec4252d5297857d4317ba99df9fb38708465
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-08 12:38:12 +0900

    Create 22193.java

---
+++ 20001-25000/22193.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		br.readLine();
+		System.out.print(new BigInteger(br.readLine()).multiply(new BigInteger(br.readLine())));
+	}
+}
