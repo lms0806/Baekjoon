1e2cba6a449a79eb28e7cb49bcc5dce8ca57c651
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-09 11:40:32 +0900

    Create 9372.java

---
+++ 5001-10000/9372.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(size --> 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			
+			sb.append(n - 1).append("\n");
+			
+			while(m --> 0) {
+				br.readLine();
+			}
+		}
+		System.out.print(sb);
+	}
+}
