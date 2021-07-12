a22eeefd3bfd318fb22e555453789a761b408919
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-12 23:13:34 +0900

    Update 5052.java

--- 5001-10000/5052.java
+++ 5001-10000/5052.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
@@ -20,11 +19,7 @@ public class Main {
 				strs[i] = br.readLine();
 			}
 			
-			Arrays.sort(strs, new Comparator<String>() {
-				public int compare(String o1, String o2) {
-					return o1.compareTo(o2);
-				}
-			});
+			Arrays.sort(strs);
 			
 			String answer = "YES";
 			for(int i = 1; i < num; i++) {
