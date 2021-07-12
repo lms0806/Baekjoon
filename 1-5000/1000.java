57f865194db78466f8397fa71fde628fe6de5f64
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-12 21:57:30 +0900

    Update 1000.java

--- 1-5000/1000.java
+++ 1-5000/1000.java
@@ -8,6 +8,6 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		System.out.println(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+		System.out.print(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
 	}
 }
