0d4f7106fae6ccf5a52630dceb769594cbfe86fc
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-13 22:34:20 +0900

    Update 1264.java

--- 1-5000/1264.java
+++ 1-5000/1264.java
@@ -1,35 +1,27 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		String str = "";
+		StringBuilder sb = new StringBuilder();
 		while(true) {
-			str = br.readLine().trim().toLowerCase();
+			String str = br.readLine().toLowerCase();
 			
 			if(str.equals("#")) {
 				break;
 			}
 			
 			int count = 0;
-			String[] strs = str.split("");
-			for(int i = 0; i < strs.length; i++) {
-				if(strs[i].equals("a") || strs[i].equals("e") || strs[i].equals("i") || strs[i].equals("o") || strs[i].equals("u")) {
+			for(char ch : str.toCharArray()) {
+				if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
 					count++;
 				}
 			}
-			bw.write(count + "\n");
+			sb.append(count).append("\n");
 		}
-		
-		bw.flush();
-
-		br.close();
-		bw.close();
+		System.out.print(sb);
 	}
 }
