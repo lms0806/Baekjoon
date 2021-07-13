409c22aa7f10a3ef35115f836b47ad7dd7c2bab7
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-13 23:08:07 +0900

    Update 14915.java

--- 10001-15000/14915.java
+++ 10001-15000/14915.java
@@ -14,38 +14,34 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		String str = "";
 		if(m == 0) {
-			sb.append(0);
+			sb.append("0\n");
 		}
-		else {
-			while(m != 0) {
-				if(m%n == 10) {
-					str += "A";
-				}
-				else if(m%n == 11) {
-					str += "B";
-				}
-				else if(m%n == 12) {
-					str += "C";
-				}
-				else if(m%n == 13) {
-					str += "D";
-				}
-				else if(m%n == 14) {
-					str += "E";
-				}
-				else if(m%n == 15) {
-					str += "F";
-				}
-				else {
-					str += Integer.toString(m%n);
-				}
-				m/=n;
+		while(m != 0) {
+			if(m%n == 10) {
+				str += "A";
 			}
+			else if(m%n == 11) {
+				str += "B";
+			}
+			else if(m%n == 12) {
+				str += "C";
+			}
+			else if(m%n == 13) {
+				str += "D";
+			}
+			else if(m%n == 14) {
+				str += "E";
+			}
+			else if(m%n == 15) {
+				str += "F";
+			}
+			else {
+				str += Integer.toString(m%n);
+			}
+			m /= n;
 		}
 		
-		for(int i = str.length() - 1; i >= 0; i--) {
-			sb.append(str.charAt(i));
-		}
+		sb.append(new StringBuilder(str).reverse().toString());
 		System.out.print(sb);
 	}
 }
