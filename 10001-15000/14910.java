b4c96441b87c5fff75f044cd11f24b952b9c88f7
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-15 01:57:50 +0900

    Create 14910.java

---
+++ 10001-15000/14910.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		
+		String answer = "Good";
+		while(st.hasMoreTokens()) {
+			int num = Integer.parseInt(st.nextToken());
+			if(n <= num) {
+				n = num;
+			}
+			else {
+				answer = "Bad";
+				break;
+			}
+		}
+		System.out.print(answer);
+	}
+}
