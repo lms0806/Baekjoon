98b91e473866c1166e7862a2746d1f38733bc9a7
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-15 00:59:09 +0900

    Create 11945.java

---
+++ 10001-15000/11945.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		st.nextToken();
+		st.nextToken();
+		
+		String str = "";
+		StringBuilder sb = new StringBuilder();
+		while((str = br.readLine()) != null){
+			sb.append(new StringBuilder(str).reverse().toString()).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
