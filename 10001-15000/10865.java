eabd0b25dc32876f12b14fe29f4b611f65d06607
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-15 02:22:02 +0900

    Create 10865.java

---
+++ 10001-15000/10865.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
+		
+		int[] num = new int[n];
+		
+		for(int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			num[Integer.parseInt(st.nextToken()) - 1]++;
+			num[Integer.parseInt(st.nextToken()) - 1]++;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < n; i++) {
+			sb.append(num[i]).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
