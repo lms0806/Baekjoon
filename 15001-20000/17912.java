b57c896db47fc383f08226e118eb149fc3ec1e6f
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-24 00:55:28 +0900

    Create 17912.java

---
+++ 15001-20000/17912.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int min = 1000000000;
+		
+		int answer = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			if(min > num) {
+				min = num;
+				answer = i;
+			}
+		}
+		System.out.print(answer);
+	}
+}
