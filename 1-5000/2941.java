f0a18da6ec50fe8be2036a959b60ec7d019c5cd5
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-23 18:23:36 +0900

    Update 2941.java

--- 1-5000/2941.java
+++ 1-5000/2941.java
@@ -8,34 +8,26 @@ public class Main {
 		
 		String str = br.readLine();
 		
-		int answer = 0, i = 0;
-		for(i = 0; i < str.length(); i++) {
+		int answer = 0;
+		for(int i = 0; i < str.length(); i++) {
 			char ch = str.charAt(i);
 			
-			if(ch == 'c' && i < str.length() - 1) {
-				if(str.charAt(i+1) == '=' || str.charAt(i+1) == '-') {
-					i++;
-				}
+			if(ch == 'c' && i < str.length() - 1 && (str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-')) {
+				i++;
 			}
 			else if(ch == 'd' && i < str.length() - 1) {
-				if(str.charAt(i+1) == 'z') {
-					if(i < str.length()-2 && str.charAt(i+2) == '=') {
-						i+=2;
-					}
+				if(str.charAt(i + 1) == 'z' && i < str.length()-2 && str.charAt(i+2) == '=') {
+					i += 2;
 				}
-				else if(str.charAt(i+1) == '-') {
+				else if(str.charAt(i + 1) == '-') {
 					i++;
 				}
 			}
-			else if((ch == 'l' || ch == 'n') && i < str.length() - 1) {
-				if(str.charAt(i+1) == 'j') {
-					i++;
-				}
+			else if((ch == 'l' || ch == 'n') && i < str.length() - 1 && str.charAt(i + 1) == 'j') {
+				i++;
 			}
-			else if((ch == 's' || ch == 'z') && i < str.length() - 1) {
-				if(str.charAt(i+1) == '=') {
-					i++;
-				}
+			else if((ch == 's' || ch == 'z') && i < str.length() - 1 && str.charAt(i + 1) == '=') {
+				i++;
 			}
 			answer++;
 		}
