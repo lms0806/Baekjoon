45106378638d62537251c9e9c96669e1ef5bc79e
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-24 12:28:53 +0900

    Create 18398.java

---
+++ 15001-20000/18398.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(size --> 0) {
+			int num = Integer.parseInt(br.readLine());
+			
+			while(num --> 0) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+				sb.append(a + b).append(" ").append(a * b).append("\n");
+			}
+		}
+		System.out.print(sb);
+	}
+}
