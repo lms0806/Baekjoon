0f656fd5573036e59e960542b11d3ed9cf68d0f7
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-24 22:45:14 +0900

    Create 2145.java

---
+++ 1-5000/2145.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		StringBuilder sb = new StringBuilder();
+		while(true) {
+			int n = Integer.parseInt(br.readLine());
+			
+			if(n == 0) {
+				break;
+			}
+			
+			while(n > 9){
+				int num = 0;
+				while(n != 0) {
+					num += n % 10;
+					n /= 10;
+				}
+				
+				n = num;
+			}
+			sb.append(n).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
