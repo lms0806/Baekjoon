6392da4f799107cd1c0b9386354474253b2121f5
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-24 19:09:49 +0900

    Update 20001.java

--- 20001-25000/20001.java
+++ 20001-25000/20001.java
@@ -6,26 +6,14 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
+		br.readLine();
 		String str = br.readLine();
+		
 		int num = 0;
-		if(str.equals("고무오리 디버깅 시작")) {
-			while(true) {
-				str = br.readLine();
-				if(str.equals("고무오리 디버깅 끝")) {
-					break;
-				}
-				else if(str.equals("문제")) {
-					num--;
-				}
-				else if(str.equals("고무오리")) {
-					if(num == 0) {
-						num-=2;
-					}
-					else {
-						num++;
-					}
-				}
-			}
+		while(!str.equals("고무오리 디버깅 끝")) {
+			num += str.equals("문제") ? -1 : num == 0 ? -2 : 1;
+			
+			str = br.readLine();
 		}
 		
 		System.out.print(num < 0 ? "힝구" : "고무오리야 사랑해");
