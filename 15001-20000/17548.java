964e4c81e394252fcebcde65c012096289d9f58d
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-25 21:11:46 +0900

    Update 17548.java

--- 15001-20000/17548.java
+++ 15001-20000/17548.java
@@ -6,11 +6,8 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		String str = br.readLine();
-		
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < str.length(); i++) {
-			char ch = str.charAt(i);
+		for(char ch : br.readLine().toCharArray()) {
 			sb.append(ch == 'e' ? ch : "").append(ch);
 		}
 		System.out.print(sb);
