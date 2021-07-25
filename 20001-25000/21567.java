977744d65360ce5c3427fcd3582e9c2aad7536e3
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-25 20:12:20 +0900

    Update 21567.java

--- 20001-25000/21567.java
+++ 20001-25000/21567.java
@@ -6,17 +6,19 @@ import java.math.BigInteger;
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		
+
 		int[] number = new int[10];
 		BigInteger sum = new BigInteger(br.readLine()).multiply(new BigInteger(br.readLine())).multiply(new BigInteger(br.readLine()));
 		
 		while(sum.compareTo(BigInteger.ZERO) != 0) {
 			number[sum.remainder(BigInteger.TEN).intValue()]++;
-			sum = sum.divide(new BigInteger("10"));
+			sum = sum.divide(BigInteger.TEN);
 		}
 		
-		for(int i = 0; i < number.length; i++) {
-			System.out.println(number[i]);
+		StringBuilder sb = new StringBuilder();
+		for(int n : number) {
+			sb.append(n).append("\n");
 		}
+		System.out.print(sb);
 	}
 }
