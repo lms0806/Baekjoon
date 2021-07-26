7b3719041d82f733c06fa0c6a4d3d94f75fdd26f
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-26 22:32:07 +0900

    Update 15874.java

--- 15001-20000/15874.java
+++ 15001-20000/15874.java
@@ -8,24 +8,23 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int size = Integer.parseInt(st.nextToken())%26;
+		int size = Integer.parseInt(st.nextToken()) % 26;
 		st.nextToken();
 		
 		String str = br.readLine();
 		
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < str.length(); i++) {
-			char ch = str.charAt(i);
+		for(char ch : str.toCharArray()) {
 			if(ch == ' ' || ch == '.' || ch == ',') {
 				sb.append(ch);
 			}
 			else {
 				int num = 0;
 				if(ch >= 'a' && ch <= 'z') {
-					num = ch+size <= 'z' ? ch+size : ch+size-26;
+					num = ch + size <= 'z' ? ch + size : ch + size-26;
 				}
 				else if(ch >= 'A' && ch <= 'Z'){
-					num = ch+size <= 'Z' ? ch+size : ch+size-26;
+					num = ch + size <= 'Z' ? ch + size : ch + size-26;
 				}
 				sb.append((char)num);
 			}
