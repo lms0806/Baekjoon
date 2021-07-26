a3d2d56ab63008caefc0864d5aca3c114a9083b5
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-26 16:58:26 +0900

    Update 1475.java

--- 1-5000/1475.java
+++ 1-5000/1475.java
@@ -1,46 +1,38 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int n = scan.nextInt();
+		int n = Integer.parseInt(br.readLine());
 		int[] num = new int[10];
 		int temp = n;
 		
 		while(temp != 0) {
-			int a = temp%10;
-			if(a == 6 || a == 9) {
-				num[9]++;
-			}
-			else {
-				num[a]++;
-			}
-			temp/=10;
+			int a = temp % 10;
+			num[a == 6 ? 9 : a]++;
+			temp /= 10;
 		}
 		
 		int nummax = 0;
 		
-		for(int i = 0; i < num.length-1; i++) {
+		for(int i = 0; i < num.length - 1; i++) {
 			if(nummax <= num[i]) {
 				nummax = num[i];
 			}
 		}
 		
-		int sixnine = 0;
+		int sixnine = num[9] / 2;
 		
-		if(num[9] % 2 == 0) {
-			sixnine = num[9] / 2;
-		}
-		else {
-			sixnine = num[9] / 2 + 1;
-		}
+		sixnine += num[9] % 2 == 0 ? 0 : 1;
 		
 		int max = (int) Math.max(nummax, sixnine);
 		if(n == 0) {
 			max = 1;
 		}
 		
-		System.out.println(max);
+		System.out.print(max);
 	}
 }
