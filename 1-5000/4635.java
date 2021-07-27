06181552e4c7213d007e6d69a54bf9f98d7228e7
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-27 10:21:41 +0900

    Create 4635.java

---
+++ 1-5000/4635.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		StringBuilder sb = new StringBuilder();
+		while(true) {
+			int n = Integer.parseInt(br.readLine());
+			
+			if(n == -1) {
+				break;
+			}
+			
+			int[][] num = new int[n][2];
+			
+			for(int i = 0; i < n; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				num[i][0] = Integer.parseInt(st.nextToken());
+				num[i][1] = Integer.parseInt(st.nextToken());
+			}
+			
+			int answer = 0;
+			for(int i = n - 1; i >= 1; i--) {
+				answer += (num[i][1] - num[i - 1][1]) * num[i][0];
+			}
+			answer += num[0][0] * num[0][1];
+			
+			sb.append(answer).append(" miles").append("\n");
+		}
+		System.out.print(sb);
+	}
+}
