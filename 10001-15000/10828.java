912bcd55569478617599f3ff0e9f2ee86ec35830
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-29 17:20:36 +0900

    Update 10828.java

--- 10001-15000/10828.java
+++ 10001-15000/10828.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
@@ -10,29 +11,29 @@ public class Main {
 		Stack<Integer> stack = new Stack<>();
 		
 		int size = Integer.parseInt(br.readLine());
-		int charat;
-		String str;
 		
 		StringBuilder sb = new StringBuilder();
 		while(size-->0) {
-			str = br.readLine();
-			charat = str.charAt(0);
-			if(charat == 't') {
-				sb.append(stack.size() != 0 ? stack.get(stack.size()-1) : -1).append("\n");
-			}
-			else if(charat == 'e') {
-				sb.append(stack.empty() ? 1 : 0).append("\n");
-			}
-			else if(charat == 's') {
-				sb.append(stack.size()).append("\n");
-			}
-			else if(charat == 'p') {
-				if(str.charAt(1) == 'u') {
-					stack.push(Integer.parseInt(str.substring(5)));
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String s = st.nextToken();
+			
+			char ch = s.charAt(0);
+			if(ch == 'p') {
+				if(s.charAt(1) == 'u') {
+					stack.push(Integer.parseInt(st.nextToken()));
 				}
 				else {
 					sb.append(stack.size() != 0 ? stack.pop() : -1).append("\n");
 				}
+			}
+			else {
+				if(ch == 't') {
+					sb.append(stack.size() != 0 ? stack.get(stack.size() - 1) : -1);
+				}
+				else {
+					sb.append(ch == 'e' ? stack.empty() ? 1 : 0 : stack.size());
+				}
+				sb.append("\n");
 			}
 		}
 		System.out.print(sb);
