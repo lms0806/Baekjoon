5337778493ff776b31613dc3ea063b34f819201d
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-30 02:12:20 +0900

    Update 6763.java

--- 5001-10000/6763.java
+++ 5001-10000/6763.java
@@ -6,20 +6,11 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int a = Integer.parseInt(br.readLine());
-		int b = Integer.parseInt(br.readLine());
+		int a = Integer.parseInt(br.readLine()), b = Integer.parseInt(br.readLine());
 		
 		StringBuilder sb = new StringBuilder();
 		if(b > a) {
-			if(b-a <= 20) {
-				sb.append("You are speeding and your fine is $100.");
-			}
-			else if(b-a >= 21 && b-a <= 30) {
-				sb.append("You are speeding and your fine is $270.");
-			}
-			else if(b-a >= 31) {
-				sb.append("You are speeding and your fine is $500.");
-			}
+			sb.append("You are speeding and your fine is ").append(b - a <= 20 ? "$100." : b - a >= 23 && b - a <= 30 ? "$270." : "$500.");
 		}
 		else {
 			sb.append("Congratulations, you are within the speed limit!");
