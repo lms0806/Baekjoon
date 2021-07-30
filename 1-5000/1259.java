fffc39d6a95e89e8500198c226d6d73544caae53
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-30 16:59:55 +0900

    Update 1259.java

--- 1-5000/1259.java
+++ 1-5000/1259.java
@@ -5,11 +5,9 @@ import java.io.InputStreamReader;
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		
-		String str = "";
 		StringBuilder sb = new StringBuilder();
-		while(!str.equals("0")) {
-			str = br.readLine();
+		while(true) {
+			String str = br.readLine();
 			
 			if(str.equals("0")) {
 				break;
@@ -17,16 +15,11 @@ public class Main {
 			
 			int count = 0;
 			for(int i = 0; i < str.length(); i++) {
-				if(str.charAt(i) == str.charAt(str.length()-i-1)) {
+				if(str.charAt(i) == str.charAt(str.length() - i - 1)) {
 					count++;
 				}
 			}
-			if(count == str.length()) {
-				sb.append("yes").append("\n");
-			}
-			else {
-				sb.append("no").append("\n");
-			}
+			sb.append(count == str.length() ? "yes" : "no").append("\n");
 		}
 		System.out.print(sb);
 	}
