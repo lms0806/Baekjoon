d4f0e5aa866314545e968a9209c81997c09d1178
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-30 16:50:14 +0900

    Update 2407.java

--- 1-5000/2407.java
+++ 1-5000/2407.java
@@ -9,18 +9,14 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
 		
-		BigInteger a = BigInteger.ONE;
-		BigInteger b = BigInteger.ONE;
+		BigInteger a = BigInteger.ONE, b = BigInteger.ONE;
 		
 		for(int i = 0; i < m; i++) {
-			a = a.multiply(new BigInteger(String.valueOf(n-i)));
-			b = b.multiply(new BigInteger(String.valueOf(i+1)));
+			a = a.multiply(new BigInteger(String.valueOf(n - i)));
+			b = b.multiply(new BigInteger(String.valueOf(i + 1)));
 		}
-		BigInteger sum = a.divide(b);
-		
-		System.out.println(sum);
+		System.out.println(a.divide(b));
 	}
 }
