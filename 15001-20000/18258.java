2b35ebaa45d67109f41e00738b6d1443180655a4
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-07-30 21:43:07 +0900

    Update 18258.java

--- 15001-20000/18258.java
+++ 15001-20000/18258.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
@@ -11,33 +12,36 @@ public class Main {
 		Queue<Integer> queue = new LinkedList<>();
 		
 		int size = Integer.parseInt(br.readLine());
-		int charat, num = 0;
-		String str;
+		int num = 0;
 		
 		StringBuilder sb = new StringBuilder();
-		while(size-->0) {
-			str = br.readLine();
-			charat = str.charAt(0);
-			if(charat == 'f') {
-				sb.append(queue.isEmpty() ? -1 : queue.peek()).append("\n");
-			}
-			else if(charat == 'b') {
-				sb.append(queue.isEmpty() ? -1 : num).append("\n");
-			}
-			else if(charat == 'e') {
-				sb.append(queue.isEmpty() ? 1 : 0).append("\n");
-			}
-			else if(charat == 's') {
-				sb.append(queue.size()).append("\n");
-			}
-			else if(charat == 'p') {
-				if(str.charAt(1) == 'u') {
-					num = Integer.parseInt(str.substring(5));
+		while(size --> 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String s = st.nextToken();
+			char ch = s.charAt(0);
+			if(ch == 'p') {
+				if(s.charAt(1) == 'u') {
+					num = Integer.parseInt(st.nextToken());
 					queue.add(num);
 				}
 				else {
 					sb.append(queue.isEmpty() ? -1 : queue.poll()).append("\n");
 				}
+			}
+			else {
+				if(ch == 'f') {
+					sb.append(queue.isEmpty() ? -1 : queue.peek());
+				}
+				else if(ch == 'b') {
+					sb.append(queue.isEmpty() ? -1 : num);
+				}
+				else if(ch == 'e') {
+					sb.append(queue.isEmpty() ? 1 : 0);
+				}
+				else {
+					sb.append(queue.size());
+				}
+				sb.append("\n");
 			}
 		}
 		System.out.print(sb);
