ff07e7204f7ea42ada83abb7d091e915cff83350
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-01 18:47:10 +0900

    Update 2490.java

--- 1-5000/2490.java
+++ 1-5000/2490.java
@@ -10,25 +10,28 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < 3; i++) {
 			int sum = 0;
+			
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int j = 0; j < 4; j++) {
 				sum += Integer.parseInt(st.nextToken());
 			}
+			
 			if(sum == 1) {
-				sb.append("C").append("\n");
+				sb.append("C");
 			}
 			else if(sum == 2) {
-				sb.append("B").append("\n");
+				sb.append("B");
 			}
 			else if(sum == 3) {
-				sb.append("A").append("\n");
+				sb.append("A");
 			}
 			else if(sum == 4) {
-				sb.append("E").append("\n");
+				sb.append("E");
 			}
 			else {
-				sb.append("D").append("\n");
+				sb.append("D");
 			}
+			sb.append("\n");
 		}
 		System.out.print(sb);
 	}
