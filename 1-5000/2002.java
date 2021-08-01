5729c4e3825797e3289f4d8447328810f59ba3dd
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-01 19:18:30 +0900

    Update 2002.java

--- 1-5000/2002.java
+++ 1-5000/2002.java
@@ -15,13 +15,14 @@ public class Main {
 		for(int i = 0; i < size; i++) {
 			arr.add(br.readLine());
 		}
+		
 		for(int i = 0; i < size; i++) {
 			num[i] = arr.indexOf(br.readLine());
 		}
 		
 		int answer = 0;
-		for(int i = 0; i < num.length-1; i++) {
-			for(int j = i+1; j < num.length; j++) {
+		for(int i = 0; i < num.length - 1; i++) {
+			for(int j = i + 1; j < num.length; j++) {
 				if(num[i] > num[j]) {
 					answer++;
 					break;
