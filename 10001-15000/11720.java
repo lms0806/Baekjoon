264a459a8007f55264ec3ad7ee106fc38ca47f10
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-01 19:07:07 +0900

    Update 11720.java

--- 10001-15000/11720.java
+++ 10001-15000/11720.java
@@ -6,13 +6,11 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int number = Integer.parseInt(br.readLine());
-		
-		String str = br.readLine();
+		br.readLine();
 		
 		int sum = 0;
-		for(int i = 0; i < str.length(); i++) {
-			sum += str.charAt(i)-'0';
+		for(char ch : br.readLine().toCharArray()) {
+			sum += ch - '0';
 		}
 		System.out.print(sum);
 	}
