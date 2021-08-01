bc2c0f6d48d423a59ec47f671946c6e7a0024eb6
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-01 19:11:52 +0900

    Update 9372.java

--- 5001-10000/9372.java
+++ 5001-10000/9372.java
@@ -12,8 +12,7 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		while(size --> 0) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken());
-			int m = Integer.parseInt(st.nextToken());
+			int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
 			
 			sb.append(n - 1).append("\n");
 			
