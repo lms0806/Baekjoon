7ab9b3b82f443ea4e17006e482654bfb4d425d8a
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-01 18:50:49 +0900

    Update 2774.java

--- 1-5000/2774.java
+++ 1-5000/2774.java
@@ -3,12 +3,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws NumberFormatException,IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
+		int size = Integer.parseInt(br.readLine());
+		
 		StringBuilder sb = new StringBuilder();
-		for(char ch : br.readLine().toCharArray()) {
-			sb.append(Character.isUpperCase(ch) ? Character.toString(ch).toLowerCase() : Character.toString(ch).toUpperCase());
+		while(size --> 0) {
+			int count = 0;
+			int[] num = new int[10];
+			
+			for(char ch : br.readLine().toCharArray()) {
+				num[ch - '0']++;
+			}
+			
+			for(int n : num) {
+				if(n != 0) {
+					count++;
+				}
+			}
+			sb.append(count).append("\n");
 		}
 		System.out.print(sb);
 	}
