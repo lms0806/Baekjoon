ae6f2252d1320ff239fc95dee7b79526caf53364
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-01 19:10:03 +0900

    Update 15927.java

--- 15001-20000/15927.java
+++ 15001-20000/15927.java
@@ -10,25 +10,27 @@ public class Main {
 	}
 	
 	public static int solve(String str) {
-		int n = 0;
-		for(int i = 1; i < str.length(); i++) {
-			if(str.charAt(0) == str.charAt(i)) {
+		int n = -1;
+		
+		for(char ch : str.toCharArray()) {
+			if(str.charAt(0) == ch) {
 				n++;
 			}
 			else {
 				break;
 			}
 		}
-		if(n == str.length()-1) {
+		
+		if(n == str.length() - 1) {
 			return -1;
 		}
 		
-		for(int i = 0; i < str.length()/2; i++) {
-			if(str.charAt(i) != str.charAt(str.length()-i-1)) {
+		for(int i = 0; i < str.length() / 2; i++) {
+			if(str.charAt(i) != str.charAt(str.length() - i - 1)) {
 				return str.length();
 			}
 		}
 		
-		return str.length()-1;
+		return str.length() - 1;
 	}
 }
