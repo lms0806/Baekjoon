2fa84aec46f73b4255f94580c84809694400dcef
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-03 01:47:35 +0900

    Update 11021.java

--- 10001-15000/11021.java
+++ 10001-15000/11021.java
@@ -10,9 +10,9 @@ public class Main {
 		int a = Integer.parseInt(br.readLine());
 		
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < a; i++) {
+		for(int i = 1; i <= a; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			sb.append("Case #").append(i + 1).append(": ").append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append("\n");
+			sb.append("Case #").append(i).append(": ").append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append("\n");
 		}
 		System.out.print(sb);
 	}
