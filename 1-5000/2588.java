9e6b6fcbbe0d96d08800a7ed21bec63a28f1257c
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-02 16:51:42 +0900

    Update 2588.java

--- 1-5000/2588.java
+++ 1-5000/2588.java
@@ -5,18 +5,14 @@ import java.io.InputStreamReader;
 public class Main {
 	public static void main(String[] args) throws NumberFormatException,IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		int a, b;
-		
-		a = Integer.parseInt(br.readLine());
-		b = Integer.parseInt(br.readLine());
-		
+
+		int a = Integer.parseInt(br.readLine()), b = Integer.parseInt(br.readLine());
+
 		StringBuilder sb = new StringBuilder();
-		
-		sb.append(a*(b%10)).append("\n");
-		sb.append(a*((b%100)/10)).append("\n");
-		sb.append(a*(b/100)).append("\n");
-		sb.append(a*b).append("\n");
-		
+		sb.append(a * (b % 10)).append("\n");
+		sb.append(a * ((b % 100) / 10)).append("\n");
+		sb.append(a * (b / 100)).append("\n");
+		sb.append(a * b).append("\n");
 		System.out.print(sb);
 	}
 }
