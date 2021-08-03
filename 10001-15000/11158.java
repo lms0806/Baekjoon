4270fbd26b1b10d6e36e1647a8a066225c3fc1ee
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-03 20:01:27 +0900

    Create 11158.java

---
+++ 10001-15000/11158.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(size --> 0) {
+			String[] s = br.readLine().split(" ");
+			
+			int answer = 0;
+			for(int i = 0; i < s.length; i++) {
+				if(i < s.length - 1 && (s[i].equals("would") || s[i].equals("should")) && s[i + 1].equals("of")){
+					answer++;
+				}
+				else if(s[i].equals("u") || s[i].equals("ur")) {
+					answer++;
+				}
+				else if(s[i].contains("lol") || s[i].equals("haha")) {
+					answer++;
+				}
+			}
+			sb.append(answer * 10).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
