2b3466bf8fa97acc113454f97ade551053921ed1
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-03 23:52:27 +0900

    Update 1181.java

--- 1-5000/1181.java
+++ 1-5000/1181.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class Main {
@@ -11,29 +10,24 @@ public class Main {
 		
 		int size = Integer.parseInt(br.readLine());
 		
-		ArrayList<String> arr = new ArrayList<>(size);
+		String[] arr = new String[size];
 		for(int i = 0; i < size; i++) {
-			arr.add(br.readLine());
+			arr[i] = br.readLine();
 		}
 		
-		Collections.sort(arr, new Comparator<String>() {
+		Arrays.sort(arr, new Comparator<String>() {
 			public int compare(String s1, String s2) {
-				if(s1.length() == s2.length()) {
-					return s1.compareTo(s2);
-				}
-				else {
-					return s1.length() - s2.length();
-				}
+				return s1.length() == s2.length() ? s1.compareTo(s2) : s1.length() - s2.length();
 			}
 		});
 		
 		StringBuilder sb = new StringBuilder();
-		sb.append(arr.get(0)).append("\n");
-		for(int i = 1; i < arr.size(); i++) {
-			if(arr.get(i-1).equals(arr.get(i))) {
+		sb.append(arr[0]).append("\n");
+		for(int i = 1; i < size; i++) {
+			if(arr[i - 1].equals(arr[i])) {
 				continue;
 			}
-			sb.append(arr.get(i)).append("\n");
+			sb.append(arr[i]).append("\n");
 		}
 		System.out.print(sb);
 	}
