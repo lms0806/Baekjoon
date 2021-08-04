5bef702e12b69ea428769fcb6078c82be1df2208
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-04 16:21:36 +0900

    Create 12526.java

---
+++ 10001-15000/12526.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 1; i <= size; i++) {
+			String s = br.readLine().trim();
+			
+			sb.append("Case #").append(i).append(": ").append(s).append(" is ruled by ");
+			char ch = s.toLowerCase().charAt(s.length() - 1);
+			if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+				sb.append("a queen");
+			}
+			else {
+				sb.append(ch == 'y' ? "nobody" : "a king");
+			}
+			sb.append(".\n");
+		}
+		System.out.print(sb);
+	}	
+}
