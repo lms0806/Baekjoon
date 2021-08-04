4f9970167a8c401ad6df7d40ec8dee675f9bb92a
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-04 18:23:42 +0900

    Update 10874.java

--- 10001-15000/10874.java
+++ 10001-15000/10874.java
@@ -12,21 +12,22 @@ public class Main {
 		int[] result = new int[10];
 		
 		for(int i = 1; i <= 10; i++) {
-			result[i-1] = ((i-1)%5)+1;
+			result[i-1] = ((i - 1) % 5) + 1;
 		}
 		
 		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < num; i++) {
-			int count = 0;
 			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int count = 0;
 			for(int j = 0; j < 10; j++) {
-				int answer = Integer.parseInt(st.nextToken());
-				if(answer == result[j]) {
+				if(Integer.parseInt(st.nextToken()) == result[j]) {
 					count++;
 				}
 			}
+			
 			if(count == 10) {
-				sb.append(i+1).append("\n");
+				sb.append(i + 1).append("\n");
 			}
 		}
 		System.out.print(sb);
