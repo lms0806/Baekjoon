5e5d793b50bbab36bbed0ad5923443b46139584a
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-05 20:31:53 +0900

    Create 1297.java

---
+++ 1-5000/1297.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int d = Integer.parseInt(st.nextToken()), h = Integer.parseInt(st.nextToken()), w = Integer.parseInt(st.nextToken());
+		double c = Math.sqrt(h * h + w * w);
+		
+		System.out.print((int)(h * d / c) + " " + (int)(w * d / c));
+	}
+}
