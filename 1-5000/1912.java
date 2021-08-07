b3c9b732f680154f74265dee77d364b4dedcafbd
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-07 18:05:34 +0900

    Update 1912.java

--- 1-5000/1912.java
+++ 1-5000/1912.java
@@ -12,17 +12,12 @@ public class Main {
 		int[] num = new int[size];
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < size; i++) {
+		int answer = Integer.parseInt(st.nextToken());
+		num[0] = answer;
+		for(int i = 1; i < size; i++) {
 			num[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		int[] sum = new int[size + 1];
-		sum[1] = num[0];
-		
-		int answer = Math.max(-1000, sum[1]);
-		for(int i = 1; i <= size; i++) {
-			sum[i] = Math.max(sum[i - 1] + num[i - 1], num[i - 1]);
-			answer = Math.max(answer, sum[i]);
+			num[i] = Math.max(num[i] + num[i - 1], num[i]);
+			answer = Math.max(answer, num[i]);
 		}
 		System.out.print(answer);
 	}
