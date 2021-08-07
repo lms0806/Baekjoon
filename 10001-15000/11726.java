3d20a13004d69296d0ec1220a1d8f8dd95360913
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-07 14:18:52 +0900

    Create 11726.java

---
+++ 10001-15000/11726.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		int[] num = new int[size + 1];
+		
+		num[0] = 1;
+		num[1] = 1;
+		
+		for(int i = 2; i <= size; i++) {
+			num[i] = (num[i - 1] +  num[i - 2]) % 10007;
+		}
+		System.out.print(num[size]);
+	}
+}
