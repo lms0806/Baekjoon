9ff8ee0f7be3efd31351309ec24ef3cc9e74595c
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-09 18:19:05 +0900

    Create 9046.java

---
+++ 5001-10000/9046.java
@@ -0,0 +1,41 @@
+// Authored by : lms0806
+// Co-authored by : -
+// Link : http://boj.kr/1637cf27836f425aa8d0393e55b23c45
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(size --> 0) {
+			int[] alpha = new int[26];
+			
+			for(char ch : br.readLine().toCharArray()) {
+				if(ch != ' ') {
+					alpha[ch - 'a']++;
+				}
+			}
+			
+			int max = 0, count = 0, n = 0;
+			for(int i = 0; i < alpha.length; i++) {
+				if(max < alpha[i]) {
+					max = alpha[i];
+					n = i;
+					count = 0;
+				}
+				else if(max == alpha[i]) {
+					count++;
+				}
+			}
+			
+			sb.append(count != 0 ? "?" : (char)(n + 'a')).append("\n");
+ 		}
+		System.out.print(sb);
+	}
+}
