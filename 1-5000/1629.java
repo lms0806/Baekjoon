67a413e5707fde1ad4030b34b65203319147c956
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-14 23:21:15 +0900

    Create 1629.java

---
+++ 1-5000/1629.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long a = Long.parseLong(st.nextToken()), b = Long.parseLong(st.nextToken()), c = Long.parseLong(st.nextToken());
+
+		long ans = 1, num = a % c;
+		while(b > 0){
+			if(b % 2 == 1){
+				ans *= num;
+				ans %= c;
+			}
+			num = ((num % c) * (num % c)) % c;
+			b /= 2;
+		}
+		System.out.print(ans);
+	}
+}
