b60dff0295e0a016bf79354ca800acc8f2d89ed7
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-14 12:15:05 +0900

    Update 14656.java

--- 10001-15000/14656.java
+++ 10001-15000/14656.java
@@ -8,15 +8,14 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
 		int size = Integer.parseInt(br.readLine());
-		int num = 0, count = 0;
 		
+		int count = 0;
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < size; i++) {
-			int number = Integer.parseInt(st.nextToken());
-			if(number != i+1) {
+			if(Integer.parseInt(st.nextToken()) != i + 1) {
 				count++;
 			}
 		}
-		System.out.println(count);
+		System.out.print(count);
 	}
 }
