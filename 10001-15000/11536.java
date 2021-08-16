79fde2ce40ff505895b1065d279f0921bbed8f76
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-16 22:16:02 +0900

    Update 11536.java

--- 10001-15000/11536.java
+++ 10001-15000/11536.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
