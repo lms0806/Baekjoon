8dc1aa935e0bab07911632943f42a432b61c05a2
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-17 16:47:43 +0900

    Update 10809.java

--- 10001-15000/10809.java
+++ 10001-15000/10809.java
@@ -1,28 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		String str = br.readLine();
-		
 		int[] alpha = new int[26];
 		
-		for(int i = 0; i < 26; i++) {
-			alpha[i] = -1;
-		}
+		Arrays.fill(alpha, -1);
 		
+		String str = br.readLine();
 		for(int i = 0; i < str.length(); i++) {
-			if(alpha[str.charAt(i) - 'a'] == -1) {
-				alpha[str.charAt(i) - 'a'] = i;
+			int ch = str.charAt(i) - 'a';
+			if(alpha[ch] == -1) {
+				alpha[ch] = i;
 			}
 		}
 		
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < alpha.length; i++) {
-			sb.append(alpha[i]).append(" ");
+		for(int n : alpha) {
+			sb.append(n).append(" ");
 		}
 		System.out.print(sb);
 	}
