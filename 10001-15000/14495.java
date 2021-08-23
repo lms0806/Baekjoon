a00c82378e587ac023060333ba3b420865ae76ec
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-23 15:02:19 +0900

    Create 14495.java

---
+++ 10001-15000/14495.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		long[] dp = new long[116];
+		
+		dp[0] = 1;
+		dp[1] = 1;
+		dp[2] = 1;
+		
+		for(int i = 3; i < n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 3];
+		}
+		System.out.print(dp[n - 1]);
+	}
+}
