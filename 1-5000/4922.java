c2a539985ad95e1633c9cbf9603c8e9176a18368
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-25 21:15:42 +0900

    Create 4922.java

---
+++ 1-5000/4922.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        
+		StringBuilder sb = new StringBuilder();
+		while(true) {
+			int n = Integer.parseInt(br.readLine());
+			
+			if(n == 0) {
+				break;
+			}
+			
+			sb.append(n).append(" => ").append(n * n - n + 1).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
