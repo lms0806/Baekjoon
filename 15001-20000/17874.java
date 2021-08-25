2153ab76b1b23900ff1d980c65a836445a11151f
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-25 16:31:47 +0900

    Create 17874.java

---
+++ 15001-20000/17874.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken()), h = Integer.parseInt(st.nextToken()), v = Integer.parseInt(st.nextToken());
+		
+		System.out.print(4 * Math.max(n - h, h) * Math.max(n - v, v));
+	}
+}
