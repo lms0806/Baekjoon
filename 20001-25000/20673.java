e272a941bc79194bdb31ac44244d813c5c8b323e
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-25 15:37:33 +0900

    Create 20673.java

---
+++ 20001-25000/20673.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		System.out.print(solve(Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine())));
+	}
+	
+	public static String solve(int a, int b) {
+		return b >= 30 ? "Red" : a <= 50 && b <= 10 ? "White" : "Yellow";
+	}
+}
