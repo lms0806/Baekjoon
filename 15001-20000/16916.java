4b9258df40e05aef9769d7db47a2da857c40f567
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-26 16:49:15 +0900

    Create 16916.java

---
+++ 15001-20000/16916.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+
+		System.out.print(KMP(br.readLine(), br.readLine()));
+	}
+	
+	public static int[] getpi(String s1) {
+		int[] pi = new int[s1.length()];
+		int j = 0;
+		for(int i = 1; i < s1.length(); i++) {
+			while(j > 0 && s1.charAt(i) != s1.charAt(j)) {
+				j = pi[j - 1];
+			}
+			if(s1.charAt(i) == s1.charAt(j)) {
+				pi[i] = j += 1;
+			}
+		}
+		return pi;
+	}
+	
+	public static int KMP(String s, String s1) {
+		int[] pi = getpi(s1);
+		int j = 0;
+		for(int i = 0; i < s.length(); i++) {
+			while(j > 0 && s.charAt(i) != s1.charAt(j)) {
+				j = pi[j - 1];
+			}
+			if(s.charAt(i) == s1.charAt(j)) {
+				if(j == s1.length() - 1) {
+					return 1;
+				}
+				else {
+					j++;
+				}
+			}
+		}
+		return 0;
+	}
+}
