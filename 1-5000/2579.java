127138bdea29985e14a7129c12499ebd86e73f93
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-27 13:45:00 +0900

    Create 2579.java

---
+++ 1-5000/2579.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[n + 1];
+		for(int i = 1; i <= n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		int[] dp = new int[n + 1];
+		dp[1] = arr[1];
+		
+		if(n > 1) {
+			dp[2] = arr[1] + arr[2];
+		}
+		
+		for(int i = 3; i <= n; i++) {
+			dp[i] = Math.max(dp[i - 2], dp[i - 3] + arr[i - 1]) + arr[i];
+		}
+		
+		System.out.print(dp[n]);
+	}
+}
