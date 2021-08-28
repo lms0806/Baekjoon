edf7eae9c971f8d412fa8ebd31c465c719397bcd
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-28 19:27:00 +0900

    Update 2563.java

--- 1-5000/2563.java
+++ 1-5000/2563.java
@@ -7,29 +7,24 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int[][] num = new int[100][100];
+		boolean[][] num = new boolean[101][101];
 		
 		int size = Integer.parseInt(br.readLine());
+		
+		int answer = 0;
 		while(size --> 0) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken()) - 1, b = Integer.parseInt(st.nextToken()) - 1;
+			int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
 			
 			for(int i = a; i < a + 10; i++) {
 				for(int j = b; j < b + 10; j++) {
-					num[i][j]++;
-				}
-			}
-		}
-		
-		int answer = 0;
-		for(int i = 0; i < 100; i++) {
-			for(int j = 0; j < 100; j++) {
-				if(num[i][j] != 0) {
-					answer++;
+					if(!num[i][j]) {
+						answer++;
+						num[i][j] = true;
+					}
 				}
 			}
 		}
 		System.out.print(answer);
-		
 	}
 }
