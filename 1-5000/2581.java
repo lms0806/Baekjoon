5e902e517b25f1e61d07eb53e87ce1b60ad7e62a
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-08-29 14:30:01 +0900

    Update 2581.java

--- 1-5000/2581.java
+++ 1-5000/2581.java
@@ -6,8 +6,7 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int a = Integer.parseInt(br.readLine());
-		int b = Integer.parseInt(br.readLine());
+		int a = Integer.parseInt(br.readLine()), b = Integer.parseInt(br.readLine());
 		
 		int answer = 0, min = 10000, count = 0;
 		for(int i = a; i <= b; i++) {
@@ -25,14 +24,6 @@ public class Main {
 			}
 			count = 0;
 		}
-		
-		StringBuilder sb = new StringBuilder();
-		if(answer == 0) {
-			sb.append(-1).append("\n");
-		}
-		else {
-			sb.append(answer).append("\n").append(min).append("\n");
-		}
-		System.out.print(sb);
+		System.out.print(answer == 0 ? -1 : answer + "\n" + min);
 	}
 }
