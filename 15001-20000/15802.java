258849fa1f787b64cbe9d74c3375cbf2d494f75c
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-01 19:38:16 +0900

    Update 15802.java

--- 15001-20000/15802.java
+++ 15001-20000/15802.java
@@ -5,9 +5,7 @@ import java.io.InputStreamReader;
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		
-		String str = br.readLine();
-		
+        	br.readLine();
 		System.out.print("1");
 	}
 }
