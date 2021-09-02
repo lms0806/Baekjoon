25e6bf67696fc94b715d57b944af180402f5e47c
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-02 21:30:44 +0900

    Update 10171.java

--- 10001-15000/10171.java
+++ 10001-15000/10171.java
@@ -1,10 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
 		StringBuilder sb = new StringBuilder();
-		sb.append("\\    /\\").append("\n");
-		sb.append(" )  ( \')").append("\n");
-		sb.append("(  /  )").append("\n");
-		sb.append(" \\(__)|");
+		sb.append("\\    /\\").append("\n").append(" )  ( \')").append("\n").append("(  /  )").append("\n").append(" \\(__)|");
 		System.out.print(sb);
 	}
 }
