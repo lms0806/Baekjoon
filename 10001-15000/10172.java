7e1283125cd98343e455fc0e1cdc4609bc313e61
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-02 21:36:24 +0900

    Update 10172.java

--- 10001-15000/10172.java
+++ 10001-15000/10172.java
@@ -1,11 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
 		StringBuilder sb = new StringBuilder();
-		sb.append("|\\_/|").append("\n");
-		sb.append("|q p|   /}").append("\n");
-		sb.append("( 0 )\"\"\"\\").append("\n");
-		sb.append("|\"^\"`    |").append("\n");
-		sb.append("||_/=\\\\__|");
+		sb.append("|\\_/|").append("\n").append("|q p|   /}").append("\n").append("( 0 )\"\"\"\\").append("\n").append("|\"^\"`    |").append("\n").append("||_/=\\\\__|");
 		System.out.print(sb);
 	}
 }
