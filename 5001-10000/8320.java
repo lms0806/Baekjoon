061acb10c834f371cece360a18d4d05f01353a94
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-04 12:36:19 +0900

    Create 8320.java

---
+++ 5001-10000/8320.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int answer = 0;
+		for(int i = 1; i <= n; i++) {
+			for(int j = i; i * j <= n; j++) {
+				answer++;
+			}
+		}
+		System.out.print(answer);
+	}
+}
