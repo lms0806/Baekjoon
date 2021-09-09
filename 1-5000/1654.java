1ef6b066e82577b80b3a7ff1929d721d42012411
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-09 21:42:12 +0900

    Create 1654.java

---
+++ 1-5000/1654.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int k = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
+		
+		int[] num = new int[k];
+		long end = 0;
+		for(int i = 0; i < k; i++) {
+			num[i] = Integer.parseInt(br.readLine());
+			end = Math.max(end, num[i]);
+		}
+		
+		long start = 1;
+		while(start <= end) {
+			long mid = (start + end) / 2;
+			
+			long count = 0;
+			for(int n : num) {
+				count += n / mid;
+			}
+			
+			if(count >= m) {
+				start = mid + 1;
+			}
+			else {
+				end = mid - 1;
+			}
+		}
+		System.out.print(end);
+	}
+}
