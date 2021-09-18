ee47ce80e1b252b9a9c61b12cf313d1c8eadaca8
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-18 22:35:23 +0900

    Create 16435.java

---
+++ 15001-20000/16435.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken()), l = Integer.parseInt(st.nextToken());
+		
+		int[] num = new int[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++){
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(num);
+		
+		for(int i = 0; i < n; i++) {
+			if(num[i] > l) {
+				break;
+			}
+			l++;
+		}
+		System.out.print(l);
+	}
+}
