f3071a4cb674955e7963142adba8e61e3179426c
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-18 20:55:19 +0900

    Create 23055.java

---
+++ 20001-25000/23055.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				if(i + j == n - 1 || i == j || i == 0 || j == 0 || i == n - 1 || j == n - 1) {
+					sb.append("*");
+				}
+				else {
+					sb.append(" ");
+				}
+			}
+			sb.append("\n");
+		}
+		System.out.print(sb);
+	}
+}
