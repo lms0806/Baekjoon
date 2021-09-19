540298f3e575ec49115c99378886a4076a3dd7b5
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-19 16:12:52 +0900

    Update 2908.java

--- 1-5000/2908.java
+++ 1-5000/2908.java
@@ -4,31 +4,13 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		String str1 = st.nextToken();
-		String str2 = st.nextToken();
+		int a = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		int b = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
 		
-		String strs1 = "";
-		String strs2 = "";
-		
-		for(int i = str1.length()-1; i >= 0; i--) {
-			strs1 += str1.charAt(i);
-			strs2 += str2.charAt(i);
-		}
-		
-		int num1 = Integer.parseInt(strs1);
-		int num2 = Integer.parseInt(strs2);
-		
-		StringBuilder sb = new StringBuilder();
-		if(num1 > num2) {
-			sb.append(num1);
-		}
-		else {
-			sb.append(num2);
-		}
-		System.out.print(sb);
+		System.out.print(a > b ? a : b);
 	}
 }
