c1965f28fd28f0123c5de32bad9b2531e9f1d6c6
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-20 23:54:02 +0900

    Create 14231.java

---
+++ 10001-15000/14231.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] num = new int[n], dp = new int[n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int answer = 0;
+		for(int i = 1; i < n; i++) {
+			for(int j = 0; j < i; j++) {
+				if(num[i] > num[j]) {
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+				}
+			}
+			answer = Math.max(answer, dp[i]);
+		}
+		
+		System.out.print(answer + 1);
+	}
+}
