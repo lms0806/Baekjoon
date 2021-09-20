996f62aafca5d03e0e08d782547101e7994823c2
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-21 03:01:14 +0900

    Update 10833.java

--- 10001-15000/10833.java
+++ 10001-15000/10833.java
@@ -4,20 +4,19 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int a = Integer.parseInt(br.readLine());
-		
+
 		int sum = 0;
-		while(a-->0) {
+		while (a --> 0) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			
-			sum += c%b;
+			int b = Integer.parseInt(st.nextToken()), c = Integer.parseInt(st.nextToken());
+
+			sum += c % b;
 		}
-		
+
 		System.out.print(sum);
 	}
 }
