6d11daf5b6e58b7ae6928560c512e38574deb4f7
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-21 02:37:48 +0900

    Update 14405.java

--- 10001-15000/14405.java
+++ 10001-15000/14405.java
@@ -3,28 +3,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		
-		char[] ch = br.readLine().toCharArray();
-		
-		String answer = "YES";
-		for(int i = 0; i < ch.length; i++) {
-			if(i < ch.length - 1 && ch[i] == 'p' && ch[i+1] == 'i') {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.print(solve(br.readLine().toCharArray()));
+	}
+
+	public static String solve(char[] ch) {
+		for (int i = 0; i < ch.length; i++) {
+			if (i < ch.length - 1 && ((ch[i] == 'p' && ch[i + 1] == 'i') || (ch[i] == 'k' && ch[i + 1] == 'a'))) {
 				i++;
-			}
-			else if(i < ch.length - 1 && ch[i] == 'k' && ch[i+1] == 'a') {
-				i++;
-			}
-			else if(i < ch.length - 2 && ch[i] == 'c' && ch[i+1] == 'h' && ch[i+2] == 'u') {
-				i+=2;
-			}
+			} 
+			else if (i < ch.length - 2 && ch[i] == 'c' && ch[i + 1] == 'h' && ch[i + 2] == 'u') {
+				i += 2;
+			} 
 			else {
-				answer = "NO";
-				break;
+				return "NO";
 			}
 		}
-		
-		System.out.print(answer);
+		return "YES";
 	}
 }
