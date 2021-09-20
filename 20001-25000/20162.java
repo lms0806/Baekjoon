61efd23c801bd2a876918d98cfadfb68488d2368
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-21 00:00:54 +0900

    Create 20162.java

---
+++ 20001-25000/20162.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] num = new int[n], dp = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			num[i] = Integer.parseInt(br.readLine());
+			dp[i] = num[i];
+		}
+		
+		int answer = dp[0];
+		for(int i = 1; i < n; i++) {
+			for(int j = 0; j < i; j++) {
+				if(num[i] > num[j]) {
+					dp[i] = Math.max(dp[i], dp[j] + num[i]);
+				}
+			}
+			answer = Math.max(answer, dp[i]);
+		}
+		
+		System.out.print(answer);
+	}
+}
