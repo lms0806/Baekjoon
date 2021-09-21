b8c480f479f224fbd00e02f368e23f312017c974
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-21 16:35:02 +0900

    Update 14491.java

--- 10001-15000/14491.java
+++ 10001-15000/14491.java
@@ -3,17 +3,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int num = Integer.parseInt(br.readLine());
-		
+
 		int answer = 0, count = 0;
-		while(num > 0) {
+		while (num > 0) {
 			answer += (num % 9) * Math.pow(10, count++);
-			num/=9;
+			num /= 9;
 		}
-		
-		System.out.println(answer);
+
+		System.out.print(answer);
 	}
 }
