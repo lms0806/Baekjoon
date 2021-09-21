28b8f097223b25532eacd78cf6dabfdd8d801e81
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-21 15:05:48 +0900

    Update 7662.java

--- 5001-10000/7662.java
+++ 5001-10000/7662.java
@@ -5,41 +5,43 @@ import java.util.StringTokenizer;
 import java.util.TreeMap;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int size = Integer.parseInt(br.readLine());
-		
+
 		StringBuilder sb = new StringBuilder();
-		while(size-->0) {
+		while (size-- > 0) {
 			TreeMap<Integer, Integer> map = new TreeMap<>();
 			int num = Integer.parseInt(br.readLine());
-			
-			while(num-->0) {
+
+			while (num --> 0) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
-				
+
 				char ch = st.nextToken().charAt(0);
 				int n = Integer.parseInt(st.nextToken());
-				
-				if(ch == 'I') {
+
+				if (ch == 'I') {
 					map.put(n, !map.containsKey(n) ? 1 : 1 + map.get(n));
-				}
-				else if(!map.isEmpty() && ch == 'D'){
-					int number = n == 1 ? map.lastKey() : map.firstKey(); 
-					if(map.get(number) == 1) {
+				} 
+				else if (!map.isEmpty() && ch == 'D') {
+					int number = n == 1 ? map.lastKey() : map.firstKey();
+					
+					if (map.get(number) == 1) {
 						map.remove(number);
-					}
+					} 
 					else {
-						map.put(number, map.get(number)-1);
+						map.put(number, map.get(number) - 1);
 					}
 				}
 			}
-			if(map.isEmpty()) {
-				sb.append("EMPTY").append("\n");
-			}
+			if (map.isEmpty()) {
+				sb.append("EMPTY");
+			} 
 			else {
-				sb.append(map.lastKey()).append(" ").append(map.firstKey()).append("\n");;
+				sb.append(map.lastKey()).append(" ").append(map.firstKey());
 			}
+			sb.append("\n");
 		}
 		System.out.print(sb);
 	}
