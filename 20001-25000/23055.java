4936045c2c3f3fb914f359e74955f10f96fcb880
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-21 17:25:37 +0900

    Update 23055.java

--- 20001-25000/23055.java
+++ 20001-25000/23055.java
@@ -11,12 +11,7 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < n; i++) {
 			for(int j = 0; j < n; j++) {
-				if(i + j == n - 1 || i == j || i == 0 || j == 0 || i == n - 1 || j == n - 1) {
-					sb.append("*");
-				}
-				else {
-					sb.append(" ");
-				}
+				sb.append(i + j == n - 1 || i == j || i == 0 || j == 0 || i == n - 1 || j == n - 1 ? "*" : " ");
 			}
 			sb.append("\n");
 		}
