cb2764058b833a76d6819e77fa594c793bfe5730
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-21 17:03:56 +0900

    Update 20944.java

--- 20001-25000/20944.java
+++ 20001-25000/20944.java
@@ -1,14 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		char[] ch = new char[Integer.parseInt(br.readLine())];
-		Arrays.fill(ch, 'a');
-		System.out.print(ch);
+		System.out.print("a".repeat(Integer.parseInt(br.readLine())));
 	}
 }
