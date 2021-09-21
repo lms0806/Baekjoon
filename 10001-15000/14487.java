f9e5abf68709b266037350a0a496d8e805dd34f5
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-21 16:32:23 +0900

    Update 14487.java

--- 10001-15000/14487.java
+++ 10001-15000/14487.java
@@ -4,19 +4,19 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int size = Integer.parseInt(br.readLine());
-		
-		int max = 0, answer = 0, num;
+
+		int max = 0, answer = 0, num = 0;
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < size; i++) {
+		while(size --> 0) {
 			num = Integer.parseInt(st.nextToken());
 			answer += num;
 			max = Math.max(num, max);
 		}
-		
-		System.out.print(answer-max);
+
+		System.out.print(answer - max);
 	}
 }
