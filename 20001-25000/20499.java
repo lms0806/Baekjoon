18bfeefc246ccd2c49c0316e53a3a6a6ff26b232
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-21 16:41:27 +0900

    Update 20499.java

--- 20001-25000/20499.java
+++ 20001-25000/20499.java
@@ -1,17 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		String[] strs = br.readLine().split("/");
-		
 		int[] num = new int[3];
 		
+		StringTokenizer st = new StringTokenizer(br.readLine(), "/");
 		for(int i = 0; i < 3; i++) {
-			num[i] = Integer.parseInt(strs[i]);
+			num[i] = Integer.parseInt(st.nextToken());
 		}
 		
 		System.out.print(num[0] + num[2] < num[1] || num[1] == 0 ? "hasu" : "gosu");
