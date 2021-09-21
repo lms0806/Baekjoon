cf097f89bcb4fc78f7d5f9bde59cb5df98b80969
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-21 16:34:26 +0900

    Update 14490.java

--- 10001-15000/14490.java
+++ 10001-15000/14490.java
@@ -1,25 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		
-		String[] str = br.readLine().split(":");
-		
-		int n = Integer.parseInt(str[0]);
-		int m = Integer.parseInt(str[1]);
-		
-		int gcd = gcd(n,m);
-		
-		System.out.println(n/gcd + ":" + m/gcd);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), ":");
+
+		int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
+
+		int gcd = gcd(n, m);
+
+		System.out.println(n / gcd + ":" + m / gcd);
 	}
-	
+
 	public static int gcd(int a, int b) {
-		if(b == 0) {
+		if (b == 0) {
 			return a;
 		}
-		return gcd(b, a%b);
+		return gcd(b, a % b);
 	}
 }
