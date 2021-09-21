6dbfe723f0c4aa9ad003c413a98fd7fe7ee02955
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-21 16:31:02 +0900

    Update 14652.java

--- 10001-15000/14652.java
+++ 10001-15000/14652.java
@@ -4,14 +4,13 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
-				
-		System.out.print(c/b + " " + c%b + " ");
+
+		st.nextToken();
+		int b = Integer.parseInt(st.nextToken()), c = Integer.parseInt(st.nextToken());
+
+		System.out.print(c / b + " " + c % b + " ");
 	}
 }
