c016336d6929ed75e42d1e01835539088366691b
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-21 16:27:01 +0900

    Update 15963.java

--- 15001-20000/15963.java
+++ 15001-20000/15963.java
@@ -8,6 +8,6 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		System.out.print(Long.parseLong(st.nextToken()) == Long.parseLong(st.nextToken()) ? 1 : 0);
+		System.out.print(st.nextToken().equals(st.nextToken()) ? 1 : 0);
 	}
 }
