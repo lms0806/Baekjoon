1a9cee5fa6e906ba8334f48c5412b8efbd26e831
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-21 15:00:09 +0900

    Update 6751.java

--- 5001-10000/6751.java
+++ 5001-10000/6751.java
@@ -9,33 +9,23 @@ public class Main {
 		int size = Integer.parseInt(br.readLine()) + 1;
 		
 		while(true) {
-			String str = Integer.toString(size);
-			boolean istrue = true;
-			
-			for(int i = 0; i < str.length(); i++) {
-				char s = str.charAt(i);
-				
-				for(int j = 0; j < str.length(); j++) {
-					if(i != j) {
-						char s1 = str.charAt(j);
-						
-						if(s == s1) {
-							istrue = false;
-							break;
-						}
-					}
-				}
-				if(!istrue) {
-					break;
-				}
-			}
-			
-			if(istrue) {
-				System.out.print(size);
+			if(solve(Integer.toString(size))) {
 				break;
 			}
 			
 			size++;
 		}
+		System.out.print(size);
+	}
+	
+	public static boolean solve(String str) {
+		for(int i = 0; i < str.length(); i++) {
+			for(int j = 0; j < str.length(); j++) {
+				if(i != j && str.charAt(i) == str.charAt(j)) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 }
