a41c85be4d9fe184ac6cd36fb8debff82ec4b620
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-21 15:15:22 +0900

    Update 1159.java

--- 1-5000/1159.java
+++ 1-5000/1159.java
@@ -6,19 +6,17 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int[] num = new int[26];
-		
 		int size = Integer.parseInt(br.readLine());
 		
-		StringBuilder sb = new StringBuilder();
+		int[] num = new int[26];
 		while(size --> 0) {
-			char ch = br.readLine().charAt(0);
-			num[ch - 'a']++;
+			num[br.readLine().charAt(0) - 'a']++;
 		}
 		
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < num.length; i++) {
 			if(num[i] >= 5) {
-				sb.append((char)(i + 97));
+				sb.append((char)(i + 'a'));
 			}
 		}
 		System.out.print(sb.length() == 0 ? "PREDAJA" : sb);
