7be7ca6f20df790383dfc39d40ccff8b567af17b
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-22 17:39:36 +0900

    Update 10816.java

--- 10001-15000/10816.java
+++ 10001-15000/10816.java
@@ -1,28 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int[] arr;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
 		int size = Integer.parseInt(br.readLine());
-		
-		int[] num = new int[20000001];
+		arr = new int[size];
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		while(size --> 0) {
-			num[Integer.parseInt(st.nextToken()) + 10000000]++;
+		for(int i = 0; i < size; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
+		
+		Arrays.sort(arr);
 		
 		size = Integer.parseInt(br.readLine());
 		
-		st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
+		st = new StringTokenizer(br.readLine());
 		while(size --> 0) {
-			sb.append(num[Integer.parseInt(st.nextToken()) + 10000000]).append(" ");
+			int n = Integer.parseInt(st.nextToken());
+			
+			sb.append(upper_bound(n, 0, arr.length) - lower_bound(n, 0, arr.length)).append(" ");
 		}
 		System.out.print(sb);
+	}
+	
+	public static int lower_bound(int input, int first, int end) {
+		while(first < end) {
+			int mid = (first + end) / 2;
+			
+			if(input <= arr[mid]){
+				end = mid;
+			}
+			else {
+				first = mid + 1;
+			}
+		}
+		return end;
+	}
+	
+	public static int upper_bound(int input, int first, int end) {
+		while(first < end) {
+			int mid = (first + end) / 2;
+			
+			if(input >= arr[mid]){
+				first = mid + 1;
+			}
+			else {
+				end = mid;
+			}
+		}
+		return end;
 	}
 }
