059fe458ac9ed2d67605bf3555c2bfe8df7be0c8
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-24 02:13:25 +0900

    Update 1476.java

--- 1-5000/1476.java
+++ 1-5000/1476.java
@@ -4,22 +4,19 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int e = Integer.parseInt(st.nextToken());
-		int s = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		
+
+		int e = Integer.parseInt(st.nextToken()), s = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
+
 		int answer = 1;
 		while(true) {
-			if((answer-e)%15 == 0 && (answer-s)%28 == 0 && (answer-m) % 19 == 0) {
+			if((answer - e) % 15 == 0 && (answer - s) % 28 == 0 && (answer - m) % 19 == 0) {
 				break;
 			}
 			answer++;
 		}
-		
-		System.out.println(answer);
+		System.out.print(answer);
 	}
 }
