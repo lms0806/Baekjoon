db1d472f975bd61acbe181a2e57f779f4e720988
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-24 01:10:57 +0900

    Create 16204.java

---
+++ 15001-20000/16204.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken()), k = Integer.parseInt(st.nextToken());
+		
+		System.out.print(Math.min(m, k) + Math.min(n - m, n - k));
+	}
+}
