f28f18c8a23589578aa6135e9728cac4f67da88b
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-23 15:55:18 +0900

    Update 2875.java

--- 1-5000/2875.java
+++ 1-5000/2875.java
@@ -4,21 +4,18 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
-		
+
+		int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken()), k = Integer.parseInt(st.nextToken());
+
 		int answer = 0;
-		while(n >= 2 && m >= 1 && n+m-2 > k) {
-			n-=2;
+		while (n >= 2 && m >= 1 && n + m - 2 > k) {
+			n -= 2;
 			m--;
 			answer++;
 		}
-		
 		System.out.print(answer);
 	}
 }
