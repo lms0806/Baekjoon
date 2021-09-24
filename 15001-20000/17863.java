ba878ac0b92918e8567a81802f5c1f9a28b7a14b
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-24 22:50:21 +0900

    Update 17863.java

--- 15001-20000/17863.java
+++ 15001-20000/17863.java
@@ -6,6 +6,6 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		System.out.print(br.readLine().substring(0,3).equals("555") ? "YES" : "NO");
+		System.out.print(br.readLine().startsWith("555") ? "YES" : "NO");
 	}
 }
