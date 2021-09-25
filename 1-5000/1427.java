08784df84cd5b8189a97501c5373af6d8c2385c9
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-25 22:52:21 +0900

    Update 1427.java

--- 1-5000/1427.java
+++ 1-5000/1427.java
@@ -4,16 +4,17 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		String str = br.readLine();
-		String[] strs = str.split("");
-		
-		Arrays.sort(strs);
-		
-		for(int i = strs.length-1; i >= 0; i--) {
-			System.out.print(strs[i]);
+		String[] s = br.readLine().split("");
+
+		Arrays.sort(s);
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = s.length - 1; i >= 0; i--) {
+			sb.append(s[i]);
 		}
+		System.out.print(sb);
 	}
 }
