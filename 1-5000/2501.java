2bea1edd9c5b9060edff2923baa2e672d52dd49a
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-27 02:40:18 +0900

    Update 2501.java

--- 1-5000/2501.java
+++ 1-5000/2501.java
@@ -4,31 +4,30 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException,IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		
+
+		int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+
 		int num = 1, count = 0;
-		
-		StringBuilder sb  = new StringBuilder();
-		while(a+1 != num) {
-			if(a%num == 0) {
+
+		StringBuilder sb = new StringBuilder();
+		while (a + 1 != num) {
+			if (a % num == 0) {
 				count++;
-				if(count == b) {
+				if (count == b) {
 					sb.append(num).append("\n");
 					break;
 				}
 			}
 			num++;
 		}
-		
-		if(num == a+1) {
+
+		if (num == a + 1) {
 			sb.append(0).append("\n");
 		}
-		
+
 		System.out.print(sb);
 	}
 }
