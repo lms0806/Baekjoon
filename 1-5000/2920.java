a2be04a6b45bb71b45a6761ab820ad874651b2c3
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-27 03:20:43 +0900

    Update 2920.java

--- 1-5000/2920.java
+++ 1-5000/2920.java
@@ -4,45 +4,35 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		int[] num = new int[8];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		int[] num = new int[8];
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < num.length; i++) {
+		for (int i = 0; i < num.length; i++) {
 			num[i] = Integer.parseInt(st.nextToken());
 		}
-		
+
 		int count = 0;
-		for(int i = 0; i < num.length; i++) {
-			if(num[i] == i+1) {
+		for (int i = 0; i < num.length; i++) {
+			if (num[i] == i + 1) {
 				count++;
-			}
+			} 
 			else {
 				break;
 			}
 		}
-		
+
 		int count1 = 0;
-		for(int i = 0; i < num.length; i++) {
-			if(num[i] == num.length-i) {
+		for (int i = 0; i < num.length; i++) {
+			if (num[i] == num.length - i) {
 				count1++;
-			}
+			} 
 			else {
 				break;
 			}
 		}
-		
-		StringBuilder sb = new StringBuilder();
-		if(count == 8) {
-			sb.append("ascending");
-		}
-		else if(count1 == 8) {
-			sb.append("descending");
-		}
-		else {
-			sb.append("mixed");
-		}
-		System.out.print(sb);
+		System.out.print(count == 8 ? "ascending" : count1 == 8 ? "descending" : "mixed");
 	}
 }
