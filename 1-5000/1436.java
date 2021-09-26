c056cb3919462d96c668564875d078d8a72428a2
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-26 15:25:36 +0900

    Update 1436.java

--- 1-5000/1436.java
+++ 1-5000/1436.java
@@ -5,18 +5,16 @@ import java.io.InputStreamReader;
 public class Main {
 	public static void main(String[] args) throws NumberFormatException,IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
 		int n = Integer.parseInt(br.readLine());
 		
-		int num = 666;
-		int count = 1;
-		
+		int num = 666, count = 1;
 		while(count != n) {
 			num++;
-			if(String.valueOf(num).contains("666")) {
+			if(Integer.toString(num).contains("666")) {
 				count++;
 			}
 		}
-		
-		System.out.println(num);
+		System.out.print(num);
 	}
 }
