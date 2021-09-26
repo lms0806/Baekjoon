0d6794800814ae9bd87d6b480885f314fa4d1633
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-26 15:15:35 +0900

    Update 2869.java

--- 1-5000/2869.java
+++ 1-5000/2869.java
@@ -8,15 +8,13 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
+		int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
 		int v = Integer.parseInt(st.nextToken());
 		
 		int day = (v - b) / (a - b);
 		if((v - b) % (a - b) != 0) {
 			day++;
 		}
-		
 		System.out.print(day);
 	}
 }
