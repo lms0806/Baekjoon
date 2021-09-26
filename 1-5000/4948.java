9aab54ba36822a7252a5d9489f6162a738267e6e
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-27 02:42:56 +0900

    Update 4948.java

--- 1-5000/4948.java
+++ 1-5000/4948.java
@@ -3,27 +3,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		
-		int a = 1;
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		StringBuilder sb = new StringBuilder();
-		while(true) {
+		while (true) {
 			int answer = 0, count = 0;
-			a = Integer.parseInt(br.readLine());
-			if(a == 0) {
+			int a = Integer.parseInt(br.readLine());
+			
+			if (a == 0) {
 				break;
 			}
-			for(int i = a+1; i <= 2*a; i++) {
-				int max = (int)Math.sqrt(i)+1;
-				for(int j = 2; j < max; j++) {
-					if(i % j == 0) {
+			
+			for (int i = a + 1; i <= 2 * a; i++) {
+				int max = (int) Math.sqrt(i) + 1;
+				for (int j = 2; j < max; j++) {
+					if (i % j == 0) {
 						count++;
 						break;
 					}
 				}
-				if(count == 0) {
+				if (count == 0) {
 					answer++;
 				}
 				count = 0;
