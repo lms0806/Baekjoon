33347821d07dbab7362038f3f819792d7387311d
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-27 03:39:30 +0900

    Update 2822.java

--- 1-5000/2822.java
+++ 1-5000/2822.java
@@ -4,21 +4,20 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		
-		int[] num = new int[8];
-		int[] answer = new int[5];
-		
-		for(int i = 0; i < 8; i++) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int[] num = new int[8], answer = new int[5];
+
+		for (int i = 0; i < 8; i++) {
 			num[i] = Integer.parseInt(br.readLine());
 		}
-		
+
 		int count = 0, sum = 0;
-		for(int i = 0; i < 7; i++) {
+		for (int i = 0; i < 7; i++) {
 			int number = 0;
-			for(int j = 0; j < 8; j++) {
-				if(num[j] > number) {
+			for (int j = 0; j < 8; j++) {
+				if (num[j] > number) {
 					number = num[j];
 					answer[count] = j;
 				}
@@ -26,18 +25,18 @@ public class Main {
 			sum += number;
 			num[answer[count]] = 0;
 			count++;
-			if(count >= 5) {
+			if (count >= 5) {
 				break;
 			}
 		}
-		
+
 		StringBuilder sb = new StringBuilder();
 		sb.append(sum).append("\n");
-		
+
 		Arrays.sort(answer);
-		
-		for(int i = 0; i < 5; i++) {
-			sb.append(answer[i]+1).append(" ");
+
+		for (int n : answer) {
+			sb.append(n + 1).append(" ");
 		}
 		System.out.print(sb);
 	}
