61818d53746340b370d7be70199edb9d0a1a3b83
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-27 02:53:21 +0900

    Update 15873.java

--- 15001-20000/15873.java
+++ 15001-20000/15873.java
@@ -3,31 +3,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		String str = br.readLine();
-		
-		String a = "", b= "";
-		if(str.length() > 2) {
-			if(str.charAt(str.length()-1) == '0') {
-				for(int i = 0; i < str.length() - 2; i++) {
+
+		String a = "", b = "";
+		if (str.length() > 2) {
+			if (str.charAt(str.length() - 1) == '0') {
+				for (int i = 0; i < str.length() - 2; i++) {
 					a += str.charAt(i);
 				}
 				b = b + str.charAt(str.length() - 2) + str.charAt(str.length() - 1);
-			}
+			} 
 			else {
-				for(int i = 0; i < str.length()-1; i++) {
+				for (int i = 0; i < str.length() - 1; i++) {
 					a += str.charAt(i);
 				}
 				b += str.charAt(str.length() - 1);
 			}
-		}
+		} 
 		else {
 			a += str.charAt(0);
 			b += str.charAt(1);
 		}
-		
-		System.out.println(Integer.parseInt(a) + Integer.parseInt(b));
+
+		System.out.print(Integer.parseInt(a) + Integer.parseInt(b));
 	}
 }
