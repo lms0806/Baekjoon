d5c229851eb64e82b892da9c9465de3728a93610
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-27 03:33:39 +0900

    Update 1978.java

--- 1-5000/1978.java
+++ 1-5000/1978.java
@@ -9,34 +9,23 @@ public class Main {
 		
 		int n = Integer.parseInt(br.readLine());
 		
-		boolean istrue = true;
-		
 		int answer = 0;
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 1; i <= n; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			istrue = true;
-			
-			istrue = sosu(num);
-			
-			if(istrue == true) {
+		while(n --> 0) {
+			if(sosu(Integer.parseInt(st.nextToken()))) {
 				answer++;
 			}
 		}
-		
-		System.out.println(answer);
+		System.out.print(answer);
 	}
 	
 	public static boolean sosu(int n) {
 		if(n == 1) {
 			return false;
 		}
-		else{
-			for (int i = 2; i*i <= n; i++) {
-				if (n % i == 0) {
-					return false;
-				}
+		for (int i = 2; i * i <= n; i++) {
+			if (n % i == 0) {
+				return false;
 			}
 		}
 		return true;
