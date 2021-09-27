01a533181d234671e1e2c72a2ac4979c9ee76e0d
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-28 00:24:17 +0900

    Update 5597.java

--- 5001-10000/5597.java
+++ 5001-10000/5597.java
@@ -3,21 +3,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		
-		int[] num = new int[30];
-		for(int i = 0; i < 28; i++) {
-			num[Integer.parseInt(br.readLine())-1]++;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int[] num = new int[31];
+		for (int i = 0; i < 28; i++) {
+			num[Integer.parseInt(br.readLine())]++;
 		}
-		
+
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < 30; i++) {
-			if(num[i] == 0) {
-				sb.append(i+1).append("\n");
+		for (int i = 1; i <= 30; i++) {
+			if (num[i] == 0) {
+				sb.append(i).append("\n");
 			}
 		}
-		
-		System.out.println(sb);
+		System.out.print(sb);
 	}
 }
