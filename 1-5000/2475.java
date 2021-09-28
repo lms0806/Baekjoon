dc59afbb5615e30536bee167f8f2ef1026285f41
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-28 12:57:22 +0900

    Update 2475.java

--- 1-5000/2475.java
+++ 1-5000/2475.java
@@ -7,12 +7,12 @@ public class Main {
 	public static void main(String[] args) throws NumberFormatException,IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 
-		int num = 0, sum = 0;
+		int sum = 0;
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < 5; i++) {
-			num = Integer.parseInt(st.nextToken());
-			sum += num*num;
+			int num = Integer.parseInt(st.nextToken());
+			sum += num * num;
 		}
-		System.out.println(sum%10);
+		System.out.print(sum % 10);
 	}
 }
