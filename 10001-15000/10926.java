86925d822a4a2557197c969c834c24286103a089
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-29 03:29:27 +0900

    Update 10926.java

--- 10001-15000/10926.java
+++ 10001-15000/10926.java
@@ -6,7 +6,6 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		String str = br.readLine() + "??!";
-		System.out.print(str);
+		System.out.print(br.readLine() + "??!");
 	}
 }
