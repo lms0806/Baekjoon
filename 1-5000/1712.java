2cd99e0c0360e0d168cc197cb3ac1f0e518c9ed2
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-28 19:11:16 +0900

    Update 1712.java

--- 1-5000/1712.java
+++ 1-5000/1712.java
@@ -6,21 +6,10 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		int a, b, c;
-		
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		a = Integer.parseInt(st.nextToken());
-		b = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
+		int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken()), c = Integer.parseInt(st.nextToken());
 		
-        StringBuilder sb = new StringBuilder();
-		if(b >= c) {
-			sb.append(-1);
-		}
-		else {
-			int count = a/(c-b) + 1;
-			sb.append(count);
-		}
-        System.out.print(sb);
+		System.out.print(b >= c ? -1 : a / (c - b) + 1);
 	}
 }
