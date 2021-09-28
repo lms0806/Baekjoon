8a59db0d8af580fa24c2b57c55872e96d16343dc
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-28 15:01:54 +0900

    Update 10951.java

--- 10001-15000/10951.java
+++ 10001-15000/10951.java
@@ -6,7 +6,7 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-
+		
 		String line = "";
 		StringBuilder sb = new StringBuilder();
 		while((line = br.readLine()) != null){
