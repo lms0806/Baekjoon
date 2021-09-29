ccb873d454d6f8e8b8352463c416bffac8e1101f
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-29 14:51:01 +0900

    Update 5692.java

--- 5001-10000/5692.java
+++ 5001-10000/5692.java
@@ -3,33 +3,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		String str = "";
 		StringBuilder sb = new StringBuilder();
-		while(true) {
-			str = br.readLine();
-			if(str.equals("0")) {
+		while (true) {
+			String str = br.readLine();
+			
+			if (str.equals("0")) {
 				break;
 			}
-			
+
 			int answer = 0;
-			for(int i = 0; i < str.length(); i++) {
-				int num = fact(i+1);
-				answer += (str.charAt(str.length()-i-1)-'0') * num;
+			for (int i = 0; i < str.length(); i++) {
+				answer += (str.charAt(str.length() - i - 1) - '0') * fact(i + 1);
 			}
 			sb.append(answer).append("\n");
 		}
-		System.out.println(sb);
+		System.out.print(sb);
 	}
-	
+
 	public static int fact(int a) {
-		if(a <= 1) {
-			return a;
-		}
-		else {
-			return fact(a-1)*a;
-		}
+		return a <= 1 ? a : fact(a - 1) * a;
 	}
 }
