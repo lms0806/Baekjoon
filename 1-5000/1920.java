0c8b2eea338d9e17b376c7dcf86b4d422bdb4a0a
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-29 22:24:00 +0900

    Update 1920.java

--- 1-5000/1920.java
+++ 1-5000/1920.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,20 +9,40 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
 		int size = Integer.parseInt(br.readLine());
-		HashSet<Integer> arr = new HashSet<>();
+		int[] arr = new int[size];
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		while(size --> 0) {
-			arr.add(Integer.parseInt(st.nextToken()));
+		for(int i = 0; i < size; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
+		
+		Arrays.sort(arr);
 		
 		size = Integer.parseInt(br.readLine());
 		
 		StringBuilder sb = new StringBuilder();
 		st = new StringTokenizer(br.readLine());
 		while(size --> 0) {
-			sb.append(arr.contains(Integer.parseInt(st.nextToken())) ? 1 : 0).append("\n");
+			sb.append(binary_search(arr, Integer.parseInt(st.nextToken()), 0, arr.length - 1)).append("\n");
 		}
 		System.out.print(sb);
+	}
+	
+	public static int binary_search(int[] arr, int input, int first, int end) {
+		while(first <= end) {
+			int mid = (first + end) / 2;
+			
+			if(input == arr[mid]) {
+				return 1;
+			}
+            
+			if(input < arr[mid]){
+				end = mid - 1;
+			}
+			else {
+				first = mid + 1;
+			}
+		}
+		return 0;
 	}
 }
