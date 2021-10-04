99be7305d041bec89076ec2a697b73a58aee62f0
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-10-04 11:44:10 +0900

    Create 1758.java

---
+++ 1-5000/1758.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int size = Integer.parseInt(br.readLine());
+		
+		int[] num = new int[size];
+		for(int i = 0; i < size; i++) {
+			num[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(num);
+		
+		long answer = 0;
+		for(int i = size - 1; i >= 0; i--) {
+			int n = num[i] - (size - i - 1);
+			if(n > 0) {
+				answer += n;
+			}
+		}
+		System.out.print(answer);
+	}
+}
