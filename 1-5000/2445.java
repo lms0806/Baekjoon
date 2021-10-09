cd9fa6cc2acb0e0898306da196831fc88aa90a23
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-10-10 02:41:27 +0900

    Update 2445.java

--- 1-5000/2445.java
+++ 1-5000/2445.java
@@ -3,35 +3,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException,IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int size = Integer.parseInt(br.readLine());
-		
+
 		StringBuilder sb = new StringBuilder();
-		for(int i = size-1; i > 0; i--) {
-			for(int j = i; j < size; j++) {
-				sb.append("*");
-			}
-			for(int j = (size-i*2); j < size; j++) {
-				sb.append(" ");
-			}
-			for(int j = i; j < size; j++) {
-				sb.append("*");
-			}
-			sb.append("\n");
+		for (int i = size - 1; i > 0; i--) {
+			sb.append("*".repeat(size - i)).append(" ".repeat(i * 2)).append("*".repeat(size - i)).append("\n");
 		}
-		for(int i = 0; i <= size-1; i++) {
-			for(int j = i; j < size; j++) {
-				sb.append("*");
-			}
-			for(int j = (size-i*2); j < size; j++) {
-				sb.append(" ");
-			}
-			for(int j = i; j < size; j++) {
-				sb.append("*");
-			}
-			sb.append("\n");
+		for (int i = 0; i <= size - 1; i++) {
+			sb.append("*".repeat(size - i)).append(" ".repeat(i * 2)).append("*".repeat(size - i)).append("\n");
 		}
 		System.out.print(sb);
 	}
