dd8f33755a5d50641f345c6f37c7cf59ce55e549
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-10-11 00:00:52 +0900

    Update 2553.java

--- 1-5000/2553.java
+++ 1-5000/2553.java
@@ -3,21 +3,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int n = Integer.parseInt(br.readLine());
-		
+
 		long sum = 1;
-		for(int i = 1; i <= n; i++) {
-			sum *= i;
-			sum %= 10000000;
-			
-			while(sum % 10 == 0) {
-				sum/=10;
+		for (int i = 1; i <= n; i++) {
+			sum = sum * i % 10000000;
+
+			while (sum % 10 == 0) {
+				sum /= 10;
 			}
 		}
-		
-		System.out.println(sum%10);
+
+		System.out.println(sum % 10);
 	}
 }
