190601af2dccc844be7a9ccab1640d69d057b8e8
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-10-15 14:28:18 +0900

    Update 1252.java

--- 1-5000/1252.java
+++ 1-5000/1252.java
@@ -9,9 +9,6 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		BigInteger a = new BigInteger(st.nextToken(),2);
-		BigInteger b = new BigInteger(st.nextToken(),2);
-		
-		System.out.println(a.add(b).toString(2));
+		System.out.print(new BigInteger(st.nextToken(),2).add(new BigInteger(st.nextToken(),2)).toString(2));
 	}
 }
