41984ef242418ba97d016c4dff640a6ca401d82e
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-10-17 14:19:48 +0900

    Update 1735.java

--- 1-5000/1735.java
+++ 1-5000/1735.java
@@ -7,8 +7,7 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int[] a = new int[2];
-		int[] b = new int[2];
+		int[] a = new int[2], b = new int[2];
 		
 		for(int i = 0; i < 2; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
@@ -16,19 +15,18 @@ public class Main {
 			b[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		int min = gcd(b[0],b[1]);
-		int max = b[0] * b[1]/ min;
+		int max = b[0] * b[1]/ gcd(b[0],b[1]);
 		int num = max / b[0] * a[0] + max / b[1] * a[1];
 		
 		int gcd = gcd(num, max);
 		
-		System.out.println(num/gcd + " " + max/gcd);
+		System.out.print(num/gcd + " " + max/gcd);
 	}
 	
 	public static int gcd(int a, int b) {
 		if(b == 0) {
 			return a;
 		}
-		return gcd(b, a%b);
+		return gcd(b, a % b);
 	}
 }
