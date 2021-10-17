90ece8b637455eae1b99002665cf3093bd14ca3f
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-10-17 17:51:57 +0900

    Update 16430.java

--- 15001-20000/16430.java
+++ 15001-20000/16430.java
@@ -4,12 +4,11 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		System.out.println(b-a + " " + b);
+
+		int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+		System.out.println(b - a + " " + b);
 	}
 }
