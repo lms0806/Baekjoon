c51dcf544e9ae35df95a1b8aaffe84348330be06
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-10-17 17:53:23 +0900

    Update 1543.java

--- 1-5000/1543.java
+++ 1-5000/1543.java
@@ -6,8 +6,7 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		String a = br.readLine();
-		String b = br.readLine();
+		String a = br.readLine(), b = br.readLine();
 		
 		int answer = 0;
 		while(a.contains(b)) {
