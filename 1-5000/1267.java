7d16d9d9850400b10ad5608361d56f249a8307e2
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-10-17 14:21:40 +0900

    Update 1267.java

--- 1-5000/1267.java
+++ 1-5000/1267.java
@@ -1,40 +1,25 @@
-package 백준;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int size = Integer.parseInt(br.readLine());
-		
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = 0, n1 = 0;
-		for(int i = 0; i < size; i++) {
+		while(size --> 0) {
 			int a = Integer.parseInt(st.nextToken());
-			n += a/30+1;
-			n1 += a/60+1;
+			n += a / 30 + 1;
+			n1 += a / 60 + 1;
 		}
-		
-		n*=10;
-		n1*=15;
-		
-		StringBuilder sb = new StringBuilder();
-		if(n > n1) {
-			sb.append("M ").append(n1);
-		}
-		else if(n == n1) {
-			sb.append("Y M ").append(n);
-		}
-		else {
-			sb.append("Y ").append(n);
-		}
-		System.out.print(sb);
+
+		n *= 10;
+		n1 *= 15;
+
+		System.out.print(n > n1 ? "M " + n1 : n == n1 ? "Y M " + n : "Y " + n);
 	}
 }
-// throws NumberFormatException,IOException
-//StringTokenizer st = new StringTokenizer(br.readLine());
-//BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
