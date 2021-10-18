9bfdb67fe916e5571153a840f4c5a7fd4b75f368
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-10-18 13:48:20 +0900

    Update 2455.java

--- 1-5000/2455.java
+++ 1-5000/2455.java
@@ -10,12 +10,11 @@ public class Main {
 		int num = 0, sum = 0;
 		for(int i = 0; i < 4; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			sum -= Integer.parseInt(st.nextToken());
-			sum += Integer.parseInt(st.nextToken());
+			sum = sum - Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
 			if(sum > num) {
 				num = sum;
 			}
 		}
-		System.out.println(num);
+		System.out.print(num);
 	}
 }
