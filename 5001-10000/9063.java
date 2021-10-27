77652595c40e4928920f187ee0c2716fe1c3f4ef
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-10-27 14:37:16 +0900

    Create 9063.java

---
+++ 5001-10000/9063.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] x = new int[n], y = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			x[i] = Integer.parseInt(st.nextToken());
+			y[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(x);
+		Arrays.sort(y);
+		
+		System.out.print((x[n - 1] - x[0]) * (y[n - 1] - y[0]));
+	}
+}
