967497d4b38031b360f05735c2c338fde0c75c60
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-10-30 15:13:24 +0900

    Create 23343.java

---
+++ 20001-25000/23343.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		String a = st.nextToken(), b = st.nextToken();
+		try {
+			System.out.print(Integer.parseInt(a) - Integer.parseInt(b));
+		}catch(Exception e) {
+			System.out.print("NaN");
+		}
+	}
+}
