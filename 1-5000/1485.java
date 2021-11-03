7812c037e863aef51b72f7e2bf36419c5d9950fb
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-11-03 21:45:20 +0900

    Create 1485.java

---
+++ 1-5000/1485.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(t --> 0) {
+			long[][] n = new long[4][2];
+			for(int i = 0; i < 4; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				n[i][0] = Long.parseLong(st.nextToken());
+				n[i][1] = Long.parseLong(st.nextToken());
+			}
+			
+			long[] arr = new long[6];
+			int k = 0;
+			for(int i = 0; i < 3; i++) {
+				for(int j = i + 1; j < 4; j++) {
+					arr[k++] = (n[i][0] - n[j][0]) * (n[i][0] - n[j][0]) + (n[i][1] - n[j][1]) * (n[i][1] - n[j][1]);
+				}
+			}
+			
+			Arrays.sort(arr);
+			
+			sb.append(arr[0] == arr[1] && arr[1] == arr[2] && arr[2] == arr[3] && arr[4] == arr[5] ? 1 : 0).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
