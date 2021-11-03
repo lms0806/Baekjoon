526d754460d908225af6b2306214acc5699160e4
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-11-03 17:19:35 +0900

    Create 23303.java

---
+++ 20001-25000/23303.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String s = br.readLine();
+		System.out.print(s.contains("d2") || s.contains("D2") ? "D2" : "unrated");
+	}
+}
