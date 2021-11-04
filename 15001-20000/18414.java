7f92d4229c9ed7be9b774df31fe882566656724c
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-11-04 14:01:40 +0900

    Update 18414.java

--- 15001-20000/18414.java
+++ 15001-20000/18414.java
@@ -4,22 +4,19 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int x = Integer.parseInt(st.nextToken());
-		int l = Integer.parseInt(st.nextToken());
-		int r = Integer.parseInt(st.nextToken());
-		
+
+		int x = Integer.parseInt(st.nextToken()), l = Integer.parseInt(st.nextToken()), r = Integer.parseInt(st.nextToken());
+
 		int min = 100000, answer = 0;
-		for(int i = l; i <= r; i++) {
-			if(Math.abs(x-i) < min) {
-				min = Math.abs(x-i);
+		for (int i = l; i <= r; i++) {
+			if (Math.abs(x - i) < min) {
+				min = Math.abs(x - i);
 				answer = i;
 			}
 		}
-		
-		System.out.println(answer);
+		System.out.print(answer);
 	}
 }
