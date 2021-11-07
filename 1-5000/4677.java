4ae7f7dcca10811b526d03e57566ad063baae52a
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-11-07 19:15:03 +0900

    Create 4677.java

---
+++ 1-5000/4677.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[] dx = {1, 0, -1, 0, 1, -1, -1, 1};
+	static int[] dy = {0, -1, 0, 1, -1, -1, 1, 1};
+	static char[][] board;
+	static boolean[][] visited;
+	static int r, c;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringBuilder sb = new StringBuilder();
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			r = Integer.parseInt(st.nextToken());
+			c = Integer.parseInt(st.nextToken());
+			
+			if(r == 0 && c == 0) {
+				break;
+			}
+			
+			board = new char[r][c];
+			visited = new boolean[r][c];
+			
+			for(int i = 0; i < r; i++) {
+				board[i] = br.readLine().toCharArray();
+			}
+			
+			int answer = 0;
+			for(int i = 0; i < r; i++) {
+				for(int j = 0; j < c; j++) {
+					if(!visited[i][j] && board[i][j] == '@') {
+						bfs(i, j);
+						answer++;
+					}
+				}
+			}
+			sb.append(answer).append("\n");
+		}
+		System.out.print(sb);
+	}
+	
+	public static void bfs(int x, int y) {
+		Queue<int[]> queue = new LinkedList<>();
+		queue.add(new int[] {x, y});
+		visited[x][y] = true;
+		
+		while(!queue.isEmpty()) {
+			int[] now = queue.poll();
+			
+			for(int i = 0; i < 8; i++) {
+				int nx = now[0] + dx[i];
+				int ny = now[1] + dy[i];
+				
+				if(nx >= 0 && nx < r && ny >= 0 && ny < c) {
+					if(board[nx][ny] == '@' && !visited[nx][ny]) {
+						visited[nx][ny] = true;
+						queue.add(new int[] {nx, ny});
+					}
+				}
+			}
+		}
+	}
+}
