6418e4abbdf5bcc1e17036b738430e4c8d767342
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-11-08 23:05:03 +0900

    Create 14264.java

---
+++ 10001-15000/14264.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.print(Math.pow(3, 0.5) / 4 * Math.pow(Integer.parseInt(br.readLine()), 2));
+	}
+}
