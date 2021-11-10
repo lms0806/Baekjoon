67149f69478bba3a7fff368b2be63e2db0239f8f
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-11-10 23:02:33 +0900

    Create 1303.java

---
+++ 1-5000/1303.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[] dx = {1, 0, -1, 0};
+	static int[] dy = {0, -1, 0, 1};
+	static int n, m;
+	static char[][] board;
+	static boolean[][] visited;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		m = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
+		
+		board = new char[n][m];
+		visited = new boolean[n][m];
+		
+		for(int i = 0; i < n; i++) {
+			board[i] = br.readLine().toCharArray();
+		}
+		
+		int w = 0, b = 0;
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				if(!visited[i][j]) {
+					int n = bfs(i, j);
+					
+					if(board[i][j] == 'W') {
+						w += n * n;
+					}
+					else {
+						b += n * n;
+					}
+				}
+			}
+		}
+		System.out.print(w + " " + b);
+	}
+	
+	public static int bfs(int x, int y) {
+		Queue<int[]> queue = new LinkedList<>();
+		queue.add(new int[] {x, y});
+		visited[x][y] = true;
+		char target = board[x][y];
+		
+		int answer = 0;
+		while(!queue.isEmpty()) {
+			int[] now = queue.poll();
+			
+			answer++;
+			for(int i = 0; i < 4; i++) {
+				int nx = now[0] + dx[i];
+				int ny = now[1] + dy[i];
+				
+				if(nx >= 0 && nx < n && ny >= 0 && ny < m) {
+					if(!visited[nx][ny] && board[nx][ny] == target) {
+						queue.add(new int[] {nx, ny});
+						visited[nx][ny] = true;
+					}
+				}
+			}
+		}
+		return answer;
+	}
+}
