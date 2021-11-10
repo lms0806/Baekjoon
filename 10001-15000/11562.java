4069c576294d18f8de540a88f90c96ab3dc7d7f2
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-11-10 17:42:53 +0900

    Create 11562.java

---
+++ 10001-15000/11562.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int INF = 987654321;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
+		
+		int[][] arr = new int[n + 1][n + 1];
+		
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= n; j++) {
+				if(i != j) {
+					arr[i][j] = INF;
+				}
+			}
+		}
+		
+		for(int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+			int cost = Integer.parseInt(st.nextToken());
+			
+			arr[a][b] = 0;
+			arr[b][a] = cost == 0 ? 1 : 0;
+		}
+		
+		for(int k = 1; k <= n; k++) {
+			for(int i = 1; i <= n; i++) {
+				for(int j = 1; j <= n; j++) {
+					if(arr[i][j] > arr[i][k] + arr[k][j]) {
+						arr[i][j] = arr[i][k] + arr[k][j];
+					}
+				}
+			}
+		}
+		
+		int k = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(k --> 0) {
+			st = new StringTokenizer(br.readLine());
+			sb.append(arr[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())]).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
