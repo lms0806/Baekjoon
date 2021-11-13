16a52eaac7eab87532dc74af482d69636c8c1f25
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-11-13 20:30:12 +0900

    Update 15792.java

--- 15001-20000/15792.java
+++ 15001-20000/15792.java
@@ -1,16 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		double a = Double.parseDouble(st.nextToken());
-		double b = Double.parseDouble(st.nextToken());
-		
-		System.out.println(a/b);
+		System.out.print(new BigDecimal(st.nextToken()).divide(new BigDecimal(st.nextToken()) , 1001 , BigDecimal.ROUND_DOWN));
 	}
 }
