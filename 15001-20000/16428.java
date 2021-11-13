2667e3cced11d44a19d61fef59dd2c6cf7cfb0c3
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-11-13 17:57:34 +0900

    Update 16428.java

--- 15001-20000/16428.java
+++ 15001-20000/16428.java
@@ -6,13 +6,26 @@ import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		BigInteger a = new BigInteger(st.nextToken());
 		BigInteger b = new BigInteger(st.nextToken());
+		BigInteger div = a.divide(b);
+		BigInteger rem = a.remainder(b);
 		
-		System.out.println(a.divide(b));
-		System.out.println(a.remainder(b));
+		StringBuilder sb = new StringBuilder();
+		if(a.compareTo(BigInteger.ZERO) < 0 && b.compareTo(BigInteger.ZERO) > 0) {
+			div = div.subtract(BigInteger.ONE);
+			sb.append(div).append("\n").append(a.subtract(b.multiply(div)));
+		}
+		else if(a.compareTo(BigInteger.ZERO) < 0 && b.compareTo(BigInteger.ZERO) < 0) {
+			div = div.add(BigInteger.ONE);
+			sb.append(div).append("\n").append(a.subtract(b.multiply(div)));
+		}
+		else {
+			sb.append(div).append("\n").append(rem);
+		}
+		System.out.print(sb);
 	}
 }
