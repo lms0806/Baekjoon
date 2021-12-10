50e6f7a30e51646c8fa8538244b3532c3a5b35ec
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-10 17:02:32 +0900

    Create 14217.java

---
+++ 10001-15000/14217.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n, m;
+	static int[][] arr;
+	static int[] num;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		arr = new int[n + 1][n + 1];
+		
+		for(int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+			
+			arr[a][b] = arr[b][a] = 1;
+		}
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(t --> 0) {
+			st = new StringTokenizer(br.readLine());
+			int check = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+			
+			arr[a][b] = arr[b][a] = check == 1 ? 1 : 0;
+			
+			num = new int[n + 1];
+			Arrays.fill(num, -1);
+			
+			bfs();
+			
+			for(int i = 1; i <= n; i++) {
+				sb.append(num[i]).append(" ");
+			}
+			sb.append("\n");
+		}
+		System.out.print(sb);
+	}
+	
+	public static void bfs() {
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(1);
+		num[1] = 0;
+		
+		while(!queue.isEmpty()) {
+			int now = queue.poll();
+			
+			for(int i = 1; i <= n; i++) {
+				if(arr[now][i] == 1 && num[i] == -1) {
+					num[i] = num[now] + 1;
+					queue.add(i);
+				}
+			}
+		}
+	}
+}
