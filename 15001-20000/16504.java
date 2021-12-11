0746432af6746e5b9d80aefe228b8e0a87383552
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-11 16:33:51 +0900

    Create 16504.java

---
+++ 15001-20000/16504.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		long answer = 0;
+		for(int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < n; j++) {
+				answer += Integer.parseInt(st.nextToken());
+			}
+		}
+		System.out.print(answer);
+	}
+}
