fe0d09d6d25b9e47106cb988fa686251e5f4ec3e
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-11 16:08:46 +0900

    Update 10718.java

--- 10001-15000/10718.java
+++ 10001-15000/10718.java
@@ -1,7 +1,5 @@
 public class Main {
 	public static void main(String[] args) {
-		StringBuilder sb = new StringBuilder();
-		sb.append("강한친구 대한육군").append("\n").append("강한친구 대한육군");
-		System.out.print(sb);
+		System.out.print("강한친구 대한육군\n".repeat(2));
 	}
 }
