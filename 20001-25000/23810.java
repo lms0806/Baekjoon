4111034c819ba0b65508a55523c4628ea03a12d4
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-12 13:03:34 +0900

    Create 23810.java

---
+++ 20001-25000/23810.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		String[] db = {"@@@@@"
+				, "@"
+				, "@@@@@"
+				, "@"
+				, "@"};
+		int n = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < n; j++) {
+				for(int k = 0; k < db[i].length(); k++) {
+					for(int w = 0; w < n; w++) {
+						sb.append(db[i].charAt(k));
+					}
+				}
+				sb.append("\n");
+			}
+		}
+		System.out.print(sb);
+	}
+}
