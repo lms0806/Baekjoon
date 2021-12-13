9ebfa0920f6605def9817a2f1eab0c3d960d4df7
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-13 23:18:50 +0900

    Create 5054.java

---
+++ 5001-10000/5054.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(t --> 0) {
+			int n = Integer.parseInt(br.readLine());
+			
+			int[] arr = new int[n];
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < n; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			Arrays.sort(arr);
+			
+			int sum = arr[n - 1] - arr[0];
+			for(int i = 1; i < n; i++) {
+				sum += arr[i] - arr[i - 1];
+			}
+			sb.append(sum).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
