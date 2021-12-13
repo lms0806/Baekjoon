ae0e7c265b674511262cfdc9453b7272a4db9b87
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-13 23:23:20 +0900

    Update 9713.java

--- 5001-10000/9713.java
+++ 5001-10000/9713.java
@@ -6,18 +6,16 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int size = Integer.parseInt(br.readLine());
+		int t = Integer.parseInt(br.readLine());
 		
-		int num = 0;
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < size; i++) {
-			num = Integer.parseInt(br.readLine());
+		while(t --> 0) {
+			int num = Integer.parseInt(br.readLine());
 			
 			int result = 0;
 			for(int j = 1; j <= num; j+=2) {
 				result += j;
 			}
-			
 			sb.append(result).append("\n");
 		}
 		System.out.print(sb);
