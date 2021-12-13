d2c55cbe8977c657a664c8c5e20d0dba22f85e2c
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-13 23:26:29 +0900

    Create 23827.java

---
+++ 20001-25000/23827.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		long sum = 0, sub = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			sum = (sum + sub * num) % 1000000007;
+			sub += num;
+		}
+		System.out.print(sum);
+	}
+}
