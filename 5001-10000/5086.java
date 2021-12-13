06903b1293fec88881d2339b603e62bba092fb0e
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-13 23:12:27 +0900

    Update 5086.java

--- 5001-10000/5086.java
+++ 5001-10000/5086.java
@@ -7,28 +7,16 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int a = 0, b = 0;
-		
 		StringBuilder sb = new StringBuilder();
 		while(true) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
 			
 			if(a == 0 && b == 0) {
 				break;
 			}
 			
-			if(a%b == 0 && a/b != 0) {
-				sb.append("multiple");
-			}
-			else if(b%a == 0 && b/a != 0) {
-				sb.append("factor");
-			}
-			else {
-				sb.append("neither");
-			}
-			sb.append("\n");
+			sb.append(a % b == 0 ? "multiple" : b % a == 0 ? "factor" : "neither").append("\n");
 		}
 		System.out.print(sb);
 	}
