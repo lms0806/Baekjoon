d9b51784c13a8769909ebbb8f90ed62732371593
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-18 18:50:38 +0900

    Update 10808.java

--- 10001-15000/10808.java
+++ 10001-15000/10808.java
@@ -5,19 +5,17 @@ import java.io.InputStreamReader;
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-	
-		String str = br.readLine();
 		
 		int[] answer = new int[26];
 		
-		for(int i = 0; i < str.length(); i++) {
-			answer[str.charAt(i) - 'a']++;
+		for(char c : br.readLine().toCharArray()) {
+			answer[c - 'a']++;
 		}
 		
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < answer.length; i++) {
-			sb.append(answer[i]).append(" ");
+		for(int a : answer) {
+			sb.append(a).append(" ");
 		}
-		System.out.println(sb);
+		System.out.print(sb);
 	}
 }
