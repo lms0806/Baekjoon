679282d0020168d80ab993e9b6930cff3b910e00
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-18 18:53:04 +0900

    Update 10757.java

--- 10001-15000/10757.java
+++ 10001-15000/10757.java
@@ -8,10 +8,7 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		BigInteger a = new BigInteger(st.nextToken());
-		BigInteger b = new BigInteger(st.nextToken());
-	    
-        	System.out.print(a.add(b));
+
+		System.out.print(new BigInteger(st.nextToken()).add(new BigInteger(st.nextToken())));
 	}
 }
