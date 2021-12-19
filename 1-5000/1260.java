9515a6b12ef1774fa549249f69a943c86eb905b2
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-19 16:21:12 +0900

    Update 1260.java

--- 1-5000/1260.java
+++ 1-5000/1260.java
@@ -9,24 +9,21 @@ public class Main {
 	public static int[][] arr;
 	public static boolean[] visited;
 	public static StringBuilder sb = new StringBuilder();
-	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int point = Integer.parseInt(st.nextToken());
 		int line = Integer.parseInt(st.nextToken());
-		int start = Integer.parseInt(st.nextToken())-1;
+		int start = Integer.parseInt(st.nextToken()) - 1;
 		
 		arr = new int[point][point];
 		
 		while(line --> 0 ) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken())-1;
-			int b = Integer.parseInt(st.nextToken())-1;
+			int a = Integer.parseInt(st.nextToken()) - 1, b = Integer.parseInt(st.nextToken()) - 1;
 			
-			arr[a][b] = 1;
-			arr[b][a] = 1;
+			arr[a][b] = arr[b][a] = 1;
 		}
 		
 		visited = new boolean[point];
@@ -42,14 +39,14 @@ public class Main {
 	
 	public static void dfs(int start) {
 		visited[start] = true;
-		sb.append(start+1).append(" ");
+		sb.append(start + 1).append(" ");
 		
 		if(start == arr.length) {
 			return;
 		}
 		
 		for(int i = 0; i < arr.length; i++) {
-			if(arr[start][i] == 1 && !visited[i]) {
+			if(!visited[i] && arr[start][i] == 1) {
 				dfs(i);
 			}
 		}
@@ -57,27 +54,25 @@ public class Main {
 	
 	public static void bfs(int start) {
 		Queue<Integer> que = new LinkedList<Integer>();
-		
 		que.add(start);
 		visited[start] = true;
-		sb.append(start+1).append(" ");
+		sb.append(start + 1).append(" ");
 		
 		while(!que.isEmpty()) {
 			int count = 0;
-			int temp = que.peek();
-			que.poll();
+			int temp = que.poll();
 			for(int i = 0; i < arr.length; i++) {
-				if(arr[temp][i] == 1 && !visited[i]) {
+				if(!visited[i] && arr[temp][i] == 1) {
 					que.add(i);
 					visited[i] = true;
-					sb.append(i+1).append(" ");
+					sb.append(i + 1).append(" ");
 				}
 				if(visited[i]) {
 					count++;
 				}
 			}
 			if(count == arr.length) {
-				break;
+				return;
 			}
 		}
 	}
