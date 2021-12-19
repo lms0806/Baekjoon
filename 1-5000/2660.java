7b2911e50761376bbf19edf78770e92f73174781
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-20 00:32:47 +0900

    Update 2660.java

--- 1-5000/2660.java
+++ 1-5000/2660.java
@@ -6,13 +6,12 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static int INF = 987654321;
-	static int[][] arr;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine());
 		
-		arr = new int[n + 1][n + 1];
+		int[][] arr = new int[n + 1][n + 1];
 		
 		for(int i = 1; i <= n; i++) {
 			for(int j = 1; j <= n; j++) {
