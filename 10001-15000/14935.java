a3c4135f7d3ea40146b1bac844c50410aa318b2d
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-20 00:49:26 +0900

    Update 14935.java

--- 10001-15000/14935.java
+++ 10001-15000/14935.java
@@ -1,12 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		
-		br.readLine();
+	public static void main(String[] args){
 		System.out.print("FA");
 	}
 }
