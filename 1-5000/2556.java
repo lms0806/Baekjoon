9f079a2f5f975da1fbfa501cd0cce9de969c6ed8
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-19 16:16:57 +0900

    Update 2556.java

--- 1-5000/2556.java
+++ 1-5000/2556.java
@@ -6,15 +6,12 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-        int n = Integer.parseInt(br.readLine());
+        	int n = Integer.parseInt(br.readLine());
         
-        StringBuilder sb = new StringBuilder();
+        	StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++){
-                sb.append("*");
-            }
-            sb.append("\n");
+			sb.append("*".repeat(n)).append("\n");
 		}
-        System.out.print(sb);
+        	System.out.print(sb);
 	}
 }
