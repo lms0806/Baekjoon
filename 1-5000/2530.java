c37fdea6cb9932d00dd74d5f0f728220a4c84683
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-20 00:31:46 +0900

    Update 2530.java

--- 1-5000/2530.java
+++ 1-5000/2530.java
@@ -4,25 +4,23 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException,IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
+		int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken()), c = Integer.parseInt(st.nextToken());
 		int d = Integer.parseInt(br.readLine());
-		
+
 		c += d;
-		
-		b += c/60;
-		c%=60;
-		
-		a += b/60;
+
+		b += c / 60;
+		c %= 60;
+
+		a += b / 60;
 		b %= 60;
-		
+
 		a %= 24;
-		
-		System.out.println(a + " " + b + " " + c);
+
+		System.out.print(a + " " + b + " " + c);
 	}
 }
