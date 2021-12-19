79a297d01e6072707307270732e108bd95572ab4
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-19 21:43:29 +0900

    Update 8393.java

--- 5001-10000/8393.java
+++ 5001-10000/8393.java
@@ -6,11 +6,11 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int a = Integer.parseInt(br.readLine());
-		int sum = 0;
+		int a = Integer.parseInt(br.readLine()) + 1;
 		
-		for(int i = 1; i <= a; i++) {
-			sum += i;
+		int sum = 0;
+		while(a --> 0) {
+			sum += a;
 		}
 		System.out.print(sum);
 	}
