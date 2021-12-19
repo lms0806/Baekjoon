5728e597071daecf3002ffa496664a51ee5dd31b
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-19 21:37:14 +0900

    Update 10952.java

--- 10001-15000/10952.java
+++ 10001-15000/10952.java
@@ -7,20 +7,15 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 
-		int a = 0, b = 0;
-		
 		StringBuilder sb = new StringBuilder();
 		while(true) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
 			
 			if(a == 0 && b == 0) {
 				break;
 			}
-			else {
-				sb.append(a+b).append("\n");
-			}
+			sb.append(a + b).append("\n");
 		}
 		System.out.print(sb);
 	}
