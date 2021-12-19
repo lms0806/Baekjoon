981f6f12709839d6a058800187c978726d91bc6c
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-19 21:32:11 +0900

    Update 10989.java

--- 10001-15000/10989.java
+++ 10001-15000/10989.java
@@ -6,11 +6,11 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int size = Integer.parseInt(br.readLine());
+		int t = Integer.parseInt(br.readLine());
 		
 		int[] num = new int[10001];
 		
-		while(size --> 0) {
+		while(t --> 0) {
 			num[Integer.parseInt(br.readLine())]++;
 		}
 		
