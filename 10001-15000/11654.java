933502b80bd1fe24bfa4f81b27734005f45b113b
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-19 21:21:52 +0900

    Update 11654.java

--- 10001-15000/11654.java
+++ 10001-15000/11654.java
@@ -6,6 +6,6 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		System.out.println((int)br.readLine().charAt(0));
+		System.out.print((int)br.read());
 	}
 }
