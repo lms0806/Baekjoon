a1ecc0ca46ffc905585a6344952dd2c94e7b89d7
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-20 20:52:07 +0900

    Create 2573.java

---
+++ 1-5000/2573.java
@@ -0,0 +1,104 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[] dx = {1, 0, -1, 0};
+	static int[] dy = {0, -1, 0, 1};
+	static int n, m;
+	static int[][] arr;
+	static boolean[][] visited;
+	public static void main(String[] args) throws NumberFormatException,IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		arr = new int[n][m];
+		
+		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < m; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int year = 0;
+		while(true) {
+			visited = new boolean[n][m];
+			int count = 0;
+			
+			for(int i = 0; i < n; i++) {
+				for(int j = 0; j < m; j++) {
+					if(!visited[i][j] && arr[i][j] != 0) {
+						bfs(i, j);
+						count++;
+					}
+				}
+			}
+			
+			if(count >= 2) {
+				break;
+			}
+			if(count == 0) {
+				year = 0;
+				break;
+			}
+			
+			int[][] arr1 = new int[n][m];
+			
+			for(int i = 0; i < n; i++) {
+				for(int j = 0; j < m; j++) {
+					if(arr[i][j] != 0) {
+						int num = 0;
+						for(int k = 0; k < 4; k++) {
+							int nx = i + dx[k];
+							int ny = j + dy[k];
+							
+							if(nx >= 0 && nx < n && ny >= 0 && ny < m) {
+								if(arr[nx][ny] == 0) {
+									num++;
+								}
+							}
+						}
+						arr1[i][j] = arr[i][j] - num;
+						if(arr1[i][j] < 0) {
+							arr1[i][j] = 0;
+						}
+					}
+				}
+			}
+			
+			arr = arr1.clone();
+			
+			year++;
+		}
+		System.out.print(year);
+	}
+	
+	public static void bfs(int x, int y) {
+		Queue<int[]> queue = new LinkedList<>();
+		queue.add(new int[] {x, y});
+		visited[x][y] = true;
+		
+		while(!queue.isEmpty()) {
+			int[] now = queue.poll();
+			
+			for(int i = 0; i < 4; i++) {
+				int nx = now[0] + dx[i];
+				int ny = now[1] + dy[i];
+				
+				if(nx >= 0 && nx < n && ny >= 0 && ny < m) {
+					if(!visited[nx][ny] && arr[nx][ny] != 0) {
+						visited[nx][ny] = true;
+						queue.add(new int[] {nx, ny});
+					}
+				}
+			}
+		}
+	}
+}
