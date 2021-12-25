efe69fb1d996ff543c32b1653bef9d4061af6d0b
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-25 23:41:13 +0900

    Update 5532.java

--- 5001-10000/5532.java
+++ 5001-10000/5532.java
@@ -3,40 +3,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int l = Integer.parseInt(br.readLine());
 		int a = Integer.parseInt(br.readLine());
 		int b = Integer.parseInt(br.readLine());
 		int c = Integer.parseInt(br.readLine());
 		int d = Integer.parseInt(br.readLine());
+
+		int num1 = a / c + (a % c != 0 ? 1 : 0);
+		int num2 = b / d + (b % d != 0 ? 1 : 0);
 		
-		int num1 = 0;
-		int num2 = 0;
-		int result = 0;
-		
-		if(a%c != 0) {
-			num1= a/c +1;
-		}
-		else {
-			num1 = a/c;
-		}
-		
-		if(b%d != 0) {
-			num2 = b/d + 1;
-		}
-		else {
-			num2 = b/d;
-		}
-		
-		if(num1 > num2) {
-			result = l - num1;
-		}
-		else {
-			result = l - num2;
-		}
-		
-		System.out.println(result);
+		System.out.println(l - Math.max(num1, num2));
 	}
 }
