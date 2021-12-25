f16b9b74276fff7a9ab6adbc7bcae227035312af
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-25 14:35:17 +0900

    Update 21567.java

--- 20001-25000/21567.java
+++ 20001-25000/21567.java
@@ -1,22 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int[] number = new int[10];
-		BigInteger sum = new BigInteger(br.readLine()).multiply(new BigInteger(br.readLine())).multiply(new BigInteger(br.readLine()));
-		
-		while(sum.compareTo(BigInteger.ZERO) != 0) {
-			number[sum.remainder(BigInteger.TEN).intValue()]++;
-			sum = sum.divide(BigInteger.TEN);
+		long sum = Long.parseLong(br.readLine()) * Long.parseLong(br.readLine()) * Long.parseLong(br.readLine());
+
+		while (sum != 0) {
+			number[(int)(sum % 10)]++;
+			sum /= 10;
 		}
-		
+
 		StringBuilder sb = new StringBuilder();
-		for(int n : number) {
+		for (int n : number) {
 			sb.append(n).append("\n");
 		}
 		System.out.print(sb);
