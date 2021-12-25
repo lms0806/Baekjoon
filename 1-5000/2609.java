04e084845bdc339e91c786a92164ab5cb6f675fe
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-25 23:44:26 +0900

    Update 2609.java

--- 1-5000/2609.java
+++ 1-5000/2609.java
@@ -5,9 +5,9 @@ import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
 
 		int min = gcd(a,b);
