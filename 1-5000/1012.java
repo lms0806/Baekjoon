db550aa508747fd0134ec0986613ec0f9bdc97fd
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-25 23:58:16 +0900

    Update 1012.java

--- 1-5000/1012.java
+++ 1-5000/1012.java
@@ -1,25 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int a,b;
-	static boolean[][] check, arr;
+	static int[] dx = {1, 0, -1, 0};
+	static int[] dy = {0, -1, 0, 1};
+	static int n, m;
+	static boolean[][] visited, arr;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		int size = Integer.parseInt(br.readLine());
+		int t = Integer.parseInt(br.readLine());
 		
 		StringBuilder sb = new StringBuilder();
-		while(size --> 0) {
+		while(t --> 0) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+			n = Integer.parseInt(st.nextToken());
 			int count = Integer.parseInt(st.nextToken());
 			
-			arr = new boolean[b][a];
-			check = new boolean[b][a];
+			arr = new boolean[n][m];
+			visited = new boolean[n][m];
 			while(count --> 0) {
 				st = new StringTokenizer(br.readLine());
 				int x = Integer.parseInt(st.nextToken()), y = Integer.parseInt(st.nextToken());
@@ -28,9 +32,9 @@ public class Main {
 			}
 			
 			int answer = 0;
-			for(int i = 0; i < b; i++) {
-				for(int j = 0; j < a; j++) {
-					if(checktrue(i,j)) {
+			for(int i = 0; i < n; i++) {
+				for(int j = 0; j < m; j++) {
+					if(!visited[i][j] && arr[i][j]) {
 						answer++;
 						bfs(i,j);
 					}
@@ -41,27 +45,23 @@ public class Main {
 		System.out.print(sb);
 	}
 	
-	public static boolean checktrue(int row, int col) {
-		if(row < 0 || row >= b || col < 0 || col >= a || check[row][col] || !arr[row][col]) {
-			return false;
-		}
-		return true;
-	}
-	
-	public static void bfs(int row, int col) {
-		check[row][col] = true;
+	public static void bfs(int x, int y) {
+		Queue<int[]> queue = new LinkedList<>();
+		queue.add(new int[] {x, y});
+		visited[x][y] = true;
 		
-		if(checktrue(row - 1, col)) {
-			bfs(row - 1, col);
-		}
-		if(checktrue(row, col - 1)) {
-			bfs(row, col - 1);
-		}
-		if(checktrue(row + 1, col)) {
-			bfs(row + 1, col);
-		}
-		if(checktrue(row, col + 1)) {
-			bfs(row, col + 1);
+		while(!queue.isEmpty()) {
+			int[] now = queue.poll();
+			
+			for(int i = 0; i < 4; i++) {
+				int nx = now[0] + dx[i];
+				int ny = now[1] + dy[i];
+				
+				if(nx >= 0 && nx < n && ny >= 0 && ny < m && !visited[nx][ny] && arr[nx][ny]) {
+					visited[nx][ny] = true;
+					queue.add(new int[] {nx, ny});
+				}
+			}
 		}
 	}
 }
