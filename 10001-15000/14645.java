8a310071b9b1c66a4967a74627a1dc86f38173a2
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-12-27 17:24:55 +0900

    Update 14645.java

--- 10001-15000/14645.java
+++ 10001-15000/14645.java
@@ -6,15 +6,7 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int a = Integer.parseInt(st.nextToken());
-		st.nextToken();
-		
-		while(a --> 0) {
-			br.readLine();
-		}
-		
+
 		System.out.print("비와이");
 	}
 }
