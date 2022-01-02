ae904107a778c16558350b7bacf7a9ed53a9e210
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-01-02 11:44:01 +0900

    Create 23968.java

---
+++ 20001-25000/23968.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n, k;
+	static int[] arr;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		
+		arr = new int[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		System.out.print(solve());
+	}
+	
+	public static String solve() {
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n - i - 1; j++) {
+				if(arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+					
+					if(--k == 0) {
+						return arr[j] + " " + arr[j + 1];
+					}
+				}
+			}
+		}
+		return "-1";
+	}
+}
