b1bf953e565784039540594838b0065cd779bc0e
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-01-05 23:59:53 +0900

    Create 14284.java

---
+++ 10001-15000/14284.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+class Node implements Comparable<Node>{
+    int to, weight;
+
+    public Node(int to, int weight){
+        this.to = to;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return weight - o.weight;
+    }
+}
+
+public class Main {
+	static int[] dist;
+	static List<Node>[] list;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int v = Integer.parseInt(st.nextToken()), e = Integer.parseInt(st.nextToken());
+		
+		dist = new int[v + 1];
+		list = new ArrayList[v + 1];
+		
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		
+		for(int i = 1; i <= v; i++) {
+			list[i] = new ArrayList<>();
+		}
+		
+		for(int i = 0; i < e; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			list[a].add(new Node(b, c));
+			list[b].add(new Node(a, c));
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		dijkstra(v, Integer.parseInt(st.nextToken()));
+		
+		System.out.print(dist[Integer.parseInt(st.nextToken())]);
+	}
+	
+	public static void dijkstra(int v, int start) {
+		PriorityQueue<Node> queue = new PriorityQueue<>();
+		boolean[] check = new boolean[v + 1];
+		queue.add(new Node(start, 0));
+		dist[start] = 0;
+		
+		while(!queue.isEmpty()) {
+			int cur = queue.poll().to;
+			
+			if(check[cur]) {
+				continue;
+			}
+			
+			check[cur] = true;
+				
+			for(Node node : list[cur]) {
+				if(dist[node.to] > dist[cur] + node.weight) {
+					dist[node.to] = dist[cur] + node.weight;
+					queue.add(new Node(node.to, dist[node.to]));
+				}
+			}
+		}
+	}
+}
