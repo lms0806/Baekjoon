178d29111ffce86e508cfeb9de0168c78027d1c4
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-01-09 17:48:57 +0900

    Update 2720.java

--- 1-5000/2720.java
+++ 1-5000/2720.java
@@ -3,24 +3,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int t = Integer.parseInt(br.readLine());
 		
-		int size = Integer.parseInt(br.readLine());
-		int a;
-		int quarter = 0, dime = 0, nickel = 0, penny = 0;
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < size; i++) {
-			a = Integer.parseInt(br.readLine());
-			quarter = a/25;
-			a%=25;
-			dime = a/10;
-			a%=10;
-			nickel = a/5;
-			a%=5;
-			penny = a;
-			sb.append(quarter).append(" ").append(dime).append(" ").append(nickel).append(" ").append(penny).append("\n");
+		while (t --> 0) {
+			int a = Integer.parseInt(br.readLine());
+			
+			sb.append(a / 25).append(" ");
+			
+			a %= 25;
+			
+			sb.append(a / 10).append(" ");
+			
+			a %= 10;
+			
+			sb.append(a / 5).append(" ");
+			
+			a %= 5;
+			
+			sb.append(a).append("\n");
 		}
-		System.out.println(sb);
+		System.out.print(sb);
 	}
 }
