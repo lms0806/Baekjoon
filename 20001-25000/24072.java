96673549184393ef0ed65a24a695f1ac24456455
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-01-11 12:33:34 +0900

    Create 24072.java

---
+++ 20001-25000/24072.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		
+		System.out.print(a <= c && c < b ? 1 : 0);
+	}
+}
