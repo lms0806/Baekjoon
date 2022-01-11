6d9b11e269e077196f43884cd7848cfe3024cf90
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-01-11 14:04:37 +0900

    Create 24077.java

---
+++ 20001-25000/24077.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
+		
+		int[] a = new int[n], b = new int[m];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			a[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < m; i++) {
+			b[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int answer = 0;
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				if(a[i] <= b[j]) {
+					answer++;
+				}
+			}
+		}
+		System.out.print(answer);
+		
+	}
+}
