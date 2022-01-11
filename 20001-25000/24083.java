ec68cc534dbfd4c6afe7bf8ef672bf44ceca5ac4
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-01-11 14:01:18 +0900

    Create 24083.java

---
+++ 20001-25000/24083.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = (Integer.parseInt(br.readLine()) + Integer.parseInt(br.readLine())) % 12;
+		
+		System.out.print(n == 0 ? 12 : n);
+	}
+}
