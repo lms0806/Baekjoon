a5536717dabb5182e09d38df96601bab36c3346c
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-01-11 13:18:25 +0900

    Create 24075.java

---
+++ 20001-25000/24075.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+		
+		System.out.print(Math.max(a + b, a - b) + "\n" + Math.min(a + b, a - b));
+	}
+}
