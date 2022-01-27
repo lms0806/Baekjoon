8d26a1856125f20761c291592fefb12a0c1e5428
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-01-27 18:25:24 +0900

    Create 24421.java

---
+++ 20001-25000/24421.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int answer = 0;
+		for(int i = 0; i < n - 2; i++) {
+			for(int j = i + 1; j < n - 1; j++) {
+				for(int k = j + 1; k < n; k++) {
+					if(arr[i] * arr[j] == arr[k]) {
+						answer++;
+					}
+				}
+			}
+		}
+		System.out.print(answer);
+	}
+}
