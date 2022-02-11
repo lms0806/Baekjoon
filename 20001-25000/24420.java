e4edd7cf00117c2e190ee44174b02ea6f6c9e3c4
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-02-11 21:16:23 +0900

    Create 24420.java

---
+++ 20001-25000/24420.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[size];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < size; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		int answer = 0;
+		for(int i = 1; i < size - 1; i++) {
+			answer += arr[i];
+		}
+		System.out.print(answer);
+	}
+}
