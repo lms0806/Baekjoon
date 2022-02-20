e7f5d23a431e8fe2fce4027aa26d415f29d12525
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-02-20 18:28:57 +0900

    Update 24510.java

--- 20001-25000/24510.java
+++ 20001-25000/24510.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -11,15 +10,21 @@ public class Main {
 		
 		int answer = 0;
 		while(t --> 0) {
-			int num = 0;
-			String s = br.readLine().replaceAll("for", " ").replaceAll("while", " ");
+			int n = 0;
+			char[] c = br.readLine().toCharArray();
 			
-			for(char c : s.toCharArray()) {
-				if(c == ' ') {
-					num++;
+			for(int i = 0; i < c.length - 2; i++) {
+				if(i < c.length - 2 && c[i] == 'f' && c[i + 1] == 'o' && c[i + 2] == 'r') {
+					n++;
+					i += 2;
+				}
+				
+				if(i < c.length - 4 && c[i] == 'w' && c[i + 1] == 'h' && c[i + 2] == 'i' && c[i + 3] == 'l' && c[i + 4] == 'e') {
+					n++;
+					i += 4;
 				}
 			}
-			answer = Math.max(answer, num);
+			answer = Math.max(answer, n);
 		}
 		System.out.print(answer);
 	}
