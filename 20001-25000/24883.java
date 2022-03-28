2cee8a77a329b18b46e130d22505b25cb40caba4
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-03-28 22:36:53 +0900

    Create 24883.java

---
+++ 20001-25000/24883.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.print("Naver " + (br.readLine().toLowerCase().equals("n") ? "D2" : "Whale"));
+	}
+}
