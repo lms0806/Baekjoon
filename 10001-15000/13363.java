271d9d2495b490e90e458cd8bd10dd1f3f05e1db
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-04-03 13:34:15 +0900

    Create 13363.java

---
+++ 10001-15000/13363.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int a = Integer.parseInt(br.readLine()), b = Integer.parseInt(br.readLine());
+		
+		int count = 0;
+		while(a != b) {
+			if(a == 360) {
+				a = 0;
+			}
+			a++;
+			count++;
+		}
+		
+		System.out.print(count <= 180 ? count : "-" + (360 - count));
+	}
+}
