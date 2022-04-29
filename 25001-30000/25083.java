fa301bfe9aeaf6ade7c8476dbb7100c0328543eb
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-04-29 20:18:50 +0900

    Create 25083.java

---
+++ 25001-30000/25083.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args){
+		System.out.print("         ,r'\"7\n"
+				+ "r`-_   ,'  ,/\n"
+				+ " \\. \". L_r'\n"
+				+ "   `~\\/\n"
+				+ "      |\n"
+				+ "      |");
+	}
+}
