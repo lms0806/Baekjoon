92780984c2bc56d04f129a8d2926ad55afe2fa93
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-05-05 22:09:15 +0900

    Update 11006.java

--- 10001-15000/11006.java
+++ 10001-15000/11006.java
@@ -6,19 +6,15 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		int n,m,u = 0, t = 0;
-		
-		int size = Integer.parseInt(br.readLine());
+
+		int t = Integer.parseInt(br.readLine());
 		
         	StringBuilder sb = new StringBuilder();
-		while(size-->0) {
+		while(t --> 0) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			n = Integer.parseInt(st.nextToken());
-			m = Integer.parseInt(st.nextToken());
+			int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
 			
-			u = m*2-n;
-			t = m-u;
-			sb.append(u).append(" ").append(t).append("\n");
+			sb.append(m * 2 - n).append(" ").append(m - (m * 2 - n)).append("\n");
 		}
         	System.out.print(sb);
 	}
