289cc71cbc31db4f35be738886ed8f6ec01cb224
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-05-09 00:14:07 +0900

    Update 17206.java

--- 15001-20000/17206.java
+++ 15001-20000/17206.java
@@ -1,41 +1,28 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int size = Integer.parseInt(br.readLine());
+		int t = Integer.parseInt(br.readLine());
 		
-		int[] num = new int[size];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < size; i++) {
-			int sum = 0;
-			num[i] = Integer.parseInt(st.nextToken());
-			
-			for(int j = 3; j <= num[i]; j += 3) {
-				sum += j;
+		int temp = 0;
+		int[] arr = new int[80001];
+		for(int i = 3; i <= 80000; i++) {
+			if(i % 3 == 0 || i % 7 == 0) {
+				temp += i;
 			}
-			
-			for(int j = 7; j <= num[i]; j += 7) {
-				sum += j;
-			}
-			
-			for(int j = 21; j <= num[i]; j += 21) {
-				sum -= j;
-			}
-			bw.write(sum + "\n");
+			arr[i] = temp;
 		}
 		
-		bw.flush();
-
-		br.close();
-		bw.close();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		while(t --> 0) {
+			sb.append(arr[Integer.parseInt(st.nextToken())]).append("\n");
+		}
+		System.out.print(sb);
 	}
 }
