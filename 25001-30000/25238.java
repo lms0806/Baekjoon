32b29c89b25a870602044e65a6231f563f21a5d9
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-06-07 11:06:43 +0900

    Create 25238.java

---
+++ 25001-30000/25238.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		System.out.print(solve(Double.parseDouble(st.nextToken()), Double.parseDouble(st.nextToken())));
+	}
+	
+	public static int solve(double a, double b) {
+		return a - (a * b / 100) < 100 ? 1 : 0;
+	}
+}
