7b96dec9e4b60bfdd70e00fa1ab10c397904a425
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-06-27 13:24:44 +0900

    Create 18310.java

---
+++ 15001-20000/18310.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+
+		System.out.print(arr[(n - 1) / 2]);
+	}
+}
