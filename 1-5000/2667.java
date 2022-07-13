c48d6cfe489ad030600e8175b71fba881345cc4c
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-07-13 23:30:45 +0900

    Update 2667.java

--- 1-5000/2667.java
+++ 1-5000/2667.java
@@ -3,35 +3,33 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-	static int size, count;
-	static boolean[][] check, arr;
+	static int[] dx = {1, 0, -1, 0};
+	static int[] dy = {0, -1, 0, 1};
+	static int n, count;
+	static boolean[][] visited;
+	static char[][] board;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
-		size = Integer.parseInt(br.readLine());
+		n = Integer.parseInt(br.readLine());
 		
-		arr = new boolean[size][size];
-		check = new boolean[size][size];
-		for(int i = 0; i < size; i++) {
-			String str = br.readLine();
-			for(int j = 0; j < str.length(); j++) {
-				if(str.charAt(j) == '1') {
-					arr[i][j] = true;
-				}
-			}
+		board = new char[n][n];
+		visited = new boolean[n][n];
+		for(int i = 0; i < n; i++) {
+			board[i] = br.readLine().toCharArray();
 		}
 		
 		ArrayList<Integer> arr = new ArrayList<>();
 		int answer = 0;
-		for(int i = 0; i < size; i++) {
-			for(int j = 0; j < size; j++) {
-				count = 0;
-				if(checktrue(i,j)) {
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				if(board[i][j] == '1' && !visited[i][j]) {
 					answer++;
-					bfs(i,j);
-					arr.add(count);
+					arr.add(bfs(i, j));
 				}
 			}
 		}
@@ -46,28 +44,28 @@ public class Main {
 		System.out.print(sb);
 	}
 	
-	public static boolean checktrue(int row, int col) {
-		if(row < 0 || row >= size || col < 0 || col >= size || check[row][col] || !arr[row][col]) {
-			return false;
-		}
-		return true;
-	}
-	
-	public static void bfs(int row, int col) {
-		check[row][col] = true;
+	public static int bfs(int x, int y) {
+		Queue<int[]> queue = new LinkedList<>();
+		queue.add(new int[] {x, y});
+		visited[x][y] = true;
 		
-		count++;
-		if(checktrue(row - 1, col)) {
-			bfs(row - 1, col);
+		int count = 1;
+		while(!queue.isEmpty()) {
+			int[] now = queue.poll();
+			
+			for(int i = 0; i < 4; i++) {
+				int nx = now[0] + dx[i];
+				int ny = now[1] + dy[i];
+				
+				if(nx >= 0 && nx < n && ny >= 0 && ny < n) {
+					if(!visited[nx][ny] && board[nx][ny] == '1') {
+						visited[nx][ny] = true;
+						queue.add(new int[] {nx, ny});
+						count++;
+					}
+				}
+			}
 		}
-		if(checktrue(row, col - 1)) {
-			bfs(row, col - 1);
-		}
-		if(checktrue(row, col + 1)) {
-			bfs(row, col + 1);
-		}
-		if(checktrue(row + 1, col)) {
-			bfs(row + 1, col);
-		}
+		return count;
 	}
 }
