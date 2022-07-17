879cc35384621d8fa022c6216dd996cf5338f703
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-07-17 13:18:02 +0900

    Create 11121.java

---
+++ 10001-15000/11121.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(t --> 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			sb.append(solve(st.nextToken(), st.nextToken())).append("\n");
+		}
+		System.out.print(sb);
+	}
+	
+	public static String solve(String a, String b) {
+		for(int i = 0; i < a.length(); i++) {
+			if(a.charAt(i) != b.charAt(i)) {
+				return "ERROR";
+			}
+		}
+		return "OK";
+	}
+}
