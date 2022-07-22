3f7a5b2ffbc046b27c8da997449a71ddc9f4f503
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-07-22 15:34:56 +0900

    Create 25377.java

---
+++ 25001-30000/25377.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int answer = Integer.MAX_VALUE;
+		while(n --> 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+			
+			if(a <= b) {
+				answer = Math.min(b,  answer);
+			}
+		}
+		System.out.print(answer == Integer.MAX_VALUE ? -1 : answer);
+	}
+}
