f2c59f756f24f81d060a3bb8d15cddd2d24d990c
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-09-04 16:23:09 +0900

    Create 21679.java

---
+++ 20001-25000/21679.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[n + 1];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int m = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < m; i++) {
+			arr[Integer.parseInt(st.nextToken())]--;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 1; i <= n; i++) {
+			sb.append(arr[i] >= 0 ? "no" : "yes").append("\n");
+		}
+		System.out.print(sb);
+	}
+}
