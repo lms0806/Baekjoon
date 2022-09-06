1cfcb5fe575db39f04df5e992951b9a7dadc9efe
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-09-06 14:11:04 +0900

    Create 21645.java

---
+++ 20001-25000/21645.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		long answer = 0;
+		if(m < k) {
+			answer = n * m;
+		}
+		else {
+			answer = n * ((k - 1) + m / k);
+		}
+		System.out.print(answer);
+	}
+}
