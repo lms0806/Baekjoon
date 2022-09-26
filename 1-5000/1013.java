62e08d0dbdead293c49692a9fdd1ee9e4063e23e
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-09-27 00:48:00 +0900

    Create 1013.java

---
+++ 1-5000/1013.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+        while(t --> 0) {
+            sb.append((br.readLine().matches("(100+1+|01)+") ? "YES" : "NO")).append("\n");
+        }
+        
+        System.out.print(sb);
+	}
+}
