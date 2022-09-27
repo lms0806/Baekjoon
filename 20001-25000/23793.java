3c3dd48655eb778b8850650d582ac252a72ace36
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-09-27 19:58:13 +0900

    Create 23793.java

---
+++ 20001-25000/23793.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n, INF = 2000000000;
+	static ArrayList<Node>[] arr;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		arr = new ArrayList[n + 1];
+		for(int i = 1; i <= n; i++) {
+			arr[i] = new ArrayList<>();
+		}
+		
+		while(m --> 0) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+			int cost = Integer.parseInt(st.nextToken());
+			
+			arr[a].add(new Node(b, cost));
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		int x = Integer.parseInt(st.nextToken()), y = Integer.parseInt(st.nextToken()), z = Integer.parseInt(st.nextToken());
+		
+		int a = dijkstra(x, y, 0);
+		int b = dijkstra(y, z, 0);
+		int c = dijkstra(x, z, y);
+		
+		System.out.print(((a == INF || b == INF) ? -1 : a + b) + " " + (c == INF ? -1 : c));
+    }
+	
+	public static int dijkstra(int start, int end, int not) {
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		int[] dist = new int[n + 1];
+		
+		Arrays.fill(dist, INF);
+		
+		pq.add(new Node(start, 0));
+		dist[start] = 0;
+		
+		while(!pq.isEmpty()) {
+			Node now = pq.poll();
+			int cur = now.start;
+			
+			if(dist[cur] < now.cost) {
+				continue;
+			}
+			
+			for(Node a : arr[cur]) {
+				if(a.start == not) {
+					continue;
+				}
+				
+				if(dist[a.start] > dist[cur] + a.cost) {
+					dist[a.start] = dist[cur] + a.cost;
+					pq.add(new Node(a.start, dist[a.start]));
+				}
+			}
+		}
+		return dist[end];
+	}
+}
+
+class Node implements Comparable<Node>{
+	int start, cost;
+	
+	public Node(int start, int cost) {
+		this.start = start;
+		this.cost = cost;
+	}
+
+	@Override
+	public int compareTo(Node o) {
+		return this.cost - o.cost;
+	}
+}
