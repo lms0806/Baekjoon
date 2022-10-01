974345317ddf111d5b60a2d69bcaa27a23397d8b
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-10-01 16:45:20 +0900

    Update 6436.java

--- 5001-10000/6436.java
+++ 5001-10000/6436.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
