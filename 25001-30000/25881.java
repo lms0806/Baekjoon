1f0590c8dff077c4d5fef2ef6a0cc7dd341377aa
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-10-24 12:33:06 +0900

    Create 25881.java

---
+++ 25001-30000/25881.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(t --> 0) {
+			int n = Integer.parseInt(br.readLine());
+			
+			sb.append(n).append(" ").append(n <= 1000 ? n * a : a * 1000 + (n - 1000) * b).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
