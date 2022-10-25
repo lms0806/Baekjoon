06b39fb51cc9e0c67345c2bd30a3b7eaa7a05053
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-10-25 23:51:02 +0900

    Create 3803.java

---
+++ 1-5000/3803.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int v;
+	static ArrayList<Node>[] arr;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		StringBuilder sb = new StringBuilder();
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			v = Integer.parseInt(st.nextToken());
+			
+			if(v == 0) {
+				break;
+			}
+			
+			int e = Integer.parseInt(st.nextToken());
+			
+			arr = new ArrayList[v + 1];
+			for(int i = 1; i <= v; i++) {
+				arr[i] = new ArrayList<>();
+			}
+			
+			while(e --> 0) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+				int cost = Integer.parseInt(st.nextToken());
+				
+				arr[a].add(new Node(b, cost));
+				arr[b].add(new Node(a, cost));
+			}
+			
+			br.readLine();
+			sb.append(prim()).append("\n");
+		}
+		System.out.print(sb);
+	}
+	
+	public static long prim() {
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		boolean[] visited = new boolean[v + 1];
+		pq.add(new Node(1, 0));
+		
+		long cost = 0;
+		while(!pq.isEmpty()) {
+			Node now = pq.poll();
+			
+			if(visited[now.to]) {
+				continue;
+			}
+			
+			cost += now.cost;
+			visited[now.to] = true;
+			
+			for(Node a : arr[now.to]) {
+				if(visited[a.to]) {
+					continue;
+				}
+				pq.add(a);
+			}
+		}
+		return cost;
+	}
+}
+
+class Node implements Comparable<Node>{
+	int to, cost;
+	
+	public Node(int to, int cost) {
+		this.to = to;
+		this.cost = cost;
+	}
+
+	@Override
+	public int compareTo(Node o) {
+		return this.cost - o.cost;
+	}
+}
