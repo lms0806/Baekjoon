cb8981299ea461a703aca6b2b38630e46dea3c51
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-10-29 10:57:50 +0900

    Create 16018.java

---
+++ 15001-20000/16018.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		br.readLine();
+		String a = br.readLine(), b = br.readLine();
+		
+		int answer = 0;
+		for(int i = 0; i < a.length(); i++) {
+			if(a.charAt(i) == 'C' && b.charAt(i) == 'C') {
+				answer++;
+			}
+		}
+		System.out.print(answer);
+	}
+}
