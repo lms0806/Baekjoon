f411b5557f10e4a8c4c70e5756962ea01a71ea55
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-10-30 20:30:00 +0900

    Create 5691.java

---
+++ 5001-10000/5691.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringBuilder sb = new StringBuilder();
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+			
+			if(a == 0 && b == 0) {
+				break;
+			}
+			
+			sb.append(2 * a - b).append("\n");
+		}
+		System.out.print(sb);
+	}	
+}
