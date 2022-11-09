d03dc54b7e09c1a1c1f7657ee94fced76a943072
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-11-09 22:00:36 +0900

    Update 6812.java

--- 5001-10000/6812.java
+++ 5001-10000/6812.java
@@ -4,27 +4,37 @@ import java.io.InputStreamReader;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] s = {"Ottawa", "Victoria", "Edmonton", "Winnipeg", "Toronto", "Halifax", "St. John's"};
+		int[] plus = {0, -300, -200, -100, 0, 100, 130};
 		
 		int n = Integer.parseInt(br.readLine());
 		
-		int[] arr = {0, -300, -200, -100, 0, 100, 130};
-		String[] str = {"Ottawa", "Victoria", "Edmonton", "Winnipeg", "Toronto", "Halifax", "St. John's"};
-		
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < arr.length; i++) {
-			int num = n + arr[i];
-			if(num < 0) {
-				num += 2400;
-			}
-			if(num >= 2400) {
-				num -= 2400;
-			}
-			if(num % 100 >= 60) {
-				num = 100 * (num / 100 + 1) + num % 100 - 60;
-			}
-			sb.append(num).append(" in ").append(str[i]).append("\n");
+		for(int i = 0; i < s.length; i++) {
+			sb.append(nums(n + plus[i])).append(" in ").append(s[i]).append("\n");
 		}
 		System.out.print(sb);
+	}
+	
+	public static int nums(int n) {
+		int temp = n;
+		
+		if(n < 0) {
+			temp = n + 2400;
+		}
+		if(n >= 2400) {
+			temp = n - 2400;
+		}
+		
+		if(temp % 100 >= 60) {
+			temp = 100 * (temp / 100 + 1) + temp % 100 - 60;
+		}
+		if(temp >= 2400) {
+			temp -= 2400;
+		}
+		
+		return temp;
 	}
 }
