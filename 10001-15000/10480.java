b8320a6c0c296c40501c6bea23d0943b96c63a7e
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-11-10 18:39:55 +0900

    Create 10480.java

---
+++ 10001-15000/10480.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(t --> 0) {
+			int n = Integer.parseInt(br.readLine());
+			
+			sb.append(n).append(" is ").append(n % 2 == 0 ? "even" : "odd").append("\n");
+		}
+		System.out.print(sb);
+	}
+}
