eaebac4fc5876e8efc758123730702abcf756615
lms0806 <42136056+lms0806@users.noreply.github.com>
2022-11-30 06:49:52 +0900

    Create 1449.java

---
+++ 1-5000/1449.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken()), l = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		int count = 0, num = Integer.MIN_VALUE;
+		for(int i = 0; i < n; i++) {
+			if(arr[i] > num + l - 1) {
+				count++;
+				num = arr[i];
+			}
+		}
+		System.out.print(count);
+	}
+}
