e7b54ba03d69dc8a89cabe4e47bf234a3da10089
lms0806 <godmlzkf1@naver.com>
2022-12-05 16:15:57 +0900

    Update 23057.java

--- 20001-25000/23057.java
+++ 20001-25000/23057.java
@@ -1,13 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
 
 public class Main {
 	static int n;
 	static int[] arr;
-	static TreeSet<Long> set = new TreeSet<>();
+	static HashSet<Long> set = new HashSet<>();
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
