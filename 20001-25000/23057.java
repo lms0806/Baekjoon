d0da10aba8a87eae2cb6dbdfbf562c4b53fd655b
lms0806 <godmlzkf1@naver.com>
2022-12-05 16:14:51 +0900

    Create 23057.java

---
+++ 20001-25000/23057.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class Main {
+	static int n;
+	static int[] arr;
+	static TreeSet<Long> set = new TreeSet<>();
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		n = Integer.parseInt(br.readLine());
+		
+		long sum = 0;
+		arr = new int[n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			sum += arr[i];
+		}
+		
+		dfs(0, 0);
+		
+		System.out.print(sum - set.size() + 1);
+	}
+	
+	public static void dfs(int depth, long sum) {
+		set.add(sum);
+		
+		if(depth == n) {
+			return;
+		}
+		
+		dfs(depth + 1, sum + arr[depth]);
+		dfs(depth + 1, sum);
+	}
+}
