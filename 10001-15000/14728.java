f4b909a272346febec93c7c9e3a9aa5478412d3f
lms0806 <godmlzkf1@naver.com>
2022-12-06 04:21:39 +0900

    Create 14728.java

---
+++ 10001-15000/14728.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
+		
+		int[][] arr = new int[n + 1][2];
+		
+		for(int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		int[] dp = new int[m + 1];
+		
+		for(int i = 1; i <= n; i++) {
+			for(int j = m; j >= arr[i][0]; j--) {
+				dp[j] = Math.max(dp[j], dp[j - arr[i][0]] + arr[i][1]);
+			}
+		}
+		
+		System.out.print(dp[m]);
+	}
+}
