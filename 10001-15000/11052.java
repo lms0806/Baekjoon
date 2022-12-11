9266ebc2cf7c99f40893a43393edbd3b069386d2
lms0806 <godmlzkf1@naver.com>
2022-12-11 12:11:11 +0900

    Create 11052.java

---
+++ 10001-15000/11052.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[n + 1], dp = new int[n + 1];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= i; j++) {
+				dp[i] = Math.max(dp[i], dp[i - j] + arr[j]);
+			}
+		}
+		
+		System.out.print(dp[n]);
+	}
+}
