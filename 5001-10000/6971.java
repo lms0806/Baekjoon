f1fd70f33632443e50a82d4531e528036f359ea5
lms0806 <godmlzkf1@naver.com>
2022-12-17 07:35:22 +0900

    Create 6971.java

---
+++ 5001-10000/6971.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(t --> 0) {
+			int n = Integer.parseInt(br.readLine());
+			
+			sb.append(n).append(n % 6 == 0 ? " is nasty" : " is not nasty").append("\n");
+		}
+		System.out.print(sb);
+	}
+}
