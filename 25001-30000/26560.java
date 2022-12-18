28912e3e1f7ff3a775c3fdadf596db86fcee2742
lms0806 <godmlzkf1@naver.com>
2022-12-18 13:51:46 +0900

    Create 26560.java

---
+++ 25001-30000/26560.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(t --> 0) {
+			String s = br.readLine();
+			
+			sb.append(s.endsWith(".") ? s : s + ".").append("\n");
+		}
+		System.out.print(sb);
+	}
+}
