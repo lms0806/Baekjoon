ed682b5b145968d28a8d63fc3bcb2bffd5904c0b
lms0806 <godmlzkf1@naver.com>
2022-12-19 13:06:23 +0900

    Create 10189.java

---
+++ 10001-15000/10189.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("#  # #### #### #  #").append("\n").append("#### #  # #  # # #").append("\n");
+		sb.append("#### #  # #  # # #").append("\n").append("#  # #### #### #  #");
+		System.out.print(sb);
+	}
+}
