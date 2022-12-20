09a19fd510018dacf8931c1b53049818e7e05bd2
lms0806 <godmlzkf1@naver.com>
2022-12-20 13:08:40 +0900

    Create 26531.java

---
+++ 25001-30000/26531.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] s = br.readLine().split(" ");
+		
+		System.out.print(Integer.parseInt(s[0]) + Integer.parseInt(s[2]) == Integer.parseInt(s[4]) ? "YES" : "NO");
+	}
+}
