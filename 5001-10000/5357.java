a8c2fc7c5309d9edc9fc14219b52ba3ea8c0faf6
lms0806 <godmlzkf1@naver.com>
2022-12-23 12:27:04 +0900

    Create 5357.java

---
+++ 5001-10000/5357.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(t --> 0) {
+			char ch = ' ';
+			
+			for(char c : br.readLine().toCharArray()) {
+				if(ch != c) {
+					ch = c;
+					sb.append(c);
+				}
+			}
+			sb.append("\n");
+		}
+		System.out.print(sb);
+	}
+}
