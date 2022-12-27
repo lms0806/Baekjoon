6026f81300d1213cba858d7e44af5ae5fc61c524
lms0806 <godmlzkf1@naver.com>
2022-12-27 14:02:17 +0900

    Create 26940.java

---
+++ 25001-30000/26940.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[n];
+		
+		int answer = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			
+			if(i > 0 && arr[i - 1] < arr[i]) {
+				answer++;
+			}
+		}
+		System.out.print(answer);
+	}
+}
