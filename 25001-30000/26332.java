652d4a104e70583b957f3392d993ab55bd6c012c
lms0806 <godmlzkf1@naver.com>
2022-12-28 17:12:00 +0900

    Create 26332.java

---
+++ 25001-30000/26332.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(t --> 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int c = Integer.parseInt(st.nextToken()), p = Integer.parseInt(st.nextToken());
+			
+			sb.append(c).append(" ").append(p).append("\n").append(c * p - ((c - 1) * 2)).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
