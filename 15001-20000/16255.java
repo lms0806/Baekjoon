2892912b3bca429fc71e58867a9eda818c2989a5
lms0806 <godmlzkf1@naver.com>
2023-01-12 09:23:11 +0900

    Create 16255.java

---
+++ 15001-20000/16255.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(t --> 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int k = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken()), y = Integer.parseInt(st.nextToken());
+			
+			sb.append(Math.max(k, Math.min(x, y) + 2) - Math.max(x, y)).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
