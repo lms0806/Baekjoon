1eebae50e9ccb335489a98034ec3d2b560a89db6
lms0806 <godmlzkf1@naver.com>
2023-01-15 09:53:43 +0900

    Create 27211.java

---
+++ 25001-30000/27211.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n, m;
+	static int[][] arr;
+	static boolean[][] visited;
+	static int[] dx = {1, 0, -1, 0};
+	static int[] dy = {0, 1, 0, -1};
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		arr = new int[n][m];
+		visited = new boolean[n][m];
+		
+		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < m; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int answer = 0;
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				if(!visited[i][j] && arr[i][j] == 0) {
+					bfs(i, j);
+					answer++;
+				}
+			}
+		}
+		System.out.print(answer);
+	}
+	
+	public static void bfs(int x, int y) {
+		Queue<int[]> queue = new LinkedList<>();
+		queue.add(new int[] {x, y});
+		visited[x][y] = true;
+		
+		while(!queue.isEmpty()) {
+			int[] now = queue.poll();
+			
+			for(int i = 0; i < 4; i++) {
+				int nx = now[0] + dx[i];
+				int ny = now[1] + dy[i];
+				
+				if(nx < 0) {
+					nx = n - 1;
+				}
+				if(nx == n) {
+					nx = 0;
+				}
+				if(ny < 0) {
+					ny = m - 1;
+				}
+				if(ny == m) {
+					ny = 0;
+				}
+				
+				if(!visited[nx][ny] && arr[nx][ny] == 0) {
+					queue.add(new int[] {nx, ny});
+					visited[nx][ny] = true;
+				}
+			}
+		}
+	}
+}
