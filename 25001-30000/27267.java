0348b1a09dde71a7cfe7c72b6d0917be5a2e2a1f
lms0806 <godmlzkf1@naver.com>
2023-01-22 13:42:53 +0900

    Create 27267.java

---
+++ 25001-30000/27267.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken()), d = Integer.parseInt(st.nextToken());
+		
+		System.out.print(a * b == c * d ? "E" : a * b > c * d ? "M" : "P");
+	}
+}
