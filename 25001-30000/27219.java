cba2263c0269991deb0ab7dcb44fe0aee5f73309
lms0806 <godmlzkf1@naver.com>
2023-01-26 12:32:08 +0900

    Create 27219.java

---
+++ 25001-30000/27219.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		System.out.print("V".repeat(n / 5) + "I".repeat(n % 5));
+	}
+}
