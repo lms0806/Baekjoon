8a88ef36ce82dce949d006f513af290cd05e1ce8
lms0806 <godmlzkf1@naver.com>
2023-01-30 11:32:30 +0900

    Create 27294.java

---
+++ 25001-30000/27294.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int t = Integer.parseInt(st.nextToken()), s = Integer.parseInt(st.nextToken());
+		
+		System.out.print(t >= 12 && t <= 16 && s == 0 ? 320 : 280);
+	}
+}
