54e4ac4d5a2329453037887b43dbe3b417617710
lms0806 <godmlzkf1@naver.com>
2023-02-01 13:12:22 +0900

    Create 9924.java

---
+++ 5001-10000/9924.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+		
+		int answer = 0;
+		while(a != b) {
+			answer++;
+			
+			int min = Math.min(a, b), max = Math.max(a, b);
+			a = max - min;
+			b = min;
+		}
+		
+		System.out.print(answer);
+	}
+}
