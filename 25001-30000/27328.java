b1de9c0642b1d75b2939bb2f7e66625b27bf20af
lms0806 <godmlzkf1@naver.com>
2023-02-04 10:47:07 +0900

    Create 27328.java

---
+++ 25001-30000/27328.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a = Integer.parseInt(br.readLine()), b = Integer.parseInt(br.readLine());
+		
+		System.out.println(a > b ? 1 : a == b ? 0 : -1);
+	}
+}
