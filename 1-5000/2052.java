db357a62e96a70bca06687135c3529b301f84d03
lms0806 <godmlzkf1@naver.com>
2023-02-24 18:21:09 +0900

    Create 2052.java

---
+++ 1-5000/2052.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		System.out.print(String.format("%." + n + "f", BigDecimal.valueOf(0.5).pow(n)));
+	}
+}
