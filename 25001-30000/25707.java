d1e28ab0e75958185f18992c798a14322ce8f8fa
lms0806 <godmlzkf1@naver.com>
2023-03-03 01:13:01 +0900

    Create 25707.java

---
+++ 25001-30000/25707.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		int answer = arr[n - 1] - arr[0];
+		for(int i = 0; i < n - 1; i++) {
+			answer += arr[i + 1] - arr[i];
+		}
+		System.out.print(answer);
+	}
+}
