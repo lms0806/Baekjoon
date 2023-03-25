b251726c1e51318c2f779334f0738445970ad5c8
lms0806 <godmlzkf1@naver.com>
2023-03-25 22:01:14 +0900

    Create 14215.java

---
+++ 10001-15000/14215.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException,IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[] arr = new int[3];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < 3; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		System.out.print(arr[0] + arr[1] + Math.min(arr[2], arr[0] + arr[1] - 1));
+	}
+}
