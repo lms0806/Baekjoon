414a085f65817d88461460c9927310b681f44e9f
lms0806 <godmlzkf1@naver.com>
2023-03-29 20:14:22 +0900

    Create 1354.java

---
+++ 1-5000/1354.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+	static long p, q, x, y;
+	static HashMap<Long, Long> map = new HashMap<>();
+	public static void main(String[] args) throws NumberFormatException,IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long n = Long.parseLong(st.nextToken());
+		p = Long.parseLong(st.nextToken());
+		q = Long.parseLong(st.nextToken());
+		x = Long.parseLong(st.nextToken());
+		y = Long.parseLong(st.nextToken());
+		
+		System.out.print(solve(n));
+	}
+	
+	public static long solve(long num) {
+		if(num <= 0) {
+			return 1;
+		}
+		if(map.containsKey(num)) {
+			return map.get(num);
+		}
+		
+		map.put(num, solve(num / p - x) + solve(num / q - y));
+		return map.get(num);
+	}
+}
