3f5148263add45fc36910f8e09694621599c819b
lms0806 <godmlzkf1@naver.com>
2023-05-06 22:00:26 +0900

    Create 28014.java

---
+++ 25001-30000/28014.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int answer = 1;
+		for(int i = 0; i < n - 1; i++) {
+			if(arr[i] <= arr[i + 1]) {
+				answer++;
+			}
+		}
+		System.out.print(answer);
+	}
+}
