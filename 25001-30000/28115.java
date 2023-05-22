b18ee2937eaeefd9991d80cfbd6bafe9f097e5df
lms0806 <godmlzkf1@naver.com>
2023-05-22 12:25:53 +0900

    Create 28115.java

---
+++ 25001-30000/28115.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] a = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.print(solve(a));
+    }
+
+    public static String solve(int[] a){
+        int[] b = new int[a.length], c = new int[a.length];
+        for(int i = 0; i < a.length; i++){
+            if(i > 1){
+                if(a[i] - a[i - 1] != a[i - 1] - a[i - 2]){
+                    return "NO";
+                }
+            }
+
+            b[i] = a[i] * 2;
+            c[i] = -a[i];
+        }
+
+        return "YES\n" + print(b) + "\n" + print(c);
+    }
+
+    public static StringBuilder print(int[] nums){
+        StringBuilder sb = new StringBuilder();
+        for(int num : nums){
+            sb.append(num).append(" ");
+        }
+        return sb;
+    }
+}
