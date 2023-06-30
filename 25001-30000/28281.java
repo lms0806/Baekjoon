e1ef6da35fa67f9ec2f08d48996050bfb00c0b1d
lms0806 <godmlzkf1@naver.com>
2023-07-01 01:04:35 +0900

    Create 28281.java

---
+++ 25001-30000/28281.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken()), x = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int answer = Integer.MAX_VALUE;
+		for(int i = 0; i < n - 1; i ++) {
+			answer = Math.min(arr[i] * x + arr[i + 1] * x, answer);
+		}
+		
+		System.out.print(answer);
+	}
+}
