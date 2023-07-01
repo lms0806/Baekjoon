5f632acb916a509d6ce64043654554ed21992837
lms0806 <godmlzkf1@naver.com>
2023-07-02 00:01:40 +0900

    Create 28224.java

---
+++ 25001-30000/28224.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		int answer = 0;
+		while(t --> 0) {
+			answer += Integer.parseInt(br.readLine());
+		}
+		System.out.print(answer);
+	}
+}
