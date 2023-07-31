d3a94f2c7d7b33a5d94bf07fa487f350a62d94f8
lms0806 <godmlzkf1@naver.com>
2023-07-31 23:08:10 +0900

    Create 21194.java

---
+++ 20001-25000/21194.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken()), k = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		int sum = 0;
+		for(int i = n - 1; i >= n - k; i--) {
+			sum += arr[i];
+		}
+		System.out.print(sum);
+	}
+}
