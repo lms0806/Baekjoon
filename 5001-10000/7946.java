8e5ccca668212e2882101871afa3da0e6bb0f4c5
lms0806 <godmlzkf1@naver.com>
2023-07-31 14:49:00 +0900

    Create 7946.java

---
+++ 5001-10000/7946.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(t --> 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken()), k = Integer.parseInt(st.nextToken()) - 1;
+			
+			int[] arr = new int[n];
+			
+			st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < n; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			Arrays.sort(arr);
+			
+			sb.append(arr[k]).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
