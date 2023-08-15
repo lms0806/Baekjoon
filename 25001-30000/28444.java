84e83a791ff5297e0ad07a3c5fb84ac0bc6881ce
lms0806 <godmlzkf1@naver.com>
2023-08-15 09:31:16 +0900

    Create 28444.java

---
+++ 25001-30000/28444.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		System.out.print((sti(st.nextToken()) * sti(st.nextToken())) - (sti(st.nextToken()) * sti(st.nextToken()) * sti(st.nextToken())));
+	}
+	
+	public static int sti(String s) {
+		return Integer.parseInt(s);
+	}
+}
