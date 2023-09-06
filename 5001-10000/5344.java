3d9790e5fe22a5ed72af754126345281726c423f
lms0806 <godmlzkf1@naver.com>
2023-09-06 22:24:46 +0900

    Create 5344.java

---
+++ 5001-10000/5344.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	
+		int t = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(t --> 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			sb.append(gcd(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()))).append("\n");
+		}
+		System.out.print(sb);
+	}
+	
+	public static int gcd(int a, int b) {
+		if(b == 0) {
+			return a;
+		}
+		return gcd(b, a % b);
+	}
+}
