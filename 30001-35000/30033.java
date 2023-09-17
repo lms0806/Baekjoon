c431c78a759c98791f50c3907bf371c92fe4eb39
lms0806 <godmlzkf1@naver.com>
2023-09-17 22:30:55 +0900

    Create 30033.java

---
+++ 30001-35000/30033.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int answer = 0;
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			if(arr[i] <= Integer.parseInt(st.nextToken())) {
+				answer++;
+			}
+		}
+		System.out.print(answer);
+	}
+}
