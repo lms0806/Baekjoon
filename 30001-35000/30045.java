f1f3ceb13c5af47509e03044148f98fd4f771398
lms0806 <godmlzkf1@naver.com>
2023-09-23 17:52:56 +0900

    Create 30045.java

---
+++ 30001-35000/30045.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		int answer = 0;
+		while(t --> 0) {
+			String s = br.readLine();
+			
+			if(s.contains("OI") || s.contains("01")) {
+				answer++;
+			}
+		}
+		System.out.print(answer);
+	}
+}
