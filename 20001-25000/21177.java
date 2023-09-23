65331d74ced60449cacafb5fe8dc154a7875de1e
lms0806 <godmlzkf1@naver.com>
2023-09-23 20:27:38 +0900

    Create 21177.java

---
+++ 20001-25000/21177.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		int answer = arr[0];
+		for(int i = 1; i < n; i++) {
+			if(arr[i - 1] + 1 == arr[i]) {
+				continue;
+			}
+			
+			answer += arr[i];
+		}
+		System.out.print(answer);
+	}
+}
