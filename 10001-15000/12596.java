615a61cc4db580cf41d434ae30ad18bad4063177
lms0806 <godmlzkf1@naver.com>
2023-09-23 19:14:06 +0900

    Create 12596.java

---
+++ 10001-15000/12596.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int size = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		for(int t = 1; t <= size; t++) {
+			int n = Integer.parseInt(br.readLine());
+			
+			int[] arr = new int[n];
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < n; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			Arrays.sort(arr);
+			
+			int answer = arr[n - 1];
+			for(int i = 0; i < n - 1; i += 2) {
+				if(arr[i] != arr[i + 1]) {
+					answer = arr[i];
+					break;
+				}
+			}
+			
+			sb.append("Case #").append(t).append(": ").append(answer).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
