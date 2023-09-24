9c13dee8e442bdbc562f5b2f02f05ed1fda9359c
lms0806 <godmlzkf1@naver.com>
2023-09-24 10:28:08 +0900

    Update 25166.java

--- 25001-30000/25166.java
+++ 25001-30000/25166.java
@@ -4,32 +4,20 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		System.out.print(solve(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
 	}
 	
-	public static String solve(int s, int m) {
-		int[] coin = new int[10];
-		
-		while(true) {
-			int count = 0;
-			for(int i = coin.length - 1; i >= 0; i--) {
-				if(s >= Math.pow(2, i) && coin[i] == 0) {
-					s -= Math.pow(2, i);
-					coin[i]++;
-					break;
-				}
-				count++;
-			}
-			
-			if(count == coin.length) {
-				break;
-			}
+	public static String solve(int a, int b) {
+		if(a < 1024) {
+			return "No thanks";
 		}
 		
-		return s == 0 ? "No thanks" : s - m == 0 ? "Thanks" : "Impossible";
+		int need = a - 1023;
+		
+		return (need & b) == need ? "Thanks" : "Impossible";
 	}
 }
