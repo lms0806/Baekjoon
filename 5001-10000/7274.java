4448236d6f861e6c2900f56af0979ee41edf0a41
lms0806 <godmlzkf1@naver.com>
2023-09-25 22:18:03 +0900

    Create 7274.java

---
+++ 5001-10000/7274.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
+        
+        int[] arr = new int[n];
+        
+        for(int i = 0; i < n; i++) {
+        	arr[i] = Integer.parseInt(br.readLine());
+        }
+        
+        Arrays.sort(arr);
+        
+        int answer = 0;
+        for(int i = 0; i < n - m; i++) {
+        	answer += arr[i];
+        }
+        System.out.print(answer);
+    }
+}
