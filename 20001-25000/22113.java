2706c96e3c1b4cd7adb979c6df8f4db6a0ecac45
lms0806 <godmlzkf1@naver.com>
2023-10-04 00:13:00 +0900

    Create 22113.java

---
+++ 20001-25000/22113.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[m];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < m; i++) {
+			arr[i] = Integer.parseInt(st.nextToken()) - 1;
+		}
+		
+		int[][] board = new int[n][n];
+		
+		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			for(int j = 0; j < n; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int answer = 0;
+		for(int i = 0; i < m - 1; i++) {
+			answer += board[arr[i]][arr[i + 1]];
+		}
+		System.out.print(answer);
+	}
+}
