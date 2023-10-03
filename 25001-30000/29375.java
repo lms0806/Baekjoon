715da025aa22cfe58ba9960cd460b98b88c77369
lms0806 <godmlzkf1@naver.com>
2023-10-03 23:43:43 +0900

    Create 29375.java

---
+++ 25001-30000/29375.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		double answer = arr[0];
+		for(int i = 1; i < n; i++) {
+			answer = (answer + arr[i]) / 2;
+		}
+		System.out.print(answer);
+	}
+}
