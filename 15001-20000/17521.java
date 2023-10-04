b35939115c2e8ea9c6dc8d3562031ac6b5ed3908
lms0806 <godmlzkf1@naver.com>
2023-10-04 20:24:42 +0900

    Create 17521.java

---
+++ 15001-20000/17521.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		long w = Long.parseLong(st.nextToken());
+		
+		int min = 0;
+		while(n --> 0) {
+			int now = Integer.parseInt(br.readLine());
+			
+			if(min != 0 && min < now) {
+				long m = w / min;
+				
+				w += m * now - (m * min);
+			}
+			min = now;
+		}
+		System.out.print(w);
+	}
+}
