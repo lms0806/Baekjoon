4dfee54fcd3a4f5b8a9da548b033f42f73da4f8c
lms0806 <godmlzkf1@naver.com>
2023-10-06 22:43:04 +0900

    Create 19751.java

---
+++ 15001-20000/19751.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int[] arr = new int[4];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < 4; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		System.out.print(arr[0] + " " + arr[2] + " " + arr[1] + " " + arr[3]);
+	}
+}
