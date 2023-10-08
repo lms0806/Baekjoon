7cfb559fa563a299e9316e6e55dc20e781febd1f
lms0806 <godmlzkf1@naver.com>
2023-10-08 11:54:40 +0900

    Create 28353.java

---
+++ 25001-30000/28353.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken()), k = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		int answer = 0;
+		int s = 0, e = n - 1;
+		while(s < e) {
+			int sum = arr[s] + arr[e];
+
+			if(sum > k) {
+				e--;
+			}
+			else {
+				answer++;
+				s++;
+				e--;
+			}
+		}
+		System.out.print(answer);
+	}
+}
