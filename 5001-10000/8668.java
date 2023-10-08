a0618aa06ad8d7a52cd8a09bcff3e99838697aa8
lms0806 <godmlzkf1@naver.com>
2023-10-08 11:08:27 +0900

    Create 8668.java

---
+++ 5001-10000/8668.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		System.out.print(Integer.parseInt(st.nextToken()) < Integer.parseInt(st.nextToken()) << 1 ? "TAK" : "NIE");
+	}
+}
