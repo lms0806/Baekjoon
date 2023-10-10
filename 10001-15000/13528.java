20d4a2000d2a657b18b2318dcda13fdc07983a45
lms0806 <godmlzkf1@naver.com>
2023-10-10 23:04:42 +0900

    Create 13528.java

---
+++ 10001-15000/13528.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		double n = Double.parseDouble(br.readLine());
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		double answer = 0;
+		while(t --> 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			answer += Double.parseDouble(st.nextToken()) * Double.parseDouble(st.nextToken());
+		}
+		
+		System.out.print(answer * n);
+	}
+}
