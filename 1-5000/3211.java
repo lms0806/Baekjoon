a085ea551af5076ba6eee9c5796a5fa4e9be73fa
lms0806 <godmlzkf1@naver.com>
2024-02-04 20:55:12 +0900

    Create 3211.java

---
+++ 1-5000/3211.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		int idx = 1;
+		while(idx < n && arr[idx - 1] + 1 > idx) {
+			idx = arr[idx - 1] + 1;
+		}
+		System.out.print(idx);
+	}
+}
