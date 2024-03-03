c768dd2535a2ea1661b132af26c44ff8869b4f0b
lms0806 <godmlzkf1@naver.com>
2024-03-03 09:56:35 +0900

    Create 31495.java

---
+++ 30001-35000/31495.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException { 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String s = br.readLine();
+		
+		System.out.print(s.length() > 2 && s.charAt(0) == '\"' && s.charAt(s.length() - 1) == '\"' ? s.substring(1, s.length() - 1) : "CE");
+	}
+}
