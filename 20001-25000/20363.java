6be3cb45ff01807919355f31db582cbeb43ea93b
lms0806 <godmlzkf1@naver.com>
2024-03-17 13:24:21 +0900

    Create 20363.java

---
+++ 20001-25000/20363.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int x = Integer.parseInt(st.nextToken()), y = Integer.parseInt(st.nextToken());
+		
+		System.out.print(x + y + (Math.min(x, y) / 10));
+	}
+}
