e3ce211b196cd5d1f4fa90e4bffaee3465becc62
lms0806 <godmlzkf1@naver.com>
2024-03-31 19:39:16 +0900

    Create 31668.java

---
+++ 30001-35000/31668.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine()), m = Integer.parseInt(br.readLine());
+		int k = Integer.parseInt(br.readLine());
+		
+		System.out.print(m / n * k);
+	}
+}
