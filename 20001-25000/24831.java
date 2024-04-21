eb17baacf13c5eb43f9082e41b7c2075b9a55d0f
lms0806 <godmlzkf1@naver.com>
2024-04-21 13:30:04 +0900

    Create 24831.java

---
+++ 20001-25000/24831.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(t --> 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken()), y = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+			
+			sb.append((y - x) % (a + b) == 0 ? (y - x) / (a + b) : -1).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
