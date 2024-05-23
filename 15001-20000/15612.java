238307a72253b20ec9eb63e0809436cfa3fb4bb2
lms0806 <godmlzkf1@naver.com>
2024-05-23 20:42:07 +0900

    Create 15612.java

---
+++ 15001-20000/15612.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(t --> 0) {
+			sb.append(Long.toString(Long.parseLong(br.readLine()), 3)).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
