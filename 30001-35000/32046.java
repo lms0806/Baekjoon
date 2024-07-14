97ccc6741c395275f485664c7498d110d1b4ec45
lms0806 <godmlzkf1@naver.com>
2024-07-14 14:53:22 +0900

    Create 32046.java

---
+++ 30001-35000/32046.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringBuilder sb = new StringBuilder();
+		while(true) {
+			int n = Integer.parseInt(br.readLine());
+			
+			if(n == 0) {
+				break;
+			}
+			
+			int[] arr = new int[n];
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < n; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			int answer = 0;
+			for(int i = 0; i < n; i++) {
+				if(answer + arr[i] <= 300) {
+					answer += arr[i];
+				}
+			}
+			sb.append(answer).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
