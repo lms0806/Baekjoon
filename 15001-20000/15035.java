2d65e21c6cebcbee0e13fda586b8d75c5b258f7a
lms0806 <godmlzkf1@naver.com>
2024-07-28 23:56:22 +0900

    Create 15035.java

---
+++ 15001-20000/15035.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		int answer = 0, min = Integer.MAX_VALUE;
+		for(int a : arr) {
+			if(t % a < min) {
+				min = t % a;
+				answer = a;
+			}
+		}
+		System.out.print(answer);
+	}
+}
