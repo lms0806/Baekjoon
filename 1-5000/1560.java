eb049aa2cb23b3db7f8b934fdb13f37467d915b7
lms0806 <godmlzkf1@naver.com>
2024-07-28 17:25:23 +0900

    Create 1560.java

---
+++ 1-5000/1560.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		BigInteger n = new BigInteger(br.readLine());
+		
+		System.out.print(n.add(n).subtract(BigInteger.TWO));
+	}
+}
