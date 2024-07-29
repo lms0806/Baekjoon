2be47c88f101bfc5ae318debca729b942376d732
lms0806 <godmlzkf1@naver.com>
2024-07-29 20:53:41 +0900

    Create 31432.java

---
+++ 30001-35000/31432.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("YES\n");
+		if(n == 1 && arr[0] == 0) {
+			sb.append(0);
+		}
+		else {
+			sb.append(arr[n - 1]).append(arr[n - 1]).append(arr[n - 1]);
+		}
+		System.out.print(sb);
+	}
+}
