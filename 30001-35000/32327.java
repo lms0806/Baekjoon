48fc1c82e3043378bb1d37e111ef60fc7ee0d64d
lms0806 <godmlzkf1@naver.com>
2024-09-20 21:49:34 +0900

    Create 32327.java

---
+++ 30001-35000/32327.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		while(true) {
+			int num = Integer.parseInt(br.readLine());
+			
+			if(num >= n) {
+				break;
+			}
+			
+			n += num;
+		}
+		System.out.print(n);
+	}
+}
