3f3dd761fc52af131b8c197df4ccf4fa3514be4d
lms0806 <godmlzkf1@naver.com>
2024-09-22 12:48:18 +0900

    Create 32297.java

---
+++ 30001-35000/32297.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		br.readLine();
+		System.out.print(br.readLine().contains("gori") ? "YES" : "NO");
+	}
+}
