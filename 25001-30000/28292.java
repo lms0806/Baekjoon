f92a69e7d8345fdf15a6fb79e3e7e87f619240d3
lms0806 <godmlzkf1@naver.com>
2024-10-01 12:41:38 +0900

    Create 28292.java

---
+++ 25001-30000/28292.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		System.out.print(n < 3 ? 1 : n < 6 ? 2 : 3);
+	}
+}
