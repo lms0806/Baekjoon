69b5c6c81f15e2d4f143c4b13c0ef8f7ca43f0c9
lms0806 <godmlzkf1@naver.com>
2024-10-06 14:42:36 +0900

    Create 32384.java

---
+++ 30001-35000/32384.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < n; i++) {
+			sb.append("LoveisKoreaUniversity ");
+		}
+		System.out.print(sb);
+	}
+}
