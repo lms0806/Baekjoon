93659f8316ce03a9cbd0ca1753b1ed185d410b7e
lms0806 <godmlzkf1@naver.com>
2024-10-08 20:15:56 +0900

    Create 7132.java

---
+++ 5001-10000/7132.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+		
+		HashSet<Double> set = new HashSet<>();
+		for(double i = a; i <= b; i++) {
+			for(double j = a; j <= b; j++) {
+				set.add(i / j);
+			}
+		}
+		System.out.print(set.size());
+	}
+}
