ca122d9b4c8e9680b5476d7f11daaab74776f1ef
lms0806 <godmlzkf1@naver.com>
2024-11-28 17:12:08 +0900

    Create 7487.java

---
+++ 5001-10000/7487.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(t --> 0) {
+			sb.append(new BigInteger(br.readLine()).gcd(new BigInteger(br.readLine()))).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
