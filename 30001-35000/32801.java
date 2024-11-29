ff89cf34ab774ec3d19ab87a5b20bb73febd4ec8
lms0806 <godmlzkf1@naver.com>
2024-11-29 19:49:42 +0900

    Create 32801.java

---
+++ 30001-35000/32801.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+		
+		int fizz = 0, buzz = 0, fizzbuzz = 0;
+		for(int i = 1; i <= n; i++) {
+			if(i % a == 0) {
+				if(i % b == 0) {
+					fizzbuzz++;
+				}
+				else {
+					fizz++;
+				}
+			}
+			else if(i % b == 0) {
+				buzz++;
+			}
+		}
+		System.out.print(fizz + " " + buzz + " " + fizzbuzz);
+	}
+}
