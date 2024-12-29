8f3363bf9c61a7b45af25da8f4c558f937d94150
lms0806 <godmlzkf1@naver.com>
2024-12-29 11:24:38 +0900

    Create 20737.java

---
+++ 20001-25000/20737.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.print(Double.parseDouble(br.readLine()) - 1);
+	}
+}
