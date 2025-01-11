daab2603281f012d4cacd21fb5873382fdfef130
lms0806 <godmlzkf1@naver.com>
2025-01-11 12:04:13 +0900

    Create 17939.java

---
+++ 15001-20000/17939.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[n], max = new int[n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		max[n - 1] = arr[n - 1];
+		for(int i = n - 2; i >= 0; i--) {
+			max[i] = Math.max(max[i + 1], arr[i]);
+		}
+		
+		int answer = 0;
+		for(int i = 0; i < n; i++) {
+			answer += max[i] - arr[i];
+		}
+		System.out.print(answer);
+	}
+}
