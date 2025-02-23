8633794bfd33e3e7188e736205045bcf937c1a55
lms0806 <godmlzkf1@naver.com>
2025-02-23 21:50:34 +0900

    Create 10219.java

---
+++ 10001-15000/10219.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(t --> 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
+			
+			for(int i = 0; i < n; i++) {
+				sb.append(new StringBuilder(br.readLine()).reverse()).append("\n");
+			}
+		}
+		System.out.print(sb);
+	}
+}
