566eeeb57143abe9d1c76a6e476ea05144671ec2
lms0806 <godmlzkf1@naver.com>
2025-03-03 16:52:45 +0900

    Create 33556.java

---
+++ 30001-35000/33556.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	String a = br.readLine(), b = br.readLine();
+    	
+    	System.out.print(solve(a, b) + "\n" + solve2(a, b));
+    }
+    
+    public static String solve(String a, String b) {
+    	return "null".equals(a) ? "NullPointerException" : "null".equals(b) ? "false" : a.equals(b) ? "true" : "false";
+    }
+    
+    public static String solve2(String a, String b) {
+    	return "null".equals(a) ? "NullPointerException" : "null".equals(b) ? "false" : a.equalsIgnoreCase(b) ? "true" : "false";
+    }
+}
