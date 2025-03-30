51fe1d0f7b28022894be4346ec89694a39df8807
lms0806 <godmlzkf1@naver.com>
2025-03-30 09:00:31 +0900

    Create 33689.java

---
+++ 30001-35000/33689.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		int answer = 0;
+		while(t --> 0) {
+			if(br.readLine().charAt(0) == 'C') {
+				answer++;
+			}
+		}
+		System.out.print(answer);
+	}
+}
