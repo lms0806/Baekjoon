a79b424868cb280552370a8c11f070d2c345d2a3
lms0806 <godmlzkf1@naver.com>
2025-04-10 20:09:04 +0900

    Create 33779.java

---
+++ 30001-35000/33779.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String s = br.readLine();
+		
+		System.out.print(s.equals(new StringBuilder(s).reverse().toString()) ? "beep" : "boop");
+	}
+}
