8d13a711a24c24c36177ba40ca6abd18b001a825
lms0806 <godmlzkf1@naver.com>
2025-05-20 21:08:20 +0900

    Create 33964.java

---
+++ 30001-35000/33964.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		System.out.print(Integer.parseInt("1".repeat(Integer.parseInt(st.nextToken()))) + Integer.parseInt("1".repeat(Integer.parseInt(st.nextToken()))));
+	}
+}
