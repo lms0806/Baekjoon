45dcbe402ad0276c69d58500a7d0965cd7e73a8c
lms0806 <godmlzkf1@naver.com>
2025-05-28 20:04:37 +0900

    Create 33965.java

---
+++ 30001-35000/33965.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		
+		System.out.print(35 * n * (n + 1) / 2);
+	}
+}
